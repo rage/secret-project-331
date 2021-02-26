00ffbe4fb079ebbd4c13cd58f1d9516640597c36
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-02-26 16:07:19 +0200

    Python editor block example

--- headless-cms/components/Editor.js
+++ headless-cms/components/Editor.js
@@ -7,21 +7,56 @@ import {
 	WritingFlow,
 	ObserveTyping,
 } from '@wordpress/block-editor';
+import { useBlockProps } from '@wordpress/block-editor';
 import {
 	Popover,
 	SlotFillProvider,
 	DropZoneProvider,
 } from '@wordpress/components';
 import { registerCoreBlocks } from '@wordpress/block-library';
+import { registerBlockType } from "@wordpress/blocks"
 import '@wordpress/block-editor/build-style/style.css'
 import '@wordpress/components/build-style/style.css'
-import '@wordpress/format-library';
+import '@wordpress/format-library'
+import { ProgrammingExercise } from "moocfi-python-editor"
 
 function Editor() {
 	const [ blocks, updateBlocks ] = useState( [] );
 
 	useEffect( () => {
 		registerCoreBlocks();
+		registerBlockType("jori/test", {
+			title: "Hello",
+			description: "World",
+			category: "embed",
+			attributes: {
+				name: {
+					type: "string",
+				}
+			},
+			edit: () => {
+				const blockProps = useBlockProps( { className: 'my-random-classname' } );
+		 
+				return <div { ...blockProps }>
+					<ProgrammingExercise
+						onExerciseDetailsChange={() => {}}
+						organization={"test"}
+						course={"python-random-testcourse"}
+						exercise={"osa01-01_hymio"}
+						token={"asd"}
+						height={"300px"}
+						outputHeight={"auto"}
+						outputPosition={"relative"}
+						language={"fi"}
+					/>
+			  	</div>;
+			},
+			save: () => {
+				const blockProps = useBlockProps.save();
+			 
+				return <div { ...blockProps }>Your block.</div>;
+			}
+		})
 	}, [] );
 
 	return (

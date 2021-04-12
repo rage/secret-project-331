3f865f5b635a06c2136888be12e39b91f3546a35
Henrik Nygren <nygrenh@gmail.com>
2021-04-12 12:15:07 +0300

    Move course material to domain root

--- cms/next.config.js
+++ cms/next.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+const config = {
   webpack(config, { dev, isServer }) {
     const aliases = config.resolve.alias || (config.resolve.alias = {})
     //aliases.react = aliases['react-dom'] = '@wordpress/element'
     return config
   },
 }
+
+if (process.env.BASE_PATH) {
+  config.basePath = process.env.BASE_PATH
+}
+
+module.exports = config

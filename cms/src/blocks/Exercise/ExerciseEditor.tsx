f46b8147c286741315a179da572c8f934e455e92
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-04-16 13:13:49 +0100

    Cleanup

--- cms/src/blocks/Exercise/ExerciseEditor.tsx
+++ cms/src/blocks/Exercise/ExerciseEditor.tsx
@@ -1,15 +1,7 @@
-import { useEffect } from "react"
 import styled from "@emotion/styled"
 import { TextField } from "@material-ui/core"
-import ChooseExerciseItemType from "./ChooseExerciseItemType"
-import IFrameEditor from "./IFrameEditor"
 import { BlockEditProps } from "@wordpress/blocks"
-import { v4 } from "uuid"
-import { exerciseFamilySelector } from "../../state/exercises"
-import { useRecoilState } from "recoil"
-import { ExerciseItem, PageUpdateExerciseItem } from "../../services/services.types"
 import { ExerciseAttributes } from "."
-import { exerciseItemTypes } from "./ChooseExerciseItemType/ExerciseServiceList"
 import { InnerBlocks } from "@wordpress/block-editor"
 
 const ALLOWED_NESTED_BLOCKS = ["moocfi/exercise-item"]
@@ -23,73 +15,20 @@ const ExerciseEditorCard = styled.div`
 const Title = styled.h1`
   font-size: 24px;
 `
-
-/**
- * Wrapper for Exercises
- * @param param0 
- * @returns 
- */
 const ExerciseEditor: React.FC<BlockEditProps<ExerciseAttributes>> = ({ attributes, setAttributes }) => {
-  // const [exercise, setExercise] = useRecoilState(exerciseFamilySelector(attributes.exercise_id))
-
-  // const onChooseExerciseType = (selectedItem: any) => {
-  //   setExercise((prev) => {
-  //     const newItem: PageUpdateExerciseItem = {
-  //       id: v4(),
-  //       exercise_type: selectedItem.identifier,
-  //       assignment: [],
-  //       spec: null,
-  //     }
-  //     return { ...prev, exercise_items: [...prev.exercise_items, newItem] }
-  //   })
-  // }
-
-  // useEffect(() => {
-  //   if (exercise) {
-  //     return
-  //   }
-  //   setExercise({ id: attributes.exercise_id, exercise_items: [], name: "" })
-  // }, [exercise])
-
-  // if (!exercise) {
-  //   return null
-  // }
-  // const exerciseChosen = exercise.exercise_items.length > 0
-
   return (
-    // <ExerciseEditorCard id={exercise.id}>
     <ExerciseEditorCard id={attributes.exercise_id}>
       <div>Exercise editor</div>
       <TextField
         fullWidth
         variant="outlined"
         value={attributes.name}
-        // value={exercise.name}
-        // onChange={(e) =>
-        //   setExercise((prev) => {
-        //     return { ...prev, name: e.target.value }
-        //   })
-        // }
         onChange={(e) =>
           setAttributes({ name: e.target.value })
         }
       />
-      {/* Exercise assignment is represented as nested gutenberg blocks.
-      However, when saving these to the database, they will be separated */}
       <Title>{attributes.name}</Title>
       <InnerBlocks allowedBlocks={ALLOWED_NESTED_BLOCKS} />
-      {/* {!exerciseChosen && <ChooseExerciseItemType onChooseItem={onChooseExerciseType} />}
-      {exerciseChosen && (
-        <>
-          <Title>{exercise.name}</Title>
-          {exercise.exercise_items.map((ei: ExerciseItem | PageUpdateExerciseItem) => {
-            const url = exerciseItemTypes.find((o) => o.identifier === ei.exercise_type)?.url
-            return (
-              <IFrameEditor key={ei.id} parentId={exercise.id} exerciseItemid={ei.id} url={url} />
-            )
-          })}
-        </>
-      )} */}
     </ExerciseEditorCard>
   )
 }
--- cms/src/blocks/ExerciseItem/ExerciseItemEditor.tsx
+++ cms/src/blocks/ExerciseItem/ExerciseItemEditor.tsx
@@ -1,13 +1,8 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import styled from "@emotion/styled"
-import { TextField } from "@material-ui/core"
 import ChooseExerciseItemType from "./ChooseExerciseItemType"
 import IFrameEditor from "./IFrameEditor"
 import { BlockEditProps } from "@wordpress/blocks"
-import { v4 } from "uuid"
-import { exerciseFamilySelector } from "../../state/exercises"
-import { useRecoilState } from "recoil"
-import { ExerciseItem, PageUpdateExerciseItem } from "../../services/services.types"
 import { ExerciseItemAttributes } from "."
 import { exerciseItemTypes } from "./ChooseExerciseItemType/ExerciseServiceList"
 import { InnerBlocks } from "@wordpress/block-editor"
@@ -18,33 +13,18 @@ const ExerciseItemEditorCard = styled.div`
   border-radius: 2px;
 `
 
-const Title = styled.h1`
-  font-size: 24px;
-`
-
 const ALLOWED_NESTED_BLOCKS = ["core/image", "core/paragraph", "core/list"]
-
 const ExerciseItemEditor: React.FC<BlockEditProps<ExerciseItemAttributes>> = (props) => {
-  // const [exercise, setExercise] = useRecoilState(exerciseFamilySelector(attributes.exercise_id))
-  const { attributes, setAttributes } = props
-  const [exerciseItem, setExerciseItem] = useState<undefined | PageUpdateExerciseItem | ExerciseItem>(undefined)
+  const { attributes } = props
+  const [exerciseType, setExerciseType] = useState<undefined | string>(undefined)
 
-  const onChooseExerciseType = (selectedItem: any) => {
-    setExerciseItem({
-        id: v4(),
-        exercise_type: selectedItem.identifier,
-        assignment: [],
-        spec: null,
-      })
-  }
-
-  const url = exerciseItemTypes.find((o) => o.identifier === exerciseItem?.exercise_type)?.url
+  const url = exerciseItemTypes.find((o) => o.identifier === exerciseType)?.url
   const id = attributes.exercise_item_id
   return (
     <ExerciseItemEditorCard id={attributes.exercise_item_id}>
       <InnerBlocks allowedBlocks={ALLOWED_NESTED_BLOCKS} />
-      {!exerciseItem && <ChooseExerciseItemType onChooseItem={onChooseExerciseType} />}
-      {exerciseItem && <IFrameEditor key={id} exerciseItemid={id} url={url} props={props} />}
+      {!exerciseType && <ChooseExerciseItemType onChooseItem={setExerciseType} />}
+      {exerciseType && <IFrameEditor key={id} exerciseItemid={id} url={url} props={props} />}
     </ExerciseItemEditorCard>
   )
 }

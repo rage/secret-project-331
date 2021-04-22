06ccfe1e58b0746e9c05bdd45dba294b29a8c5ee
george-misan <george.eyoyibo@gmail.com>
2021-04-22 15:44:20 +0300

    fetch fromm upstream

---
+++ course-material/src/components/ContentRenderer/ImageBlock.tsx
@@ -0,0 +1,23 @@
+import { css } from "@emotion/css"
+import { BlockRendererProps } from "."
+import { normalWidthCenteredComponentStyles } from "../../styles/componentStyles"
+
+interface ImageBlockAttributes {
+  alt: string
+  url: string
+  caption: string
+}
+
+const ImageBlock: React.FC<BlockRendererProps<ImageBlockAttributes>> = ({ data }) => {
+  const attributes: ImageBlockAttributes = data.attributes
+  return (
+    <figure>
+      <img src={attributes.url} alt={attributes.alt}
+        className={css`${normalWidthCenteredComponentStyles}`}
+      />
+      <figcaption>{attributes.caption}</figcaption>
+    </figure>
+  )
+}
+
+export default ImageBlock

cfa3d6ba9e24d80a23b433ad6d44937edd578658
george-misan <george.eyoyibo@gmail.com>
2021-04-29 11:16:19 +0300

    + BubttonBlock

--- course-material/src/components/ContentRenderer/HeadingBlock.tsx
+++ course-material/src/components/ContentRenderer/HeadingBlock.tsx
@@ -1,6 +1,9 @@
 import { css } from "@emotion/css"
 import { BlockRendererProps } from "."
 import { normalWidthCenteredComponentStyles } from "../../styles/componentStyles"
+import H1 from "./Headings/H1"
+import H2 from "./Headings/H2"
+import H3 from "./Headings/H3"
 
 interface HeadingBlockAttributes {
   level: string
@@ -21,3 +24,26 @@ const HeadingBlock: React.FC<BlockRendererProps<HeadingBlockAttributes>> = ({ da
 }
 
 export default HeadingBlock
+
+
+
+/* const HeadingBlock: React.FC<BlockRendererProps<HeadingBlockAttributes>> = ({ data }) => {
+  onst attributes: HeadingBlockAttributes = data.attributesc
+  return (
+    {
+      switch (attributes.level) {
+        case 1:
+          <H1>{attributes.content}</H1>
+          break;
+        case 2:
+          <H2>{attributes.content}</H2>
+          break;
+        case 3:
+          <H3>{attributes.content}</H3>
+          break;
+      }   
+    }
+  )
+}
+
+export default HeadingBlock */

f9798ee0e370982ef84d98e683d1700a90a093af
george-misan <george.eyoyibo@gmail.com>
2021-04-29 15:59:09 +0300

    + Table

---
+++ course-material/src/components/ContentRenderer/AudioBlock.tsx
@@ -0,0 +1,28 @@
+import { css } from "@emotion/css"
+import { BlockRendererProps } from "."
+import { normalWidthCenteredComponentStyles } from "../../styles/componentStyles"
+
+interface AudioBlockAttributes {
+  url: string
+  caption: string
+}
+
+const AudioBlock: React.FC<BlockRendererProps<AudioBlockAttributes>> = ({ data }) => {
+  const attributes: AudioBlockAttributes = data.attributes
+  return (
+    <figure>
+    <audio controls>
+      <source
+        src={attributes.url}
+        className={css`
+          ${normalWidthCenteredComponentStyles}
+        `}
+      />
+      </audio>
+      <figcaption>{attributes.caption}</figcaption>
+      </figure>
+    
+  )
+}
+
+export default AudioBlock

a616438bbf91327ec4e795f0fb0f86751d084e70
Henrik Nygren <nygrenh@gmail.com>
2021-04-30 15:54:58 +0300

    Simple grader

--- example-exercise/src/pages/api/grade.ts
+++ example-exercise/src/pages/api/grade.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default (req: NextApiRequest, res: NextApiResponse): unknown => {
   if (req.method !== "POST") {
     return res.status(404)
   }
@@ -9,13 +9,19 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
 }
 
 interface GradingResult {
-  grading_progress: "FullyGraded | Pending | PendingManual | Failed"
+  grading_progress: "FullyGraded" | "Pending" | "PendingManual" | "Failed"
   score_given: number
   score_maximum: number
   feedback_text: string
   feedback_obj: null
 }
 
-const handlePost = (req: NextApiRequest, res: NextApiResponse<GradingResult>) => {
-  res.status(200).json({ name: "John Doe" })
+const handlePost = (_req: NextApiRequest, res: NextApiResponse<GradingResult>) => {
+  res.status(200).json({
+    grading_progress: "FullyGraded",
+    score_given: 1,
+    score_maximum: 1,
+    feedback_text: "Good job!",
+    feedback_obj: null,
+  })
 }

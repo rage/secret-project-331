37ce9d68ddcc9f28b591d7c9386124b954fe7646
Henrik Nygren <nygrenh@gmail.com>
2021-05-03 09:30:11 +0300

    Fix course page uniqueness index

---
+++ headless-lms/migrations/20210503061144_add_fix_course_path_uniqueness_index.down.sql
@@ -0,0 +1,3 @@
+-- Add down migration script here
+ALTER TABLE pages DROP constraint pages_url_path_course_id_deleted_key;
+ALTER TABLE pages ADD constraint pages_url_path_deleted_key UNIQUE (url_path, deleted);

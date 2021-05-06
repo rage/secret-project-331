9d3f151381fad39f294f717d37f91e1a81898589
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-05-06 09:25:35 +0100

    Pages in part block, eslint

---
+++ cms/src/blocks/PagesInPart/PagesInPartSave.tsx
@@ -0,0 +1,11 @@
+import { InnerBlocks } from "@wordpress/block-editor"
+
+const PagesInPartSave: React.FC = () => {
+  return (
+    <div>
+      <InnerBlocks.Content />
+    </div>
+  )
+}
+
+export default PagesInPartSave

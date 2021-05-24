0f024f14afc69fbfedacdcd5911cc45bbe43a449
Henrik Nygren <nygrenh@gmail.com>
2021-05-24 11:30:16 +0300

    Refactor paragraphblock

--- course-material/src/styles/fontSizeMapper.tsx
+++ course-material/src/styles/fontSizeMapper.tsx
@@ -1,14 +1,22 @@
-const fontSizes = [
-  ["small", "13px"],
-  ["normal", "16px"],
-  ["medium", "24px"],
-  ["large", "30px"],
-  ["huge", "36px"],
-]
+const fontSizes: { [colorName: string]: string | undefined } = {
+  small: "13px",
+  normal: "16px",
+  medium: "24px",
+  large: "30px",
+  huge: "36px",
+}
 
-const FontSizeMapper = (fontName: string | undefined) => {
-  const fontSize =
-    fontName !== undefined ? fontSizes.find((fontSize) => fontSize[0] === fontName)[1] : "16px"
+const DEFAULT_FONT_SIZE = "16px"
+
+const FontSizeMapper = (fontName: string | undefined): string => {
+  if (!fontName) {
+    return DEFAULT_FONT_SIZE
+  }
+  const fontSize = fontSizes[fontName]
+
+  if (!fontSize) {
+    return DEFAULT_FONT_SIZE
+  }
 
   return fontSize
 }

98dd6ce842968c6d08667b61389ea12c3c9eb548
Henrik Nygren <nygrenh@gmail.com>
2021-05-24 19:18:13 +0300

    Refactor example exercise exercise

---
+++ example-exercise/src/components/Exercise.tsx
@@ -0,0 +1,77 @@
+import { PublicAlternative } from "../util/stateInterfaces"
+import { css } from "@emotion/css"
+import { useLayoutEffect, useRef, useState } from "react"
+
+interface Props {
+  state: PublicAlternative[]
+  onHeightChange: (newHeight: number, port: MessagePort) => void
+  port: MessagePort
+  maxWidth: number | null
+}
+
+const Exercise: React.FC<Props> = ({ onHeightChange, port, maxWidth, state }) => {
+  const contentRef = useRef<HTMLDivElement>(null)
+  useLayoutEffect(() => {
+    const ref = contentRef.current
+    if (!ref || !port) {
+      return
+    }
+    onHeightChange(ref.getBoundingClientRect().height, port)
+  })
+
+  const [selectedId, _setSelectedId] = useState<string | null>(null)
+
+  const setSelectedId: typeof _setSelectedId = (value) => {
+    const res = _setSelectedId(value)
+    if (!port) {
+      console.error("Cannot send current state to parent because I don't have a port")
+      return
+    }
+    console.log("Posting current state to parent")
+    port.postMessage({ message: "current-state-2", data: { selectedValue: value } })
+    return res
+  }
+
+  return (
+    <div
+      ref={contentRef}
+      className={css`
+        width: 100%;
+        ${maxWidth && `max-width: ${maxWidth}px;`}
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+      `}
+    >
+      {state.map((option) => {
+        const selected = selectedId === option.id
+        return (
+          <button
+            className={css`
+              padding: 1rem 2rem;
+              background-color: ${selected ? "#4210f5" : "#6188ff"};
+              border-radius: 1rem;
+              border: 0;
+              color: white;
+              transition: all 0.3s;
+              cursor: pointer;
+              margin-top: 0.5rem;
+              margin-bottom: 0.5rem;
+
+              &:hover {
+                background-color: ${selected ? "#330eb8" : "#507afb"};
+              }
+            `}
+            aria-selected={selected}
+            onClick={() => setSelectedId(option.id)}
+            key={option.id}
+          >
+            {option.name}
+          </button>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Exercise
--- example-exercise/src/pages/exercise.tsx
+++ example-exercise/src/pages/exercise.tsx
@@ -1,9 +1,9 @@
-import { css } from "@emotion/css"
 import { useRouter } from "next/dist/client/router"
-import { useEffect, useLayoutEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
+import Exercise from "../components/Exercise"
 import { PublicAlternative } from "../util/stateInterfaces"
 
-const Editor: React.FC = () => {
+const ExercisePage: React.FC = () => {
   const [state, setState] = useState<PublicAlternative[] | null>(null)
   const [port, setPort] = useState<MessagePort | null>(null)
   const router = useRouter()
@@ -42,29 +42,6 @@ const Editor: React.FC = () => {
     }
   }, [])
 
-  const contentRef = useRef<HTMLDivElement>(null)
-  // Automatic height resizing events
-  const [selectedId, _setSelectedId] = useState<string | null>(null)
-
-  const setSelectedId: typeof _setSelectedId = (value) => {
-    const res = _setSelectedId(value)
-    if (!port) {
-      console.error("Cannot send current state to parent because I don't have a port")
-      return
-    }
-    console.log("Posting current state to parent")
-    port.postMessage({ message: "current-state-2", data: { selectedValue: value } })
-    return res
-  }
-
-  useLayoutEffect(() => {
-    const ref = contentRef.current
-    if (!ref || !port) {
-      return
-    }
-    onHeightChange(ref.getBoundingClientRect().height, port)
-  })
-
   if (!maxWidth) {
     return null
   }
@@ -75,46 +52,7 @@ const Editor: React.FC = () => {
   if (!port) {
     return <>Waiting for port...</>
   }
-  return (
-    <div
-      ref={contentRef}
-      className={css`
-        width: 100%;
-        ${maxWidth && `max-width: ${maxWidth}px;`}
-        margin: 0 auto;
-        display: flex;
-        flex-direction: column;
-      `}
-    >
-      {state.map((option) => {
-        const selected = selectedId === option.id
-        return (
-          <button
-            className={css`
-              padding: 1rem 2rem;
-              background-color: ${selected ? "#4210f5" : "#6188ff"};
-              border-radius: 1rem;
-              border: 0;
-              color: white;
-              transition: all 0.3s;
-              cursor: pointer;
-              margin-top: 0.5rem;
-              margin-bottom: 0.5rem;
-
-              &:hover {
-                background-color: ${selected ? "#330eb8" : "#507afb"};
-              }
-            `}
-            aria-selected={selected}
-            onClick={() => setSelectedId(option.id)}
-            key={option.id}
-          >
-            {option.name}
-          </button>
-        )
-      })}
-    </div>
-  )
+  return <Exercise port={port} maxWidth={maxWidth} onHeightChange={onHeightChange} state={state} />
 }
 
 function onHeightChange(newHeight: number, port: MessagePort) {
@@ -124,4 +62,4 @@ function onHeightChange(newHeight: number, port: MessagePort) {
   })
 }
 
-export default Editor
+export default ExercisePage

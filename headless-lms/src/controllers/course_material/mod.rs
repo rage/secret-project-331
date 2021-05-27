9a574f35d1ec0af4d995f344a5da76192d654448
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-05-27 16:33:52 +0300

    Add pages in course parts

--- headless-lms/src/controllers/course_material/mod.rs
+++ headless-lms/src/controllers/course_material/mod.rs
@@ -5,6 +5,7 @@ This documents all endpoints. Select a module below for a category.
 
 */
 
+pub mod course_parts;
 pub mod courses;
 pub mod exercises;
 pub mod organizations;
@@ -12,13 +13,14 @@ pub mod organizations;
 use actix_web::web::{self, ServiceConfig};
 
 use self::{
-    courses::_add_courses_routes, exercises::_add_exercises_routes,
-    organizations::_add_organizations_routes,
+    course_parts::_add_course_parts_routes, courses::_add_courses_routes,
+    exercises::_add_exercises_routes, organizations::_add_organizations_routes,
 };
 
 /// Add controllers from all the submodules.
 pub fn add_course_material_routes(cfg: &mut ServiceConfig) {
     cfg.service(web::scope("/courses").configure(_add_courses_routes))
         .service(web::scope("/organizations").configure(_add_organizations_routes))
-        .service(web::scope("/exercises").configure(_add_exercises_routes));
+        .service(web::scope("/exercises").configure(_add_exercises_routes))
+        .service(web::scope("/course-parts").configure(_add_course_parts_routes));
 }

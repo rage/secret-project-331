38c5c5adfc21e630a1cd1f1fdc147af40c303867
sebazai <39335537+sebazai@users.noreply.github.com>
2021-05-31 11:15:15 +0000

    Deploying to gh-pages from  @ 70f881b61b528fa357578e909117821a078dbbab 🚀

--- headless_lms_actix/models/course_parts/sidebar-items.js
+++ headless_lms_actix/models/course_parts/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["course_course_parts",""],["delete_course_part",""],["insert_course_part",""],["update_course_part",""]],"struct":[["CoursePart",""],["CoursePartUpdate",""],["NewCoursePart",""]]});
+initSidebarItems({"fn":[["course_course_parts",""],["delete_course_part",""],["insert_course_part",""],["update_course_part",""]],"struct":[["CoursePart",""],["CoursePartPagesWithExercises",""],["CoursePartUpdate",""],["NewCoursePart",""]]});

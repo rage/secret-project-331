bc0da577a8727d5dbb2f40a6d35d947675f88073
heikki <heikki.pulli@helsinki.fi>
2021-06-04 17:17:13 +0300

    maybe breaking

--- services/headless-lms/src/controllers/course_material/pages.rs
+++ services/headless-lms/src/controllers/course_material/pages.rs
@@ -5,15 +5,15 @@ use std::str::FromStr;
 use uuid::Uuid;
 
 async fn get_next_page(
-  request_page_id: web::Path<String>,
-  pool: web::Data<PgPool>,
+    request_page_id: web::Path<String>,
+    pool: web::Data<PgPool>,
 ) -> ApplicationResult<Json<NextPage>> {
-  let pages_id = Uuid::from_str(&request_page_id)?;
-  let next_page_data: NextPage =
-    crate::models::pages::get_next_page(pool.get_ref(), pages_id).await?;
-  Ok(Json(next_page_data))
+    let pages_id = Uuid::from_str(&request_page_id)?;
+    let next_page_data: NextPage =
+        crate::models::pages::get_next_page(pool.get_ref(), pages_id).await?;
+    Ok(Json(next_page_data))
 }
 
 pub fn _add_pages_routes(cfg: &mut ServiceConfig) {
-  cfg.route("/:current_page_id/next_page", web::get().to(get_next_page));
+    cfg.route("/:current_page_id/next_page", web::get().to(get_next_page));
 }

0ac150e311450f6fa91a6818b61e423bab0b1ed2
Henrik Nygren <nygrenh@gmail.com>
2021-06-04 08:17:17 +0300

    Check sqlx prepare on precommit

--- .husky/lint-staged.config.js
+++ .husky/lint-staged.config.js
@@ -4,5 +4,6 @@ module.exports = {
   "*.rs": () => [
     "cargo fmt --manifest-path services/headless-lms/Cargo.toml -- --files-with-diff",
     "cargo clippy --manifest-path services/headless-lms/Cargo.toml -- -D warnings",
+    "./services/headless-lms/bin/sqlx-prepare-check",
   ],
 }

d218af190c2340a12cce21343e1fa826b4b48268
Heliozoa <daniel.x.martinez@helsinki.fi>
2021-06-10 01:32:06 +0300

    Add login, logout, logged-in

---
+++ services/headless-lms/src/controllers/auth.rs
@@ -0,0 +1,87 @@
+/*!
+Handlers for HTTP requests to `/api/v0/login`.
+*/
+
+use crate::{controllers::ApplicationResult, OAuthClient};
+use actix_session::Session;
+use actix_web::{
+    web::{self, Json, ServiceConfig},
+    HttpResponse,
+};
+use anyhow::Context;
+use oauth2::{ResourceOwnerPassword, ResourceOwnerUsername, TokenResponse};
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub struct Login {
+    login: String,
+    password: String,
+}
+
+#[derive(Deserialize)]
+struct CurrentUser {
+    id: i32,
+}
+
+/**
+POST `/api/v0/auth/login` Logs in to TMC.
+**/
+pub async fn login(
+    session: Session,
+    client: web::Data<OAuthClient>,
+    payload: web::Form<Login>,
+) -> ApplicationResult<HttpResponse> {
+    let Login { login, password } = payload.into_inner();
+
+    let token = client
+        .exchange_password(
+            &ResourceOwnerUsername::new(login.clone()),
+            &ResourceOwnerPassword::new(password.clone()),
+        )
+        .request_async(oauth2::reqwest::async_http_client)
+        .await
+        .context("Failed to authenticate")?;
+
+    let current_user_url = "https://tmc.mooc.fi/api/v8/users/current";
+    let client = Client::default();
+    let res = client
+        .get(current_user_url)
+        .bearer_auth(token.access_token().secret())
+        .send()
+        .await
+        .context("Failed to send request to TMC")?;
+    if !res.status().is_success() {
+        return Err(anyhow::anyhow!("Failed to get current user from TMC").into());
+    }
+
+    let current_user: CurrentUser = res.json().await.context("Unexpected response from TMC")?;
+    let legacy_id = current_user.id;
+
+    session
+        .insert("session", legacy_id)
+        .map_err(|_| anyhow::anyhow!("Failed to insert to session"))?;
+    Ok(HttpResponse::Ok().finish())
+}
+
+/**
+POST `/api/v0/auth/logout` Logs out.
+**/
+pub async fn logout(session: Session) -> HttpResponse {
+    session.remove("session");
+    HttpResponse::Ok().finish()
+}
+
+/**
+GET `/api/v0/auth/logged-in` Logs in to TMC.
+**/
+pub async fn logged_in(session: Session) -> Json<bool> {
+    let logged_in = session.entries().get("session").is_some();
+    Json(logged_in)
+}
+
+pub fn add_auth_routes(cfg: &mut ServiceConfig) {
+    cfg.route("/login", web::post().to(login))
+        .route("/logout", web::post().to(logout))
+        .route("/logged-in", web::get().to(logged_in));
+}
--- services/headless-lms/src/main.rs
+++ services/headless-lms/src/main.rs
@@ -6,13 +6,18 @@ pub mod utils;
 #[macro_use]
 extern crate log;
 
+use actix_session::CookieSession;
 use actix_web::{error, middleware::Logger, web, App, HttpResponse, HttpServer};
 use anyhow::Result;
 use controllers::configure_controllers;
 use dotenv::dotenv;
 use listenfd::ListenFd;
+use oauth2::{basic::BasicClient, AuthUrl, ClientId, ClientSecret, TokenUrl};
 use sqlx::PgPool;
-use std::env;
+use std::{env, sync::Arc};
+use url::Url;
+
+type OAuthClient = Arc<BasicClient>;
 
 /// The entrypoint to the application.
 #[actix_web::main]
@@ -21,12 +26,30 @@ async fn main() -> Result<()> {
     dotenv().ok();
     env_logger::init();
 
+    // read environment variables
+    let database_url = env::var("DATABASE_URL")
+        .unwrap_or_else(|_| "postgres://localhost/headless_lms_dev".to_string());
+    let oauth_application_id =
+        env::var("OAUTH_APPLICATION_ID").expect("OAUTH_APPLICATION_ID must be defined");
+    let oauth_secret = env::var("OAUTH_SECRET").expect("OAUTH_SECRET must be defined");
+    let private_cookie_key =
+        env::var("PRIVATE_COOKIE_KEY").expect("PRIVATE_COOKIE_KEY must be defined");
+
     // this will enable us to keep application running during recompile: systemfd --no-pid -s http::5000 -- cargo watch -x run
     let mut listenfd = ListenFd::from_env();
 
-    let database_url = env::var("DATABASE_URL")
-        .unwrap_or_else(|_| "postgres://localhost/headless_lms_dev".to_string());
     let db_pool = PgPool::connect(&database_url).await?;
+
+    let auth_url: Url = "https://tmc.mooc.fi/oauth/token"
+        .parse()
+        .expect("known to work");
+
+    let oauth_client: OAuthClient = Arc::new(BasicClient::new(
+        ClientId::new(oauth_application_id),
+        Some(ClientSecret::new(oauth_secret)),
+        AuthUrl::from_url(auth_url.clone()),
+        Some(TokenUrl::from_url(auth_url)),
+    ));
 
     let mut server = HttpServer::new(move || {
         let json_config = web::JsonConfig::default()
@@ -40,12 +63,15 @@ async fn main() -> Result<()> {
                 ));
                 error::InternalError::from_response(err, response).into()
             });
+
         App::new()
+            .wrap(CookieSession::private(private_cookie_key.as_bytes()).secure(false))
             .wrap(Logger::new(
                 "\"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %a %Dms",
             ))
             .app_data(json_config)
             .data(db_pool.clone()) // pass database pool to application so we can access it inside handlers
+            .data(oauth_client.clone())
             .service(web::scope("/api/v0").configure(configure_controllers))
     });
 

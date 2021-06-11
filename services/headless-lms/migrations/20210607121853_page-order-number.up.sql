7fa99567da71f3b7ffc76e7b9cb782f931486a9f
d471061c <cpastethat@gmail.com>
2021-06-11 10:10:20 +0300

    Update services/headless-lms/migrations/20210607121853_page-order-number.up.sql
    Co-authored-by: Henrik Nygren <nygrenh@gmail.com>

--- services/headless-lms/migrations/20210607121853_page-order-number.up.sql
+++ services/headless-lms/migrations/20210607121853_page-order-number.up.sql
@@ -1,4 +1,4 @@
 ALTER TABLE pages
 ADD COLUMN order_number INTEGER NOT NULL;
-CREATE UNIQUE INDEX pages_order_number_uniqueness ON pages (id, order_number)
+CREATE UNIQUE INDEX pages_order_number_uniqueness ON pages (chapter_id, order_number)
 WHERE deleted_at IS NULL;

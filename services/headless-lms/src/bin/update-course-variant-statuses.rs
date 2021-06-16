f06c62c9dab00789716a82336176146e6b71dc3d
Martinez <Heliozoa@users.noreply.github.com>
2021-06-16 10:09:08 +0300

    Course variant status (#68)
    * Added course-variant-status to courses
    
    * Added update-course-variant-statuses bin
    
    * Bug and rebase fixes
    
    * Implemented update-course-variant-statuses
    
    * cronjob
    
    * Moved cronjob config file, set image to headless-lms and added to kustomization.yaml. Addressed other review change requests
    
    * Fixed sqlx prepare not generating query info for both binaries
    
    * Moved cronjob and models

---
+++ services/headless-lms/src/bin/update-course-variant-statuses.rs
@@ -0,0 +1,70 @@
+use anyhow::Result;
+use chrono::Utc;
+use dotenv::dotenv;
+use headless_lms_actix::models::course_instances::{CourseInstance, VariantStatus};
+use sqlx::PgPool;
+use std::env;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    env::set_var("RUST_LOG", "info,actix_web=info");
+    dotenv().ok();
+    env_logger::init();
+
+    let database_url = env::var("DATABASE_URL")
+        .unwrap_or_else(|_| "postgres://localhost/headless_lms_dev".to_string());
+    let db_pool = PgPool::connect(&database_url).await?;
+    let mut transaction = db_pool.begin().await?;
+
+    let course_instances = sqlx::query_as!(
+        CourseInstance,
+        r#"
+SELECT
+    id, variant_status as "variant_status: VariantStatus", starts_at, ends_at
+FROM course_instances
+WHERE deleted_at IS NOT NULL;
+"#
+    )
+    .fetch_all(&mut transaction)
+    .await?;
+
+    for course_instance in course_instances {
+        if course_instance.variant_status == VariantStatus::Upcoming {
+            if let Some(starts_at) = course_instance.starts_at {
+                if starts_at <= Utc::now() {
+                    sqlx::query!(
+                        r#"
+UPDATE course_instances
+SET variant_status = $1
+WHERE id = $2;
+"#,
+                        VariantStatus::Active as _,
+                        course_instance.id
+                    )
+                    .execute(&mut transaction)
+                    .await?;
+                }
+            }
+        } else if course_instance.variant_status == VariantStatus::Active {
+            if let Some(ends_at) = course_instance.ends_at {
+                if ends_at <= Utc::now() {
+                    sqlx::query!(
+                        r#"
+UPDATE course_instances
+SET variant_status = $1
+WHERE id = $2;
+"#,
+                        VariantStatus::Ended as _,
+                        course_instance.id
+                    )
+                    .execute(&mut transaction)
+                    .await?;
+                }
+            }
+        }
+    }
+
+    transaction.commit().await?;
+
+    Ok(())
+}

63c667436fd959d4861fbcb18c0b8251c827092e
heikki <heikki.pulli@helsinki.fi>
2021-06-17 15:06:19 +0300

    listed pages are links

--- services/course-material/src/components/ContentRenderer/PagesListBlock/PagesInChapter.tsx
+++ services/course-material/src/components/ContentRenderer/PagesListBlock/PagesInChapter.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link"
+import { useRouter } from "next/router"
 import React from "react"
 import { useQuery } from "react-query"
 import { fetchChaptersPagesWithExercises } from "../../../services/backend"
 import GenericLoading from "../../GenericLoading"
 
 const PagesInChapter: React.FC<{ chapterId: string }> = ({ chapterId }) => {
+  const coursePath = useRouter().asPath
   const { isLoading, error, data } = useQuery(`chapter-${chapterId}-pages`, () =>
     fetchChaptersPagesWithExercises(chapterId),
   )
@@ -21,7 +24,7 @@ const PagesInChapter: React.FC<{ chapterId: string }> = ({ chapterId }) => {
       <ol>
         {data.map((page) => (
           <li key={page.id} id={page.id}>
-            {page.title}
+            <Link href={coursePath + page.url_path}>{page.title}</Link>
           </li>
         ))}
       </ol>

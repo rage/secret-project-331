797da5955040e3128eb039438c7c127721363296
d471061c <cpastethat@gmail.com>
2021-06-21 10:29:28 +0300

    User course progress (#74)
    * add user course progress to models
    
    * fix typo
    
    * fix queries
    
    * Update queries and fetch in parallel
    
    * remove unused join
    
    * add controller draft
    
    * Update services/headless-lms/src/models/user_exercise_states.rs
    
    Co-authored-by: Henrik Nygren <nygrenh@gmail.com>
    
    * get user in course progress controller
    
    * fix user progress model
    
    * remove unused join
    
    * add the missing data
    
    * add get-request to requests.rest
    
    Co-authored-by: Henrik Nygren <nygrenh@gmail.com>

---
+++ services/headless-lms/src/controllers/course_material/course_instances.rs
@@ -0,0 +1,39 @@
+//! Controllers for requests starting with `/api/v0/course-material/course-instances`.
+use crate::domain::authorization::AuthUser;
+use crate::{controllers::ApplicationResult, models::user_exercise_states::UserProgress};
+use actix_web::web::{self, Json, ServiceConfig};
+use sqlx::PgPool;
+use uuid::Uuid;
+
+/**
+ GET /api/v0/course-material/course-instance/:course_intance_id/progress - returns user progress information.
+ # Example,
+```json
+{
+   "score_given": 3,
+   "score_maximum": 10,
+   "total_exericises": 66,
+   "completed_exercises": 13
+}
+```
+*/
+async fn get_user_progress_page(
+    user: AuthUser,
+    request_course_instance_id: web::Path<Uuid>,
+    pool: web::Data<PgPool>,
+) -> ApplicationResult<Json<Option<UserProgress>>> {
+    let user_course_progress = crate::models::user_exercise_states::get_user_progress(
+        pool.get_ref(),
+        &request_course_instance_id,
+        &user.id,
+    )
+    .await?;
+    Ok(Json(Some(user_course_progress)))
+}
+
+pub fn _add_user_progress_routes(cfg: &mut ServiceConfig) {
+    cfg.route(
+        "/{course_instance_id}/progress",
+        web::get().to(get_user_progress_page),
+    );
+}

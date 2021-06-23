926b17116f04a0e38093c1ea141a0786789a5412
Henrik Nygren <nygrenh@gmail.com>
2021-06-23 11:17:51 +0300

    Fix lint staged config

--- .husky/lint-staged.config.js
+++ .husky/lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "*.js,*.ts,*.jsx,*.tsx": ["eslint --cache --fix", "stylelint --fix"],
-  "*.md,*.json,*.scss,*.css": "prettier --write",
+  "*.{js,jsx,ts,tsx}": ["eslint --cache --fix", "stylelint --fix"],
+  "*.{md,json,scss,css}": "prettier --write",
   "*.rs": () => [
     "cargo fmt --manifest-path services/headless-lms/Cargo.toml -- --files-with-diff",
     "cargo clippy --manifest-path services/headless-lms/Cargo.toml -- -D warnings",

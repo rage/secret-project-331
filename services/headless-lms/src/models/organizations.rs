f8fc825f2002cefc9d14b98e2627d1155d0bdf77
Martinez <Heliozoa@users.noreply.github.com>
2021-06-24 10:32:45 +0300

    Testing (#116)
    * set up helper for integration tests with db
    
    * Added support module for unit tests
    
    * Moved some configuration to lib
    
    * Removed use of a recently stabilised function which was not supported on CI
    
    * Moved some configuration to lib and added default db url for integration test
    
    * Added default db url for Conn and documented its use
    
    * Ignore tests for now due to missing CI setup

--- services/headless-lms/src/models/organizations.rs
+++ services/headless-lms/src/models/organizations.rs
@@ -23,3 +23,33 @@ pub async fn all_organizations(conn: &mut PgConnection) -> Result<Vec<Organizati
     .await?;
     Ok(courses)
 }
+
+pub async fn insert(slug: &str, name: &str, conn: &mut PgConnection) -> Result<()> {
+    sqlx::query!(
+        "
+INSERT INTO organizations (slug, name)
+VALUES ($1, $2)
+",
+        slug,
+        name
+    )
+    .execute(conn)
+    .await?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_helper::Conn;
+
+    #[tokio::test]
+    #[ignore = "db not set up in CI, still useful as an example test"]
+    async fn gets_organizations() {
+        let mut conn = Conn::init().await;
+        let mut tx = conn.begin().await;
+        insert("slug", "org", tx.as_mut()).await.unwrap();
+        let orgs = all_organizations(tx.as_mut()).await.unwrap();
+        assert_eq!(orgs.len(), 1);
+    }
+}

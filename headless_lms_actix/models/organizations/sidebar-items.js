29b558e012e0005e17da0f703cb558f6e5166e0e
nygrenh <nygrenh@gmail.com>
2021-06-24 07:38:18 +0000

    Deploying to gh-pages from  @ 3bb5d3965808f8ce30ec1561c14caa26459961e4 🚀

--- headless_lms_actix/models/organizations/sidebar-items.js
+++ headless_lms_actix/models/organizations/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["all_organizations",""]],"struct":[["Organization",""]]});
+initSidebarItems({"fn":[["all_organizations",""],["insert",""]],"struct":[["Organization",""]]});
--- headless_lms_actix/sidebar-items.js
+++ headless_lms_actix/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["controllers","Handlers for HTTP requests to `/api/v0`."],["domain",""],["models","Functions and structs for interacting with the database."],["utils","Commonly used utils."]],"type":[["OAuthClient",""]]});
+initSidebarItems({"fn":[["configure",""]],"mod":[["controllers","Handlers for HTTP requests to `/api/v0`."],["domain",""],["models","Functions and structs for interacting with the database."],["utils","Commonly used utils."]],"type":[["OAuthClient",""]]});

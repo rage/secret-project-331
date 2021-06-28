7dced179f65b925e1a784259e58ed7fc5851ce6e
Sebastian <39335537+sebazai@users.noreply.github.com>
2021-06-28 10:25:09 +0300

    Unregister useless blocks (#126)
    * Unregister already registered blocks
    
    * Enhance extracting correct types
    
    * Add command to list block names
    
    * Document listing Gutenberg blocks

--- services/cms/src/components/GutenbergEditor.tsx
+++ services/cms/src/components/GutenbergEditor.tsx
@@ -33,7 +33,15 @@ import {
 } from "@wordpress/block-editor"
 import { Popover, SlotFillProvider } from "@wordpress/components"
 import { registerCoreBlocks } from "@wordpress/block-library"
-import { BlockInstance, registerBlockType } from "@wordpress/blocks"
+import {
+  BlockInstance,
+  getBlockType,
+  getBlockTypes,
+  registerBlockType,
+  unregisterBlockType,
+  /* @ts-ignore: type signature incorrect */
+  unregisterBlockVariation,
+} from "@wordpress/blocks"
 
 /**
  * Internal dependencies
@@ -41,6 +49,7 @@ import { BlockInstance, registerBlockType } from "@wordpress/blocks"
 import SerializeGutenbergModal from "./SerializeGutenbergModal"
 import DebugModal from "./DebugModal"
 import { blockTypeMap } from "../blocks"
+import { supportedCoreBlocks, allowedEmbedBlocks } from "../blocks/supportedGutenbergBlocks"
 
 interface GutenbergEditor {
   content: BlockInstance[]
@@ -60,7 +69,22 @@ const GutenbergEditor: React.FC<GutenbergEditor> = (props: GutenbergEditor) => {
   }
 
   useEffect(() => {
+    // Register all core blocks
     registerCoreBlocks()
+    // Register own blocks
+    // Unregister unwanted blocks
+    getBlockTypes().forEach((block) => {
+      if (supportedCoreBlocks.indexOf(block.name) === -1) {
+        unregisterBlockType(block.name)
+      }
+    })
+    /* @ts-ignore: type signature incorrect */
+    getBlockType("core/embed").variations.forEach((variation) => {
+      if (allowedEmbedBlocks.indexOf(variation.name) === -1) {
+        unregisterBlockVariation("core/embed", variation.name)
+      }
+    })
+
     blockTypeMap.forEach(([blockName, block]) => {
       registerBlockType(blockName, block)
     })

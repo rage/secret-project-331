23158d9d5433eec1ee0f8c4c74f022009409c6b8
heikki <heikki.pulli@helsinki.fi>
2021-06-29 16:32:54 +0300

    added not null constraints

--- services/headless-lms/migrations/20210629103218_email-delivery.up.sql
+++ services/headless-lms/migrations/20210629103218_email-delivery.up.sql
@@ -3,10 +3,10 @@ CREATE TABLE email_deliveries (
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
   deleted_at TIMESTAMP WITH TIME ZONE,
-  email_template_id UUID,
+  email_template_id UUID NOT NULL,
   error VARCHAR(255) DEFAULT NULL,
-  sent BOOLEAN DEFAULT FALSE,
-  user_id UUID
+  sent BOOLEAN DEFAULT FALSE NOT NULL,
+  user_id UUID NOT NULL
 );
 CREATE TRIGGER set_timestamp BEFORE
 UPDATE ON email_deliveries FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();

b4a183cd40c4f51d64f97f795f1dc3b50a0dfd8e
Jori Lampi <jori.lampi@helsinki.fi>
2021-06-29 17:37:55 +0300

    Exercise list on course management page

--- services/main-frontend/src/pages/manage/courses/[id]/index.tsx
+++ services/main-frontend/src/pages/manage/courses/[id]/index.tsx
@@ -9,6 +9,7 @@ import { useQuery } from "react-query"
 import { deleteCourse, getCourse } from "../../../../services/backend/courses"
 import { Dialog, Button } from "@material-ui/core"
 import UpdateCourseForm from "../../../../components/forms/UpdateCourseForm"
+import ExerciseList from "../../../../components/ExerciseList"
 
 const StatsPage: React.FC<unknown> = () => {
   const id = useQueryParameter("id")
@@ -59,6 +60,8 @@ const StatsPage: React.FC<unknown> = () => {
           </div>
         </Dialog>
       </div>
+      <h3>All exercises</h3>
+      <ExerciseList courseId={id} />
     </Layout>
   )
 }

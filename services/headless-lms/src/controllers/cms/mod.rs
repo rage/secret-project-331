9ab98fa823a37bbfc3eafaf966abcc6b89ea6964
Jori Lampi <jori.lampi@helsinki.fi>
2021-06-30 16:10:38 +0300

    Move exercises endpoint from cms to main_frontend

--- services/headless-lms/src/controllers/cms/mod.rs
+++ services/headless-lms/src/controllers/cms/mod.rs
@@ -7,20 +7,17 @@ This documents all endpoints. Select a module below for a category.
 
 pub mod chapters;
 pub mod courses;
-pub mod exercises;
 pub mod pages;
 
 use actix_web::web::{self, ServiceConfig};
 
 use self::{
-    chapters::_add_chapters_routes, courses::_add_courses_routes, exercises::_add_exercises_routes,
-    pages::_add_pages_routes,
+    chapters::_add_chapters_routes, courses::_add_courses_routes, pages::_add_pages_routes,
 };
 
 /// Add controllers from all the submodules.
 pub fn add_cms_routes(cfg: &mut ServiceConfig) {
     cfg.service(web::scope("/courses").configure(_add_courses_routes))
-        .service(web::scope("/exercises").configure(_add_exercises_routes))
         .service(web::scope("/pages").configure(_add_pages_routes))
         .service(web::scope("/chapters").configure(_add_chapters_routes));
 }

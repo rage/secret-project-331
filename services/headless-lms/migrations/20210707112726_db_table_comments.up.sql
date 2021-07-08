9fb59e3780aa02227ec6f6ebe20e4d503a94c59b
Henrik Nygren <nygrenh@gmail.com>
2021-07-08 11:59:43 +0300

    Update services/headless-lms/migrations/20210707112726_db_table_comments.up.sql

--- services/headless-lms/migrations/20210707112726_db_table_comments.up.sql
+++ services/headless-lms/migrations/20210707112726_db_table_comments.up.sql
@@ -3,7 +3,7 @@ COMMENT ON TABLE organizations IS 'A unit that organizes and manages courses e.g
 COMMENT ON TABLE courses IS 'Collection of course materials and exercises that define a online course.';
 COMMENT ON TABLE pages IS 'A web page that either contains course material or information about a course.  The content of the page is stored in the content field as as an array of blocks that have to be rendered before showing to users. Each page has a unique URL which can be customized if necessary.';
 COMMENT ON TABLE chapters IS 'Chapters divide pages of a course. They also have a front page.';
-COMMENT ON TABLE submissions IS 'Students submission table. data_json is the specific submission data from a exercise and grading_id refers to the gradings table and it tells how the submission is to be graded.';
+COMMENT ON TABLE submissions IS 'A returned exercise task. The system is oblivious to the structure of the submission. Does not contain anything related to points or grading.';
 COMMENT ON TABLE exercise_tasks IS 'Exercise tasks contains the specific exercise which the students completes. Exercise has a assignments field as JSONB which contains info about the tasks to be completed. Table also contains both private and public specs. Private spec tells conditions where exercise is correct and is not shown to the student. Public spec is shown to the student.';
 COMMENT ON TABLE course_instance_enrollments IS 'tells in which of the courses many instances certain user has enrolled to.';
 COMMENT ON TABLE users IS 'Either students, teachers or staff.';

dd5f19102f5b6805b3212167b47f19e0632b4247
Jori Lampi <jori.lampi@helsinki.fi>
2021-07-12 14:36:35 +0300

    Tweak migrations

--- services/headless-lms/migrations/20210707122257_chapter_image_url.down.sql
+++ services/headless-lms/migrations/20210707122257_chapter_image_url.down.sql
@@ -1,2 +1,2 @@
 -- Add down migration script here
-ALTER TABLE chapters DROP COLUMN IF EXISTS chapter_image_url;
+ALTER TABLE chapters DROP COLUMN chapter_image_url;
--- services/headless-lms/migrations/20210707122257_chapter_image_url.up.sql
+++ services/headless-lms/migrations/20210707122257_chapter_image_url.up.sql
@@ -1,3 +1,3 @@
 -- Add up migration script here
 ALTER TABLE chapters
-ADD COLUMN IF NOT EXISTS chapter_image_url varchar(255) DEFAULT NULL;
+ADD COLUMN chapter_image_url varchar(255);

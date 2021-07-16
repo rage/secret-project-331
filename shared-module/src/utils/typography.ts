3fe9ad4adb8035f537f638bf3120f952dc6b0214
iamgeorge <george.eyoyibo@polestar.com>
2021-07-16 09:02:55 +0300

    update Title stories

--- shared-module/src/utils/typography.ts
+++ shared-module/src/utils/typography.ts
@@ -2,12 +2,12 @@ export const primaryFont = '"Josefin Sans", sans-serif'
 export const secondaryFont = `"Roboto Mono", monospace`
 
 export const typography = {
-  h1: "clamp(40px, 8vw, 90px);",
-  h2: "clamp(32px, 6vw, 60px);",
-  h3: "clamp(26px, 4vw, 48px);",
-  h4: "clamp(24px, 3vw, 34px);",
-  h5: "clamp(22px, 2.4vw, 24px);",
-  h6: "clamp(18px, 2vw, 20px);",
+  h1: "clamp(40px, 8vw, 90px",
+  h2: "clamp(32px, 6vw, 60px)",
+  h3: "clamp(26px, 4vw, 48px)",
+  h4: "clamp(24px, 3vw, 34px)",
+  h5: "clamp(22px, 2.4vw, 24px)",
+  h6: "clamp(18px, 2vw, 20px)",
   paragraph: "1.1rem",
   helperText: "0.8rem",
   copyrightText: "0.7rem",

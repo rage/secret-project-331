0562aea51559589f562b42df8497105af5443bfc
iPegii <51372604+iPegii@users.noreply.github.com>
2021-07-16 10:40:50 +0300

    move other stuff to shared module Fixes #199

--- shared-module/src/utils/muiTheme.ts
+++ shared-module/src/utils/muiTheme.ts
@@ -1,18 +1,4 @@
 import { createMuiTheme } from "@material-ui/core/styles"
-import { Palette } from "@material-ui/core/styles/createPalette"
-import { Typography, TypographyOptions } from "@material-ui/core/styles/createTypography"
-import { ComponentsProps } from "@material-ui/core/styles/props"
-
-declare module "@material-ui/core/styles" {
-  interface Theme {
-    typography: Typography
-  }
-
-  interface ThemeOptions {
-    typography?: TypographyOptions | ((palette: Palette) => TypographyOptions) | undefined
-    props?: ComponentsProps | undefined
-  }
-}
 
 const muiTheme = createMuiTheme({
   typography: {

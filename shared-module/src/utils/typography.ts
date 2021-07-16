966cb8f749fa4b91c2777e3f3e00c69fcc8db901
iamgeorge <george.eyoyibo@polestar.com>
2021-07-16 08:55:56 +0300

    + Title component

--- shared-module/src/utils/typography.ts
+++ shared-module/src/utils/typography.ts
@@ -2,11 +2,12 @@ export const primaryFont = '"Josefin Sans", sans-serif'
 export const secondaryFont = `"Roboto Mono", monospace`
 
 export const typography = {
-  h1: "1.8rem",
-  h2: "1.6rem",
-  h3: "1.4rem",
-  h4: "1.2rem",
-  h5: "1.1rem",
+  h1: "clamp(40px, 8vw, 90px);",
+  h2: "clamp(32px, 6vw, 60px);",
+  h3: "clamp(26px, 4vw, 48px);",
+  h4: "clamp(24px, 3vw, 34px);",
+  h5: "clamp(22px, 2.4vw, 24px);",
+  h6: "clamp(18px, 2vw, 20px);",
   paragraph: "1.1rem",
   helperText: "0.8rem",
   copyrightText: "0.7rem",

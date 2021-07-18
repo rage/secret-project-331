9029d1c6525c5b382cd5b3631b7c7e63ebc36d74
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-07-18 11:28:20 +0300

    User e-mail

--- system-tests/src/setup/globalSetup.ts
+++ system-tests/src/setup/globalSetup.ts
@@ -3,9 +3,9 @@ import { login } from "../utils/login"
 // Create session states for each user, state will be named as username, e.g. admin.json
 async function globalSetup() {
   console.log("Creating login states for supported test users.")
-  await login("admin", "admin")
-  await login("teacher", "teacher")
-  await login("user", "user")
+  await login("admin@example.com", "admin")
+  await login("teacher@example.com", "teacher")
+  await login("user@example.com", "user")
 }
 
 export default globalSetup

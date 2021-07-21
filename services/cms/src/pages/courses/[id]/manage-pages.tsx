17c2411b8fe6a7aa5eac97691b5a11511f5c9a3f
iPegii <51372604+iPegii@users.noreply.github.com>
2021-07-21 10:43:06 +0300

    move componentStyles and constants

--- services/cms/src/pages/courses/[id]/manage-pages.tsx
+++ services/cms/src/pages/courses/[id]/manage-pages.tsx
@@ -15,9 +15,9 @@ import { Chapter } from "../../../shared-module/bindings"
 import DebugModal from "../../../shared-module/components/DebugModal"
 import { withSignedIn } from "../../../shared-module/contexts/LoginStateContext"
 import useQueryParameter from "../../../shared-module/hooks/useQueryParameter"
+import { normalWidthCenteredComponentStyles } from "../../../shared-module/styles/componentStyles"
 import { dontRenderUntilQueryParametersReady } from "../../../shared-module/utils/dontRenderUntilQueryParametersReady"
 import withErrorBoundary from "../../../shared-module/utils/withErrorBoundary"
-import { normalWidthCenteredComponentStyles } from "../../../styles/componentStyles"
 
 const CoursePages: React.FC<unknown> = () => {
   const id = useQueryParameter("id")

b26cfdaec93ab1da16a1cddfc0a77a8b81958b88
heikki <heikki.pulli@helsinki.fi>
2021-07-22 11:45:40 +0300

    better version

--- .husky/lint-staged.config.js
+++ .husky/lint-staged.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "*.{js,jsx,ts,tsx}": ["eslint --cache --fix", "stylelint --fix"],
-  "services/example-exercise/**/*.{js,jsx,ts,tsx}": ["bash -c 'cd services/example-exercise && npx tsc --noEmit'"],
-  "services/cms/**/*.{js,jsx,ts,tsx}": ["bash -c 'cd services/cms && npx tsc --noEmit'"],
-  "services/main-frontend/**/*.{js,jsx,ts,tsx}": ["bash -c 'cd services/main-frontend && npx tsc --noEmit'"],
-  "services/course-material/**/*.{js,jsx,ts,tsx}": ["bash -c 'cd services/course-material && npx tsc --noEmit'"],
+  "services/example-exercise/**/*.{js,jsx,ts,tsx}": ["npx tsc -p services/example-exercise/ --noEmit"],
+  "services/cms/**/*.{js,jsx,ts,tsx}": ["npx tsc -p services/cms/ --noEmit"],
+  "services/main-frontend/**/*.{js,jsx,ts,tsx}": ["npx tsc -p services/main-frontend/ --noEmit"],
+  "services/course-material/**/*.{js,jsx,ts,tsx}": ["npx tsc -p services/course-material/ --noEmit"],
   "*.{md,json,scss,css}": "prettier --write",
   "*.rs": () => [
     "cargo fmt --manifest-path services/headless-lms/Cargo.toml -- --files-with-diff",

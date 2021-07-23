72d994fa10f44b60aa1e85258e08723a4e9205e6
nygrenh <nygrenh@gmail.com>
2021-07-23 09:49:58 +0000

    Deploying to gh-pages from  @ 012a5517a3e136b7b9ae1a7dd0e4df8d74263b10 🚀

---
+++ headless_lms_actix/controllers/main_frontend/pages/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["_add_pages_routes","Add a route for each controller in this module."],["delete_page","DELETE `/api/v0/main-frontend/pages/:page_id` - Delete a page, related exercises, and related exercise tasks by id."],["post_new_page","POST `/api/v0/main-frontend/pages` - Create a new page."]]});

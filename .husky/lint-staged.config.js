7f4644cbb3745738717ada498dc819db794e92c3
Henrik Nygren <nygrenh@gmail.com>
2021-07-23 14:28:26 +0300

    Add a precommit hook for test.only

--- .husky/lint-staged.config.js
+++ .husky/lint-staged.config.js
@@ -1,15 +1,17 @@
 module.exports = {
   "*.{js,jsx,ts,tsx}": ["eslint --cache --fix", "stylelint --fix"],
-  "services/example-exercise/**/*.{js,jsx,ts,tsx}": () => "npx tsc -p services/example-exercise/ --noEmit",
+  "services/example-exercise/**/*.{js,jsx,ts,tsx}": () =>
+    "npx tsc -p services/example-exercise/ --noEmit",
   "services/cms/**/*.{js,jsx,ts,tsx}": () => "npx tsc -p services/cms/ --noEmit",
-  "services/main-frontend/**/*.{js,jsx,ts,tsx}": () => "npx tsc -p services/main-frontend/ --noEmit",
-  "services/course-material/**/*.{js,jsx,ts,tsx}": () => "npx tsc -p services/course-material/ --noEmit",
+  "services/main-frontend/**/*.{js,jsx,ts,tsx}": () =>
+    "npx tsc -p services/main-frontend/ --noEmit",
+  "services/course-material/**/*.{js,jsx,ts,tsx}": () =>
+    "npx tsc -p services/course-material/ --noEmit",
+  "system-tests/src/**/*.{js,jsx,ts,tsx}": () => "./bin/check-for-test-only",
   "*.{md,json,scss,css}": "prettier --write",
   "*.rs": () => [
     "cargo fmt --manifest-path services/headless-lms/Cargo.toml -- --files-with-diff",
     "cargo clippy --manifest-path services/headless-lms/Cargo.toml -- -D warnings",
   ],
-  "**/models/**/*.rs": () => [
-    "./services/headless-lms/bin/sqlx-prepare-check",
-  ],
+  "**/models/**/*.rs": () => ["./services/headless-lms/bin/sqlx-prepare-check"],
 }

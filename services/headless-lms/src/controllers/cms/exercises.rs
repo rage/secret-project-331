6e41fe8ac3f8187c5b7f73cb6b3fd489442dbbb6
heikki <heikki.pulli@helsinki.fi>
2021-07-26 12:34:44 +0300

    fetch single exercise and its tasks

--- services/headless-lms/src/controllers/cms/exercises.rs
+++ services/headless-lms/src/controllers/cms/exercises.rs
@@ -2,15 +2,18 @@ use actix_web::web::{self, Json, ServiceConfig};
 use sqlx::PgPool;
 use uuid::Uuid;
 
-use crate::{controllers::ControllerResult, models::exercises::Exercise};
+use crate::{controllers::ControllerResult, utils::document_schema_processor::GutenbergBlock};
 
 #[instrument(skip(pool))]
 async fn get_exercise_by_id(
     request_exercise_id: web::Path<Uuid>,
     pool: web::Data<PgPool>,
-) -> ControllerResult<Json<Exercise>> {
+) -> ControllerResult<Json<Vec<GutenbergBlock>>> {
     let mut conn = pool.acquire().await?;
-    let exercise = crate::models::exercises::get_by_id(&mut conn, *request_exercise_id).await?;
+    let exercise =
+        crate::models::exercises::get_denormalized_exercise_by_id(&mut conn, *request_exercise_id)
+            .await?;
+    format!("{:?}", exercise);
     Ok(Json(exercise))
 }
 

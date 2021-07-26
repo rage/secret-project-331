627fc2d9417b81ee415fb26f2e55f3be8ef66731
d471061c <cpastethat@gmail.com>
2021-07-26 15:33:24 +0300

    update test name

--- system-tests/src/tests/latex.spec.ts
+++ system-tests/src/tests/latex.spec.ts
@@ -6,7 +6,7 @@ test.use({
   storageState: "src/states/admin@example.com.json",
 })
 
-test("test", async ({ page }) => {
+test("latex-block renders", async ({ page }) => {
   // Go to http://project-331.local/
   await page.goto("http://project-331.local/")
   // Click text=University of Helsinki, Department of Mathematics and Statistics

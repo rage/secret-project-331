6458def0635e67a28e129953378640fbfc895609
d471061c <cpastethat@gmail.com>
2021-07-29 16:29:12 +0300

    update material-ui dependencies

--- shared-module/src/utils/muiTheme.ts
+++ shared-module/src/utils/muiTheme.ts
@@ -1,10 +1,13 @@
-import { createMuiTheme } from "@material-ui/core/styles"
+import { createTheme } from "@material-ui/core/styles"
 
-const muiTheme = createMuiTheme({
+import { primaryFont } from "./typography"
+
+const muiTheme = createTheme({
   typography: {
     button: {
       textTransform: "none",
     },
+    fontFamily: primaryFont,
   },
 })
 

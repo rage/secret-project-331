371b2b217938ac8a2858d247f1c508463504715b
nygrenh <nygrenh@gmail.com>
2021-08-02 07:41:08 +0000

    Deploying to gh-pages from  @ b75a94e114f15f57a54ad59a66f46ef188345015 🚀

--- headless_lms_actix/controllers/cms/email_templates/sidebar-items.js
+++ headless_lms_actix/controllers/cms/email_templates/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["_add_email_templates_routes","Add a route for each controller in this module."],["delete_email_template",""],["get_email_template",""],["update_email_template",""]]});
+initSidebarItems({"fn":[["_add_email_templates_routes","Add a route for each controller in this module."],["get_email_template",""],["update_email_template",""]]});

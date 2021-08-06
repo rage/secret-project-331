d925fc1696721d3887af148fd8a28e73a0b0c651
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-08-06 14:46:52 +0300

    Hard code heading 1 for main hero section

--- services/cms/src/blocks/HeroSection/HeroSectionEditor.tsx
+++ services/cms/src/blocks/HeroSection/HeroSectionEditor.tsx
@@ -1,12 +1,13 @@
-import { InnerBlocks } from "@wordpress/block-editor"
+import { InnerBlocks, RichText } from "@wordpress/block-editor"
 import { BlockEditProps, Template } from "@wordpress/blocks"
 import React from "react"
 
 import BlockWrapper from "../BlockWrapper"
 
+import { HeroSectionAttributes } from "."
+
 const ALLOWED_NESTED_BLOCKS = ["core/heading", "core/buttons", "core/paragraph"]
 const HERO_SECTION_TEMPLATE: Template[] = [
-  ["core/heading", { placeholder: "Welcome to...", level: 1, textAlign: "center" }],
   ["core/paragraph", { placeholder: "Insert short sales speech...", align: "center" }],
   [
     "core/buttons",
@@ -20,9 +21,21 @@ const HERO_SECTION_TEMPLATE: Template[] = [
   ],
 ]
 
-const HeroSectionEditor: React.FC<BlockEditProps<Record<string, never>>> = ({ clientId }) => {
+const HeroSectionEditor: React.FC<BlockEditProps<HeroSectionAttributes>> = ({
+  clientId,
+  attributes,
+  setAttributes,
+}) => {
+  const { heroHeading } = attributes
   return (
     <BlockWrapper id={clientId}>
+      <RichText
+        className="has-text-align-center wp-block-heading"
+        tagName="h1"
+        value={heroHeading}
+        onChange={(value: string) => setAttributes({ heroHeading: value })}
+        placeholder={"Welcome message for course..."}
+      />
       <InnerBlocks template={HERO_SECTION_TEMPLATE} allowedBlocks={ALLOWED_NESTED_BLOCKS} />
     </BlockWrapper>
   )
--- services/cms/src/blocks/HeroSection/index.tsx
+++ services/cms/src/blocks/HeroSection/index.tsx
@@ -3,12 +3,22 @@ import { BlockConfiguration } from "@wordpress/blocks"
 import HeroSectionEditor from "./HeroSectionEditor"
 import HeroSectionSave from "./HeroSectionSave"
 
-const HeroSectionConfiguration: BlockConfiguration = {
+export interface HeroSectionAttributes {
+  heroHeading: string
+}
+
+const HeroSectionConfiguration: BlockConfiguration<HeroSectionAttributes> = {
   title: "Hero Section",
   description:
     "A hero section is a full screen section typically consisting of a background image, or animations, with text and sometimes a call to action.",
   category: "design",
-  attributes: {},
+  attributes: {
+    heroHeading: {
+      type: "string",
+      source: "html",
+      selector: "h1",
+    },
+  },
   edit: HeroSectionEditor,
   save: HeroSectionSave,
 }

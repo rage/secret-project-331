6701af5882aa711848e45464641000bf675654da
Pekka <51372604+iPegii@users.noreply.github.com>
2021-08-06 11:33:30 +0300

    Fix image types missing (#295)
    * types to CMS
    
    * add types to all services

---
+++ services/cms/types/imageTypes.d.ts
@@ -0,0 +1,10 @@
+declare module "*.svg" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const content: any
+  export default content
+}
+declare module "*.png" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const content: any
+  export default content
+}

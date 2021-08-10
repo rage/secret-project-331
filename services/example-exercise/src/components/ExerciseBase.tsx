7490566fe7c1638b2fc07d8cb052f4ff33a85467
d471061c <cpastethat@gmail.com>
2021-08-10 13:31:30 +0300

    use borders instead

--- services/example-exercise/src/components/ExerciseBase.tsx
+++ services/example-exercise/src/components/ExerciseBase.tsx
@@ -1,7 +1,7 @@
 import { css } from "@emotion/css"
 
+import { baseTheme } from "../shared-module/utils/theme"
 import { PublicAlternative } from "../util/stateInterfaces"
-
 interface Props {
   alternatives: PublicAlternative[]
   selectedId: string | null
@@ -32,61 +32,51 @@ const ExerciseBase: React.FC<Props> = ({
       {alternatives.map((option) => {
         const selected = selectedId === option.id
         const correct = model_solutions.includes(option.id)
+        const green = baseTheme.colors.green[100]
+        const red = baseTheme.colors.red[100]
         return (
-          <div key={option.id}>
-            <button
-              className={
-                interactable
-                  ? css`
-                      width: 97%;
-                      display: inline;
-                      float: right;
-                      padding: 1rem 2rem;
-                      background-color: ${selected ? "#4210f5" : "#6188ff"};
-                      border-radius: 1rem;
-                      border: 0;
-                      color: white;
-                      transition: all 0.3s;
-                      cursor: pointer;
-                      margin-top: 0.5rem;
-                      margin-bottom: 0.5rem;
-                      &:hover {
-                        background-color: ${interactable
-                          ? selected
-                            ? "#330eb8"
-                            : "#507afb"
-                          : "#6188ff"};
-                      }
-                    `
-                  : css`
-                      display: inline;
-                      padding: 1rem 2rem;
-                      width: 97%;
-                      background-color: ${selected ? "#4210f5" : "#6188ff"};
-                      border-radius: 1rem;
-                      border: 0;
-                      color: white;
-                      margin-top: 0.5rem;
-                      margin-bottom: 0.5rem;
-                    `
-              }
-              aria-selected={selected}
-              onClick={() => onClick(option.id)}
-              key={option.id}
-            >
-              {option.name}
-            </button>
-            <div
-              className={css`
-                display: sticky;
-                height: 70px;
-                width: 8px;
-                float: right;
-                margin-right: 10px;
-                background-color: ${correct ? "green" : "red"};
-              `}
-            />
-          </div>
+          <button
+            className={
+              interactable
+                ? css`
+                    width: 97%;
+                    display: inline;
+                    float: right;
+                    padding: 1rem 2rem;
+                    background-color: ${selected ? "#4210f5" : "#6188ff"};
+                    border-radius: 1rem;
+                    border: 4px solid ${correct ? green : red};
+                    color: white;
+                    transition: all 0.3s;
+                    cursor: pointer;
+                    margin-top: 0.5rem;
+                    margin-bottom: 0.5rem;
+                    &:hover {
+                      background-color: ${interactable
+                        ? selected
+                          ? "#330eb8"
+                          : "#507afb"
+                        : "#6188ff"};
+                    }
+                  `
+                : css`
+                    display: inline;
+                    padding: 1rem 2rem;
+                    width: 97%;
+                    background-color: ${selected ? "#4210f5" : "#6188ff"};
+                    border-radius: 1rem;
+                    border: 4px solid ${correct ? green : red};
+                    color: white;
+                    margin-top: 0.5rem;
+                    margin-bottom: 0.5rem;
+                  `
+            }
+            aria-selected={selected}
+            onClick={() => onClick(option.id)}
+            key={option.id}
+          >
+            {option.name}
+          </button>
         )
       })}
     </div>

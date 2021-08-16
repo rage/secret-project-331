cf2146dcb93b5fa9bd7ce0a205c474b55747b035
Henrik Nygren <nygrenh@gmail.com>
2021-08-17 01:01:06 +0300

    Speech balloon component (#316)
    * Update dependencies
    
    * Add speech bubble component
    
    * Improve speech balloon
    
    * Finalize speechballoon

---
+++ shared-module/src/components/SpeechBalloon.tsx
@@ -0,0 +1,47 @@
+import { css } from "@emotion/css"
+import React from "react"
+
+import { baseTheme } from "../utils"
+
+export interface SpeechBalloonProps {
+  className?: string
+}
+
+const SQUARE_SIZE = "1rem"
+
+const SpeechBalloon: React.FC<SpeechBalloonProps> = ({ children, className }) => {
+  const bg = baseTheme.colors.neutral[300]
+  return (
+    <div
+      className={css`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: max-content;
+        ${className}
+      `}
+    >
+      <div
+        className={css`
+          background: ${bg};
+          width: max-content;
+          padding: 1rem;
+        `}
+      >
+        {children}
+      </div>
+      <div
+        className={css`
+          width: ${SQUARE_SIZE};
+          height: ${SQUARE_SIZE};
+          position: relative;
+          top: calc(-${SQUARE_SIZE} / 2);
+          background: ${bg};
+          transform: rotate(45deg);
+        `}
+      />
+    </div>
+  )
+}
+
+export default SpeechBalloon

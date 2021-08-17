e4b5a721aa45f7ddcab82044cfd2daba6ebf3483
d471061c <cpastethat@gmail.com>
2021-08-17 10:11:27 +0300

    add api to frontend

--- services/main-frontend/src/services/backend/exercise-services/index.tsx
+++ services/main-frontend/src/services/backend/exercise-services/index.tsx
@@ -5,3 +5,44 @@ export const fetchExerciseServices = async (): Promise<[ExerciseService]> => {
   const data = (await mainFrontendClient.get(`/exercise-services/`, { responseType: "json" })).data
   return data
 }
+
+export const fetchExerciseServiceById = async (
+  exercise_service_id: string,
+): Promise<ExerciseService> => {
+  const data = (
+    await mainFrontendClient.get(`/exercise-services/${exercise_service_id}`, {
+      responseType: "json",
+    })
+  ).data
+  return data
+}
+
+export const addExerciseService = async (
+  exercise_service: ExerciseService,
+): Promise<ExerciseService> => {
+  const data = (
+    await mainFrontendClient.post("/exercise-services", exercise_service, {
+      responseType: "json",
+    })
+  ).data
+  return data
+}
+
+export const deleteExerciseService = async (
+  exercise_service_id: string,
+): Promise<ExerciseService> => {
+  const data = (await mainFrontendClient.delete(`/exercise-services/${exercise_service_id}`)).data
+  return data
+}
+
+export const updateExerciseService = async (
+  exercise_service_id: string,
+  exercise_service: ExerciseService,
+): Promise<ExerciseService> => {
+  const data = (
+    await mainFrontendClient.put(`/exercise-services/${exercise_service_id}`, exercise_service, {
+      headers: { "Content-Type": "application/json" },
+    })
+  ).data
+  return data
+}

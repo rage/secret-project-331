00d4af493ed5bba4b9cbb1bbbd96fe602357b8db
d471061c <cpastethat@gmail.com>
2021-08-17 16:08:43 +0300

    switch to cards

--- services/main-frontend/src/pages/manage/services.tsx
+++ services/main-frontend/src/pages/manage/services.tsx
@@ -1,15 +1,8 @@
-import {
-  IconButton,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from "@material-ui/core"
-import InfoIcon from "@material-ui/icons/Info"
-import React from "react"
+import { css } from "@emotion/css"
+import { Box, Card, CardContent, CardHeader, IconButton, TextField } from "@material-ui/core"
+import DoneIcon from "@material-ui/icons/Done"
+import EditIcon from "@material-ui/icons/Edit"
+import React, { ChangeEvent, useState } from "react"
 import { useQuery } from "react-query"
 
 import Layout from "../../components/Layout"
@@ -23,51 +16,132 @@ interface ExerciseService {
   public_url: string
   internal_url: string
   max_reprocessing_submissions_at_once: number
+  updated_at: Date
+  created_at: Date
 }
 
 interface ExerciseServiceEditorProps {
   exercise_services: ExerciseService[]
 }
 
-const ExerciseServiceTable: React.FC<ExerciseServiceEditorProps> = ({ exercise_services }) => (
-  <TableContainer component={Paper}>
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>Name</TableCell>
-          <TableCell>Slug</TableCell>
-          <TableCell>Public URL</TableCell>
-          <TableCell>Internal URL</TableCell>
-          <TableCell>Reprocessing submissions</TableCell>
-          <TableCell></TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {exercise_services.map((exercise_service) => (
-          <TableRow key={exercise_service.id}>
-            <TableCell component="th" scope="row">
-              {exercise_service.name}
-            </TableCell>
-            <TableCell>{exercise_service.slug}</TableCell>
-            <TableCell>{exercise_service.public_url}</TableCell>
-            <TableCell>{exercise_service.internal_url}</TableCell>
-            <TableCell>{exercise_service.max_reprocessing_submissions_at_once}</TableCell>
-            <TableCell>
-              <IconButton component="span">
-                <InfoIcon />
-              </IconButton>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  </TableContainer>
+interface ExerciseServiceCardProps {
+  key: string
+  exercise_service: ExerciseService
+}
+
+interface ContentAreaProps {
+  title: string
+  text: string | number
+  editing: boolean
+  onChange(value: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): unknown
+}
+
+const ContentArea: React.FC<ContentAreaProps> = ({ title, text, editing, onChange }) => {
+  return (
+    <div
+      className={css`
+        margin-bottom: 4px;
+      `}
+    >
+      <strong>{title}:</strong>
+      <br />
+      {editing ? (
+        <TextField onChange={onChange} fullWidth value={text} placeholder={`${title}...`} />
+      ) : (
+        <span>{text}</span>
+      )}
+    </div>
+  )
+}
+
+const ExerciseServiceCard: React.FC<ExerciseServiceCardProps> = ({ key, exercise_service }) => {
+  const [editing, setEditing] = useState(false)
+  const [service, setService] = useState(exercise_service)
+
+  const toggleEdit = () => {
+    setEditing(!editing)
+  }
+
+  const onChange = (key) => (event) => {
+    setService({ ...service, [key]: event.target.value })
+  }
+
+  return (
+    <Box width={800}>
+      <Card key={key} variant="outlined">
+        <CardHeader
+          title={editing ? "Edit exercise service" : service.name}
+          subheader={editing || `Slug: ${service.slug}`}
+          action={
+            <IconButton onClick={toggleEdit}> {editing ? <DoneIcon /> : <EditIcon />}</IconButton>
+          }
+        />
+        <CardContent>
+          {editing && (
+            <>
+              <ContentArea
+                title={"Name"}
+                text={service.name}
+                editing={editing}
+                onChange={onChange("name")}
+              />
+              <ContentArea
+                title={"Slug"}
+                text={service.slug}
+                editing={editing}
+                onChange={onChange("slug")}
+              />
+            </>
+          )}
+          <ContentArea
+            title={"Public URL"}
+            text={service.public_url}
+            editing={editing}
+            onChange={onChange("public_url")}
+          />
+          <ContentArea
+            title={"Internal URL"}
+            text={service.internal_url}
+            editing={editing}
+            onChange={onChange("internal_url")}
+          />
+          <ContentArea
+            title={"Reprocessing submissions"}
+            text={service.max_reprocessing_submissions_at_once}
+            editing={editing}
+            onChange={onChange("max_reprocessing_submissions_at_once")}
+          />
+        </CardContent>
+
+        <CardContent>
+          <span>
+            <strong>Created:</strong>
+            <span> {service.created_at.toLocaleDateString("fi-FI")} </span>
+          </span>
+          <span
+            className={css`
+              float: right;
+            `}
+          >
+            <strong>Updated:</strong>
+            <span> {service.updated_at.toLocaleDateString("fi-FI")} </span>
+          </span>
+        </CardContent>
+      </Card>
+    </Box>
+  )
+}
+
+const ExerciseServiceContainer: React.FC<ExerciseServiceEditorProps> = ({ exercise_services }) => (
+  <div>
+    {exercise_services.map((service) => (
+      <ExerciseServiceCard key={service.id} exercise_service={service} />
+    ))}
+  </div>
 )
 
-const ExerciseServicePage: React.FC<ExerciseServiceEditorProps> = () => {
-  const { isLoading, error, data, refetch } = useQuery(`exercise-services`, () =>
-    fetchExerciseServices(),
-  )
+const ExerciseServicePage: React.FC = () => {
+  const { isLoading, error, data } = useQuery(`exercise-services`, () => fetchExerciseServices())
 
   if (error) {
     return <div>Error fetching exercise services.</div>
@@ -80,7 +154,7 @@ const ExerciseServicePage: React.FC<ExerciseServiceEditorProps> = () => {
   return (
     <Layout>
       <h1> Manage exercise services:</h1>
-      <ExerciseServiceTable exercise_services={data} />
+      <ExerciseServiceContainer exercise_services={data} />
       <br />
       <Button variant="primary" size="medium">
         Add new service

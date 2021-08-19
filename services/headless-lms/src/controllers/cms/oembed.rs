64ad74a7996ae6eac6c4b78f63a92fa75bcfa2a7
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-08-19 10:45:43 +0300

    Comment and auth user

--- services/headless-lms/src/controllers/cms/oembed.rs
+++ services/headless-lms/src/controllers/cms/oembed.rs
@@ -2,6 +2,7 @@ use std::time::Duration;
 
 use crate::controllers::ControllerError;
 use crate::controllers::ControllerResult;
+use crate::domain::authorization::AuthUser;
 use crate::utils::url_to_oembed_endpoint::url_to_oembed_endpoint;
 use actix_web::web::ServiceConfig;
 use actix_web::web::{self, Json};
@@ -15,8 +16,41 @@ pub struct OEmbedRequest {
 // Needed for Spotify oembed, should be fetched from env?
 static APP_USER_AGENT: &str = concat!("moocfi", "/", "0.1.0",);
 
-async fn get_oembed(
+/**
+GET `/api/v0/cms/oembed/preview?url=https://youtube.com/watch?v=123123123` - Fetch oembed response from correct provider.
+Endpoint for proxying oembed requests to correct provider using url query param.
+
+# Example
+
+Request:
+```http
+GET /api/v0/cms/oembed/preview?url=https://youtube.com/watch?v=123123123 HTTP/1.1
+Content-Type: text/html
+
+```
+
+Response:
+```json
+{
+    "title":"AUTHOR - Title (OFFICIAL)",
+    "author_name":"Author Name",
+    "author_url":"https://www.youtube.com/author",
+    "type":"video",
+    "height":439,
+    "width":780,
+    "version":"1.0",
+    "provider_name":"YouTube",
+    "provider_url":"https://www.youtube.com/",
+    "thumbnail_height":360,"thumbnail_width":480,
+    "thumbnail_url":"https://i.ytimg.com/vi/JWBo/hqdefault.jpg",
+    "html":"<iframe width=\"780\" height=\"439\" src=\"https://www.youtube.com/embed/JYjVo?feature=oembed\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"}
+}
+
+```
+*/
+async fn get_oembed_data_from_provider(
     query_params: web::Query<OEmbedRequest>,
+    _user: AuthUser,
 ) -> ControllerResult<Json<serde_json::Value>> {
     let endpoint = url_to_oembed_endpoint(query_params.url.to_string())?;
     let client = reqwest::Client::builder()
@@ -56,5 +90,5 @@ The name starts with an underline in order to appear before other functions in t
 We add the routes by calling the route method instead of using the route annotations because this method preserves the function signatures for documentation.
 */
 pub fn _add_oembed_routes(cfg: &mut ServiceConfig) {
-    cfg.route("/preview", web::get().to(get_oembed));
+    cfg.route("/preview", web::get().to(get_oembed_data_from_provider));
 }

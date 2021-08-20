44b05c1df66f7a77890103b32baa607d76017e80
Henrik Nygren <nygrenh@gmail.com>
2021-08-20 18:14:29 +0300

    Fix example exercise cors fonts

--- services/example-exercise/next.config.js
+++ services/example-exercise/next.config.js
@@ -2,6 +2,16 @@ const config = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      // This applcation is meant to be used with a sandboxed iframe.
+      // That causes that we need cors headers for fonts.
+      {
+        source: "*.woff2",
+        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
+      },
+    ]
+  },
 }
 
 if (process.env.BASE_PATH) {

5f517d7df571c2414dd911ee1f7754781eced466
George Misan <57463554+george-misan@users.noreply.github.com>
2021-08-22 10:15:28 +0300

    Exercise tracker (#327)
    * + circularProgressBar
    
    * update CircularProgressBar
    
    * removed unused code
    
    * remove unused code
    
    * remove unused code
    
    * fix build error

---
+++ shared-module/stories/ListCourseProgress.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, Story } from "@storybook/react"
+import React from "react"
+
+import ListCourseProgress from "../src/components/ListCourseProgress"
+
+export default {
+  title: "Components/ListCourseProgress",
+  component: ListCourseProgress,
+} as Meta
+
+const Component = ListCourseProgress
+
+type ComponentProps = React.ComponentProps<typeof Component>
+
+const Template: Story<ComponentProps> = (args: ComponentProps) => <Component {...args} />
+
+export const Simple: Story<ComponentProps> = Template.bind({})
+Simple.args = {
+  children: "ListCourseProgress",
+  chapterIndex: "I",
+  chapterTitle: "Introduction to everything",
+  selected: true,
+}

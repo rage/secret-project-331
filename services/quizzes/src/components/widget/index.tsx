1d0a6ab52fde7de5e3c5f6f7e835e30aaae3fd66
HegePI <19744083+HegePI@users.noreply.github.com>
2021-08-25 16:59:25 +0300

    Quizzes unsupported widget (#337)
    * playground
    
    * model solution added
    
    * removed playground
    
    * widget but everything is unsupported

---
+++ services/quizzes/src/components/widget/index.tsx
@@ -0,0 +1,51 @@
+import { PublicQuiz } from "../../types/types"
+import HeightTrackingContainer from "../HeightTrackingComponent"
+
+import Unsupported from "./Unsupported"
+
+interface WidgetProps {
+  quiz: PublicQuiz[]
+  port: MessagePort
+  maxWidth: number | null
+}
+
+type QuizItemType =
+  | "essay"
+  | "multiple-choice"
+  | "scale"
+  | "checkbox"
+  | "open"
+  | "custom-frontend-accept-data"
+
+const componentsByTypeNames = (typeName: QuizItemType) => {
+  const mapTypeToComponent = {
+    essay: Unsupported,
+    "multiple-choice": Unsupported,
+    scale: Unsupported,
+    checkbox: Unsupported,
+    open: Unsupported,
+    "custom-frontend-accept-data": Unsupported,
+    "multiple-choice-dropdown": Unsupported,
+    "clickable-multiple-choice": Unsupported,
+  }
+
+  return mapTypeToComponent[typeName]
+}
+
+const Widget: React.FC<WidgetProps> = ({ quiz, port }) => {
+  console.log(quiz)
+  return (
+    <div>
+      <HeightTrackingContainer port={port}>
+        {quiz[0].items
+          .sort((i1, i2) => i1.order - i2.order)
+          .map((i) => {
+            const Component = componentsByTypeNames(i.type as QuizItemType)
+            return <Component key={i.id} />
+          })}
+      </HeightTrackingContainer>
+    </div>
+  )
+}
+
+export default Widget

e49fa0973741da80f9c175abb76124b1aac565c3
Henrik Nygren <nygrenh@gmail.com>
2021-08-26 14:37:39 +0300

    Center the history page

--- services/main-frontend/src/pages/manage/pages/[id]/history.tsx
+++ services/main-frontend/src/pages/manage/pages/[id]/history.tsx
@@ -1,7 +1,9 @@
+import { css } from "@emotion/css"
 import { useRouter } from "next/router"
 
 import HistoryView from "../../../../components/HistoryView"
 import Layout from "../../../../components/Layout"
+import { normalWidthCenteredComponentStyles } from "../../../../shared-module/styles/componentStyles"
 import basePath from "../../../../shared-module/utils/base-path"
 
 const History: React.FC<unknown> = () => {
@@ -19,8 +21,14 @@ const History: React.FC<unknown> = () => {
 
   return (
     <Layout frontPageUrl={basePath()} navVariant="complex">
-      <h2>Page edit history</h2>
-      <HistoryView pageId={id} />
+      <div
+        className={css`
+          ${normalWidthCenteredComponentStyles}
+        `}
+      >
+        <h2>Page edit history</h2>
+        <HistoryView pageId={id} />
+      </div>
     </Layout>
   )
 }

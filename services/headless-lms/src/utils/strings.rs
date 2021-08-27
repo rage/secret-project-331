fce47194bfed256530fbe8617c2ffcb42cefbf42
Jori Lampi <jori.lampi@helsinki.fi>
2021-08-27 11:37:14 +0300

    Course language versions (#324)
    * Add locale to courses and user settings
    
    * Add language input to course form
    
    * Backend validation, rename locale to language code
    
    * WIP clone course for translation
    
    * Add copied_from fields to course related tables
    
    * Improve queries
    
    * Test that copying works
    
    * Region code underscore to hyphen
    
    * Remove user_course_settings to be reimplemented in next task
    
    * Fix stuff
    
    * Update exercise ids when copying
    
    * Refactor and documentation
    
    * Refactoring
    
    * Clippy suggestion after updating
    
    * Improve course creation form
    
    * Don't use update_page_content

--- services/headless-lms/src/utils/strings.rs
+++ services/headless-lms/src/utils/strings.rs
@@ -1,5 +1,12 @@
+use once_cell::sync::Lazy;
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
+use regex::Regex;
+
+static IETF_LANGUAGE_CODE_REGEX: Lazy<Regex> = Lazy::new(|| {
+    Regex::new(r"^[a-z]{2,3}(-[A-Z][a-z]{3})?-[A-Z]{2}$")
+        .expect("Invalid IETF language code regex.")
+});
 
 pub fn generate_random_string(length: usize) -> String {
     thread_rng()
@@ -7,4 +14,29 @@ pub fn generate_random_string(length: usize) -> String {
         .take(length)
         .map(char::from)
         .collect()
+}
+
+/// Checks whether the string is IETF language code where subtags are separated with underscore.
+pub fn is_ietf_language_code_like(string: &str) -> bool {
+    IETF_LANGUAGE_CODE_REGEX.is_match(string)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn ietf_language_code_validation_works() {
+        // Invalid scenarios
+        assert!(!is_ietf_language_code_like(""));
+        assert!(!is_ietf_language_code_like("en"));
+        assert!(!is_ietf_language_code_like("en_us"));
+        assert!(!is_ietf_language_code_like("en_US"));
+        assert!(!is_ietf_language_code_like("in-cans"));
+        assert!(!is_ietf_language_code_like("in-cans-ca"));
+
+        // Valid scenarios
+        assert!(is_ietf_language_code_like("en-US"));
+        assert!(is_ietf_language_code_like("in-Cans-CA"));
+    }
 }

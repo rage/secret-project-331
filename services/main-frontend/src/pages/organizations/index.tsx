dabc118013bb770648b8fd073b11eddb661280bb
d471061c <cpastethat@gmail.com>
2021-08-27 12:37:32 +0300

    add link to manage services to frontpage

--- services/main-frontend/src/pages/organizations/index.tsx
+++ services/main-frontend/src/pages/organizations/index.tsx
@@ -1,3 +1,8 @@
+import { css } from "@emotion/css"
+import { Container } from "@material-ui/core"
+import Link from "next/link"
+import React from "react"
+
 import Layout from "../../components/Layout"
 import OrganizationsList from "../../components/lists/OrganizationsList"
 import basePath from "../../shared-module/utils/base-path"
@@ -7,6 +12,36 @@ const Home: React.FC = () => {
   return (
     <Layout frontPageUrl={basePath()} navVariant="simple">
       <OrganizationsList />
+
+      {/* Temporal view for the exercise services */}
+      <h1
+        className={css`
+          text-align: center;
+          font-weight: 600;
+          font-size: 3em;
+          color: #707070;
+        `}
+      >
+        Services
+      </h1>
+      <Container maxWidth="lg">
+        <Link
+          href={{
+            pathname: `/manage/exercise-services`,
+          }}
+        >
+          <p
+            className={css`
+              cursor: pointer;
+              color: blue;
+              text-decoration: underline;
+            `}
+          >
+            {" "}
+            Manage exercise services{" "}
+          </p>
+        </Link>
+      </Container>
     </Layout>
   )
 }

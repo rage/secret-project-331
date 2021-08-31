eb82e161420fc31c2c79117fee8d258f8aa47bba
d471061c <cpastethat@gmail.com>
2021-08-31 10:22:40 +0300

    fix seed

--- services/headless-lms/src/bin/seed.rs
+++ services/headless-lms/src/bin/seed.rs
@@ -199,33 +199,6 @@ async fn main() -> Result<()> {
     )
     .await?;
 
-    let _example_exercise_exercise_service = exercise_services::insert_exercise_service(
-        &mut conn,
-        &exercise_services::ExerciseServiceNewOrUpdate {
-            name: "Example Exercise".to_string(),
-            slug: "example-exercise".to_string(),
-            public_url: "http://project-331.local/example-exercise/api/service-info".to_string(),
-            internal_url: Some("http://example-exercise.default.svc.cluster.local:3002/example-exercise/api/service-info".to_string()),
-            max_reprocessing_submissions_at_once: 5,
-        }
-    )
-    .await?;
-
-    exercise_services::insert_exercise_service(
-        &mut conn,
-        &exercise_services::ExerciseServiceNewOrUpdate {
-            name: "Quizzes".to_string(),
-            slug: "quizzes".to_string(),
-            public_url: "http://project-331.local/quizzes/api/service-info".to_string(),
-            internal_url: Some(
-                "http://quizzes.default.svc.cluster.local:3004/quizzes/api/service-info"
-                    .to_string(),
-            ),
-            max_reprocessing_submissions_at_once: 5,
-        },
-    )
-    .await?;
-
     // roles
     roles::insert(&mut conn, admin, None, None, UserRole::Admin).await?;
     roles::insert(&mut conn, teacher, Some(uh_cs), None, UserRole::Teacher).await?;

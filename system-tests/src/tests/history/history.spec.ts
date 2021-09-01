7c2ef0a114bde4fcacb1e5f817c9fe84c0c9f024
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-09-01 11:46:06 +0300

    Await?

--- system-tests/src/tests/history/history.spec.ts
+++ system-tests/src/tests/history/history.spec.ts
@@ -15,7 +15,7 @@ test("test", async ({ page, headless }) => {
   // Click text=University of Helsinki, Department of Computer Science
   await Promise.all([
     page.waitForNavigation(),
-    page.click("text=University of Helsinki, Department of Computer Science"),
+    await page.click("text=University of Helsinki, Department of Computer Science"),
   ])
   expect(page.url()).toBe(
     "http://project-331.local/organizations/8bb12295-53ac-4099-9644-ac0ff5e34d92",

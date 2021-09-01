62c2690efb26eddd9e5ccaff43f89cde35975c11
HegePI <19744083+HegePI@users.noreply.github.com>
2021-09-01 10:33:39 +0000

    Deploying to gh-pages from  @ 3b128ae60543ee918cba163ac1950304daf1d983 🚀

---
+++ headless_lms_actix/models/playground_examples/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["delete_playground_example",""],["get_all_playground_examples",""],["insert_playground_example",""],["update_playground_example",""]],"struct":[["PlaygroundExample",""],["PlaygroundExampleData",""]]});

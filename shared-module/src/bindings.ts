d5122cea78089cf0e869eefdad1d3fbe323e46da
d471061c <cpastethat@gmail.com>
2021-09-09 11:57:01 +0300

    Exercise service page (#314)
    * add exercise-services api to backend
    
    * link service management page
    
    * load exercise data
    
    * add api to frontend
    
    * switch to cards
    
    * update exercise services
    
    * update dates
    
    * add deletion and update speech bubbles
    
    * remove default height and fix overflow
    
    * add dialog for creating service
    
    * add field validation
    
    * fix validation
    
    * extract validation to utils
    
    * refactor time component
    
    * remove unused comment
    
    * update time.js
    
    * update change event on name
    
    * add to container
    
    * move time component to shared-module
    
    * add link to manage services to frontpage
    
    * update time and component
    
    * add materil-ui icons to quizzes
    
    * Update services/main-frontend/src/pages/organizations/index.tsx
    
    Co-authored-by: Henrik Nygren <nygrenh@gmail.com>
    
    * merge master with fixes
    
    * add initial tests for exercise services
    
    * switch container
    
    * fix seed
    
    * update sqlx-data.json
    
    * Update tests
    
    * update snapshots for exercise service
    
    * replace dates with format in test
    
    * fix speech balloon positioning
    
    * add database migrations
    
    * fix migration
    
    * update snapshots
    
    * update snapshots
    
    * Update snapshots
    
    * update snapshots
    
    * fix snapshots
    
    Co-authored-by: Henrik Nygren <nygrenh@gmail.com>

--- shared-module/src/bindings.ts
+++ shared-module/src/bindings.ts
@@ -136,6 +136,26 @@ export interface SubmissionResult {
   grading: Grading
 }
 
+export interface ExerciseService {
+  id: string
+  created_at: Date
+  updated_at: Date
+  deleted_at: Date | null
+  name: string
+  slug: string
+  public_url: string
+  internal_url: string | null
+  max_reprocessing_submissions_at_once: number
+}
+
+export interface ExerciseServiceNewOrUpdate {
+  name: string
+  slug: string
+  public_url: string
+  internal_url: string | null
+  max_reprocessing_submissions_at_once: number
+}
+
 export interface Course {
   id: string
   slug: string

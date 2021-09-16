1b625913a14dd80aae6fc77df17b3edccb20bb2e
sebazai <39335537+sebazai@users.noreply.github.com>
2021-09-16 13:12:58 +0000

    Deploying to gh-pages from  @ dde409943bb8ee3f5162d5a82c811523c71fe644 🚀

--- headless_lms_actix/models/organizations/sidebar-items.js
+++ headless_lms_actix/models/organizations/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["all_organizations",""],["insert",""]],"struct":[["DatabaseOrganization",""],["Organization",""]]});
+initSidebarItems({"fn":[["all_organizations",""],["get_organization",""],["insert",""],["update_organization_image_path",""]],"struct":[["DatabaseOrganization",""],["Organization",""]]});

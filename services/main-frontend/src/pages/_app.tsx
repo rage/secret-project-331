0f70e4392006fad10f848e18f160ecd97e7e8607
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-09-21 10:56:03 +0300

    Remove MuITheme add inherit important to override Material UI Font

--- services/main-frontend/src/pages/_app.tsx
+++ services/main-frontend/src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { ThemeProvider } from "@material-ui/styles"
 import type { AppProps } from "next/app"
 import React from "react"
 import { QueryClientProvider } from "react-query"
@@ -8,7 +7,6 @@ import { RecoilRoot } from "recoil"
 import { LoginStateContextProvider } from "../shared-module/contexts/LoginStateContext"
 import { queryClient } from "../shared-module/services/appQueryClient"
 import GlobalStyles from "../shared-module/styles/GlobalStyles"
-import muiTheme from "../shared-module/styles/muiTheme"
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   React.useEffect(() => {
@@ -22,14 +20,12 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
-        <ThemeProvider theme={muiTheme}>
-          {/* <Devtools /> */}
-          <GlobalStyles />
-          <LoginStateContextProvider>
-            <Component {...pageProps} />
-          </LoginStateContextProvider>
-          <ReactQueryDevtools initialIsOpen={false} />
-        </ThemeProvider>
+        {/* <Devtools /> */}
+        <GlobalStyles />
+        <LoginStateContextProvider>
+          <Component {...pageProps} />
+        </LoginStateContextProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
       </RecoilRoot>
     </QueryClientProvider>
   )

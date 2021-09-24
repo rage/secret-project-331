6c0007fa49b85273baacd87d92b8d1378ff15cbf
Henrik Nygren <nygrenh@gmail.com>
2021-09-24 13:50:26 +0300

    Fixes (#394)

--- shared-module/src/styles/muiTheme.ts
+++ shared-module/src/styles/muiTheme.ts
@@ -1,21 +1,14 @@
 import { createTheme } from "@material-ui/core"
 
-export const theme = createTheme({
+import { primaryFont } from "./typography"
+
+const muiTheme = createTheme({
   typography: {
-    fontFamily: "Lato",
-  },
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: `
-        @font-face {
-          font-family: 'Lato';
-          font-style: normal;
-          font-display: swap;
-          font-weight: 400;
-          src: url('../../node_modules/@fontsource/lato/files/lato-latin-ext-400-normal.woff2') format('woff2'), url('../../node_modules/@fontsource/lato/files/lato-all-400-normal.woff') format('woff');
-          unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
-        }
-      `,
+    fontFamily: primaryFont,
+    button: {
+      textTransform: "none",
     },
   },
 })
+
+export default muiTheme

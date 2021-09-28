f1ff5ac4b8f40794a78fac8609d5634d01eee86a
heikki <heikki.pulli@helsinki.fi>
2021-09-28 14:48:29 +0300

    essay tests

---
+++ system-tests/src/tests/quizzes/widget/essay-widget.spec.ts
@@ -0,0 +1,58 @@
+import { test } from "@playwright/test"
+
+import expectScreenshotsToMatchSnapshots from "../../../utils/screenshot"
+import waitForFunction from "../../../utils/waitForFunction"
+
+test.use({
+  storageState: "src/states/teacher@example.com.json",
+})
+
+test("widget, essay", async ({ page, headless }) => {
+  // Go to http://project-331.local/playground
+  await page.goto("http://project-331.local/playground")
+
+  // Click div[role="button"]:has-text("")
+  await page.click('div[role="button"]:has-text("​")')
+
+  // Click text=Quizzes example, scale
+  await page.click("text=Quizzes example, essay")
+
+  const frame = await waitForFunction(page, () =>
+    page.frames().find((f) => {
+      return f.url().startsWith("http://project-331.local/quizzes/exercise?width=500")
+    }),
+  )
+
+  await expectScreenshotsToMatchSnapshots({
+    headless,
+    snapshotName: "widget-scale",
+    waitForThisToBeVisibleAndStable: [`text="Of the lamps of Fëanor"`],
+    frame,
+  })
+})
+
+test("widget, essay with an answer", async ({ page, headless }) => {
+  // Go to http://project-331.local/playground
+  await page.goto("http://project-331.local/playground")
+
+  // Click div[role="button"]:has-text("")
+  await page.click('div[role="button"]:has-text("​")')
+
+  // Click text=Quizzes example, scale
+  await page.click("text=Quizzes example, essay")
+
+  const frame = await waitForFunction(page, () =>
+    page.frames().find((f) => {
+      return f.url().startsWith("http://project-331.local/quizzes/exercise?width=500")
+    }),
+  )
+
+  await frame.fill("#Answer", "I think I enrolled in the wrong course XD")
+
+  await expectScreenshotsToMatchSnapshots({
+    headless,
+    snapshotName: "widget-scale",
+    waitForThisToBeVisibleAndStable: [`text="Of the lamps of Fëanor"`],
+    frame,
+  })
+})

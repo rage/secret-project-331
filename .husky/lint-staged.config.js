99b2eaa4079729420cd9a513158a99728f20b377
Sebastian <39335537+sebazai@users.noreply.github.com>
2021-10-04 17:06:09 +0300

    Enable npx tsc for microservices if changes in shared-modules src or types (#409)

--- .husky/lint-staged.config.js
+++ .husky/lint-staged.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   "*.{js,jsx,ts,tsx}": ["eslint --cache --fix", "stylelint --fix"],
-  "services/example-exercise/**/*.{js,jsx,ts,tsx}": () => "npx tsc -p services/example-exercise/ --noEmit",
-  "services/cms/**/*.{js,jsx,ts,tsx}": () => "npx tsc -p services/cms/ --noEmit",
-  "services/main-frontend/**/*.{js,jsx,ts,tsx}": () => "npx tsc -p services/main-frontend/ --noEmit",
-  "services/course-material/**/*.{js,jsx,ts,tsx}": () => "npx tsc -p services/course-material/ --noEmit",
-  "services/quizzes/**/*.{js,jsx,ts,tsx}": () => "npx tsc -p services/quizzes/ --noEmit",
+  "(services/example-exercise/|shared-module/src/)**/*.{js,jsx,ts,tsx}": () => "npx tsc -p services/example-exercise/ --noEmit",
+  "(services/cms/|shared-module/src/)**/*.{js,jsx,ts,tsx}": () => "npx tsc -p services/cms/ --noEmit",
+  "(services/main-frontend/|shared-module/src/)**/*.{js,jsx,ts,tsx}": () => "npx tsc -p services/main-frontend/ --noEmit",
+  "(services/course-material/|shared-module/src/)**/*.{js,jsx,ts,tsx}": () => "npx tsc -p services/course-material/ --noEmit",
+  "(services/quizzes/|shared-module/src/)**/*.{js,jsx,ts,tsx}": () => "npx tsc -p services/quizzes/ --noEmit",
   "*.{md,json,scss,css}": "prettier --write",
   "*.rs": () => [
     "cargo fmt --manifest-path services/headless-lms/Cargo.toml -- --files-with-diff",

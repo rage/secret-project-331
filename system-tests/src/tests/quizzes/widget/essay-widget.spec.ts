e4fdaa1baf299c7dad7340f1456b368496c2704d
heikki <heikki.pulli@helsinki.fi>
2021-10-07 12:17:59 +0300

    essay test improvements

--- system-tests/src/tests/quizzes/widget/essay-widget.spec.ts
+++ system-tests/src/tests/quizzes/widget/essay-widget.spec.ts
@@ -38,7 +38,10 @@ test("widget, essay", async ({ page, headless }) => {
   await expectScreenshotsToMatchSnapshots({
     headless,
     snapshotName: "widget-essay-answered",
-    waitForThisToBeVisibleAndStable: [`text="Of the lamps of Fëanor"`],
+    waitForThisToBeVisibleAndStable: [
+      `text="Of the lamps of Fëanor"`,
+      `text="I think I enrolled in the wrong course XD"`,
+    ],
     frame,
   })
 })

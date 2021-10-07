2d670c3e78bbedb60c9d80e6e863249a72f51f9c
heikki <heikki.pulli@helsinki.fi>
2021-10-07 10:30:30 +0300

    essay, single test

--- system-tests/src/tests/quizzes/widget/essay-widget.spec.ts
+++ system-tests/src/tests/quizzes/widget/essay-widget.spec.ts
@@ -29,23 +29,6 @@ test("widget, essay", async ({ page, headless }) => {
     waitForThisToBeVisibleAndStable: [`text="Of the lamps of Fëanor"`],
     frame,
   })
-})
-
-test("widget, essay with an answer", async ({ page, headless }) => {
-  // Go to http://project-331.local/playground
-  await page.goto("http://project-331.local/playground")
-
-  // Click div[role="button"]:has-text("")
-  await page.click('div[role="button"]:has-text("​")')
-
-  // Click text=Quizzes example, scale
-  await page.click("text=Quizzes example, essay")
-
-  const frame = await waitForFunction(page, () =>
-    page.frames().find((f) => {
-      return f.url().startsWith("http://project-331.local/quizzes/exercise?width=500")
-    }),
-  )
 
   await frame.fill(
     `textarea:below(:text("Word count"))`,

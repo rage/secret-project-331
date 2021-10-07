0f81c7131beca4aff8816dd343fc6c171777001a
HegePI <19744083+HegePI@users.noreply.github.com>
2021-10-07 10:28:52 +0300

    widget multiple-choice-dropdown (#410)
    * multiple-choice-dropdown
    
    * butter tests
    
    * snapshots updated
    
    * correct snapshots
    
    * different snapshot name
    
    * removed clog
    
    * history view snapshots
    
    * single test
    
    * valid field added
    
    * history snapshots restored

---
+++ system-tests/src/tests/quizzes/widget/multiple-choice-dropdown.spec.ts
@@ -0,0 +1,53 @@
+import { test } from "@playwright/test"
+
+import expectScreenshotsToMatchSnapshots from "../../../utils/screenshot"
+import waitForFunction from "../../../utils/waitForFunction"
+
+test.use({
+  storageState: "src/states/teacher@example.com.json",
+})
+
+test("widget, multiple-choice-dropdown screenshot test", async ({ page, headless }) => {
+  // Go to http://project-331.local/
+  await page.goto("http://project-331.local/playground")
+
+  // Click text=University of Helsinki, Department of Computer Science
+
+  await page.click('div[role="button"]:has-text("​")')
+  // Click text=Quizzes example, multiple-choice
+  await page.click("text=Quizzes example, multiple-choice dropdown")
+
+  const frame = await waitForFunction(page, () =>
+    page.frames().find((f) => {
+      return f.url().startsWith("http://project-331.local/quizzes/exercise?width=500")
+    }),
+  )
+
+  await expectScreenshotsToMatchSnapshots({
+    headless,
+    snapshotName: "widget-multiple-choice-dropdown",
+    waitForThisToBeVisibleAndStable: `text="How many different CSS hexadecimal color codes there are?"`,
+    frame,
+  })
+
+  await frame.selectOption(
+    `select:right-of(:text("How many different CSS hexadecimal color codes there are?"))`,
+    { label: "at least two" },
+  )
+
+  await frame.selectOption(
+    `select:right-of(:text("What other ways there are to represent colors in CSS?"))`,
+    { label: "RGB -color system" },
+  )
+
+  await expectScreenshotsToMatchSnapshots({
+    headless,
+    snapshotName: "widget-multiple-choice-dropdown-answered",
+    waitForThisToBeVisibleAndStable: [
+      `text="How many different CSS hexadecimal color codes there are?"`,
+      `text="How many different CSS hexadecimal color codes there are?"`,
+      `text="What other ways there are to represent colors in CSS?"`,
+    ],
+    frame,
+  })
+})

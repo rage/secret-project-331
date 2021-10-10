e74cb37bf06e2b9a31f95fc0a7fcfe2e9c57f409
george-misan <george.eyoyibo@gmail.com>
2021-10-10 20:32:52 +0300

    + Cards component & its dependencies to course-material

---
+++ services/course-material/src/components/Cards/Confetti.tsx
@@ -0,0 +1,166 @@
+import styled from "@emotion/styled"
+import React from "react"
+import Particles from "react-tsparticles"
+import type { Container, ISourceOptions, Main } from "tsparticles"
+
+import Tick from "../img/tick.svg"
+
+const StyledDiv = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100% !important;
+  height: 100% !important;
+
+  #tsparticles {
+    height: 100% !important;
+  }
+`
+
+const Confetti: React.FC<any> = () => {
+  const particlesInit = (main: Main) => {
+    console.log(main)
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  }
+
+  const particlesLoaded = (container: Container) => {
+    console.log(container)
+  }
+
+  return (
+    <StyledDiv>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "transparent",
+            },
+          },
+          interactivity: {
+            detectsOn: "window",
+          },
+          emitters: {
+            position: {
+              x: 50,
+              y: 100,
+            },
+            rate: {
+              quantity: 10,
+              delay: 0.25,
+            },
+          },
+          particles: {
+            color: {
+              value: ["#7b5cff", "#6245e0", "#b3c7ff", "#8fa5e5", "#5c86ff", "#345dd1"],
+            },
+            move: {
+              decay: 0.05,
+              direction: "top",
+              enable: true,
+              gravity: {
+                enable: true,
+                maxSpeed: 140,
+              },
+              outModes: {
+                top: "none",
+                default: "destroy",
+              },
+              speed: { min: 25, max: 50 },
+            },
+            number: {
+              value: 0,
+            },
+            opacity: {
+              value: 1,
+            },
+            rotate: {
+              value: {
+                min: 0,
+                max: 180,
+              },
+              direction: "random",
+              animation: {
+                enable: true,
+                speed: 30,
+              },
+            },
+            tilt: {
+              direction: "random",
+              enable: true,
+              value: {
+                min: 0,
+                max: 360,
+              },
+              animation: {
+                enable: true,
+                speed: 60,
+              },
+            },
+            size: {
+              value: 7,
+            },
+            roll: {
+              darken: {
+                enable: true,
+                value: 25,
+              },
+              enable: true,
+              speed: {
+                min: 5,
+                max: 15,
+              },
+            },
+            wobble: {
+              distance: 30,
+              enable: true,
+              speed: {
+                min: -7,
+                max: 7,
+              },
+            },
+            shape: {
+              type: ["rectangle", "rectangle", "square", "square"],
+              options: {
+                image: [
+                  {
+                    src: Tick,
+                    width: 32,
+                    height: 32,
+                    particles: {
+                      size: {
+                        value: 16,
+                      },
+                    },
+                  },
+                ],
+                polygon: [
+                  {
+                    sides: 5,
+                  },
+                  {
+                    sides: 6,
+                  },
+                ],
+                character: [
+                  {
+                    fill: true,
+                    font: "Verdana",
+                    value: ["⛛", "⚪", "⚡", "⚈", "⭐️", "✖"],
+                    style: "",
+                    weight: 400,
+                  },
+                ],
+              },
+            },
+          },
+        }}
+      />
+    </StyledDiv>
+  )
+}
+
+export default Confetti

a0910a96e797224ace126c5c4fbf983889010bfc
d471061c <cpastethat@gmail.com>
2021-10-14 10:39:57 +0300

    Embedded blocks (#379)
    * add twitter and spotify
    
    * set iframes to sandbox
    
    * Update sandbox
    
    * disable unimplemented blocks

--- services/course-material/src/components/ContentRenderer/EmbedBlock.tsx
+++ services/course-material/src/components/ContentRenderer/EmbedBlock.tsx
@@ -6,7 +6,7 @@ import { EmbedAttributes } from "../../types/GutenbergBlockAttributes"
 
 import { BlockRendererProps } from "."
 
-const YoutubeEmbddedBlock: React.FC<EmbedAttributes> = (props) => {
+const YoutubeEmbeddedBlock: React.FC<EmbedAttributes> = (props) => {
   const { url } = props
   const video = url?.split("v=")[1]
 
@@ -14,6 +14,7 @@ const YoutubeEmbddedBlock: React.FC<EmbedAttributes> = (props) => {
     <iframe
       src={`https://www.youtube.com/embed/${video}`}
       title="YouTube video player"
+      sandbox="allow-scripts allow-same-origin"
       frameBorder="0"
       allowFullScreen
       className={css`
@@ -25,13 +26,66 @@ const YoutubeEmbddedBlock: React.FC<EmbedAttributes> = (props) => {
   )
 }
 
+const SpotifyEmbeddedBlock: React.FC<EmbedAttributes> = (props) => {
+  const url = props.url ?? ""
+  const content = url.split("/")
+  const type = content[content.length - 2]
+  const spotifyId = content[content.length - 1]
+
+  return (
+    <iframe
+      src={`https://open.spotify.com/embed/${type}/${spotifyId}`}
+      frameBorder="0"
+      title="Spotify"
+      sandbox="allow-scripts allow-same-origin"
+      allowTransparency={true}
+      allow="encrypted-media"
+      className={css`
+        display: block;
+        width: 768px;
+        height: 576px;
+      `}
+    ></iframe>
+  )
+}
+
+const TwitterEmbeddedBlock: React.FC<EmbedAttributes> = (props) => {
+  const blob = new Blob(
+    [
+      `
+      <blockquote class="twitter-tweet">
+        <a href="${props.url}">Tweet</a>
+      </blockquote>
+      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+      `,
+    ],
+    { type: "text/html" },
+  )
+
+  return (
+    <iframe
+      src={window.URL.createObjectURL(blob)}
+      title="Tweet"
+      sandbox="allow-scripts allow-same-origin"
+      frameBorder="0"
+      className={css`
+        display: block;
+        width: 768px;
+        height: 576px;
+      `}
+    />
+  )
+}
+
 const EmbedBlock: React.FC<BlockRendererProps<EmbedAttributes>> = (props) => {
   const { data } = props
   const type = data.attributes.providerNameSlug
 
   return (
     <div className={courseMaterialCenteredComponentStyles}>
-      {type === "youtube" && <YoutubeEmbddedBlock {...props.data.attributes} />}
+      {type === "youtube" && <YoutubeEmbeddedBlock {...props.data.attributes} />}
+      {type === "twitter" && <TwitterEmbeddedBlock {...props.data.attributes} />}
+      {type === "spotify" && <SpotifyEmbeddedBlock {...props.data.attributes} />}
     </div>
   )
 }

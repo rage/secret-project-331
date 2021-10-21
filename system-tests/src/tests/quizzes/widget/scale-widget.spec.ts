b1ae04181b158e89b763653927650e6ca0666bbb
HegePI <19744083+HegePI@users.noreply.github.com>
2021-10-21 15:12:57 +0300

    playground select refactor (#441)
    * playground select refactor
    
    * select
    
    * Test fix
    
    Co-authored-by: Henrik Nygren <nygrenh@gmail.com>

--- system-tests/src/tests/quizzes/widget/scale-widget.spec.ts
+++ system-tests/src/tests/quizzes/widget/scale-widget.spec.ts
@@ -11,11 +11,8 @@ test("widget, scale", async ({ page, headless }) => {
   // Go to http://project-331.local/playground
   await page.goto("http://project-331.local/playground")
 
-  // Click div[role="button"]:has-text("")
-  await page.click('div[role="button"]:has-text("​")')
-
   // Click text=Quizzes example, scale
-  await page.click("text=Quizzes example, scale")
+  await page.selectOption("select", { label: "Quizzes example, scale" })
 
   const frame = await waitForFunction(page, () =>
     page.frames().find((f) => {

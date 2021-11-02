3c6de6fc1418f24e41460ee827d2ca60cedeb99e
Henrik Nygren <nygrenh@gmail.com>
2021-11-02 12:33:38 +0200

    Fix wait for db

--- services/headless-lms/wait-for-db.sh
+++ services/headless-lms/wait-for-db.sh
@@ -7,8 +7,11 @@ if [ -z ${DATABASE_URL+x} ]; then
     exit 1
 fi
 
-echo "Waiting until I psql can connect to the database..."
-until test "$(psql "echo \"$DATABASE_URL\" | sed 's/headless_lms_/database_that_should_not_exist_/'" -c 'SELECT 83256;' --csv | grep -c 'database_that_should_not_exist_')" -eq 1 2> /dev/null; do
+# Use a database name that does not exist in the target postgres so that we can wait until we get the database does not exist error from postres.
+NON_EXISTING_DB_URL="${DATABASE_URL//headless_lms_/database_that_should_not_exist_}"
+echo "Waiting until psql can connect to the database..."
+# Succeeeds when we get `psql: error: FATAL:  database "database_that_should_not_exist_dev" does not exist`
+until test "$( (timeout 5 psql "$NON_EXISTING_DB_URL" -c 'SELECT 83256;' --csv 2>&1 || true) | grep -c 'database_that_should_not_exist_')" -eq 1 2> /dev/null; do
   echo -n "."
   sleep 1
 done

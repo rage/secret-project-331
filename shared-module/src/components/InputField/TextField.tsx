e84e66aa5a6e6b63dc3ecdab98c53354dab4dc73
george-misan <george.eyoyibo@gmail.com>
2021-11-09 10:19:08 +0200

    complete TextField logic

--- shared-module/src/components/InputField/TextField.tsx
+++ shared-module/src/components/InputField/TextField.tsx
@@ -1,27 +1,73 @@
-import React from 'react'
-import styled from '@emotion/styled'
+import { css, cx, keyframes } from "@emotion/css"
+import styled from "@emotion/styled"
+import React from "react"
+
+import { headingFont, primaryFont } from "../../styles"
 
 interface TextFieldExtraProps {
   type?: "email" | "password" | "text"
   label: string
   hint?: string
-  errorMessage?: string
+  error?: string
   placeholder?: string
   required?: boolean
   value?: string
-/*   onBlur?: (name?:string) => void */
-  onChange: (value:string, name?:string) => void
+  /*   onBlur?: (name?:string) => void */
+  onChange: (value: string, name?: string) => void
 }
+
+const Input = styled.input`
+  background: #fcfcfc;
+  border-width: 1.6px;
+  border-style: solid;
+  border-radius: 3px;
+  border-color: ${({ error }) => (error ? "#F76D82" : "#dedede")};
+  padding: 4px 12px;
+  transition: ease-in-out, width 0.35s ease-in-out;
+  outline: none;
+  min-width: 280px;
+  display: block;
+
+  &:focus,
+  &:active {
+    border-color: #55b3f5;
+  }
+`
+const label = css`
+  color: #333;
+  font-family: ${primaryFont};
+  font-weight: 600;
+  font-size: 12px;
+  opacity: 0.7;
+  display: inline-block;
+  margin-bottom: 2px;
+`
+
+const error = css`
+  color: #f76d82;
+  font-size: 12px;
+  display: inline-block;
+  margin-top: -10px;
+`
+
+const Wrapper = styled.div``
 
 export type TextFieldProps = React.HTMLAttributes<HTMLInputElement> & TextFieldExtraProps
 
-const TextField = ({onChange, ...rest}: TextFieldExtraProps) => {
-
+const TextField = ({ onChange, ...rest }: TextFieldExtraProps) => {
   return (
-    <label><span>{rest.label}</span>
-    <input onChange={({ target: { value }}) => onChange(value)} {...rest} />
-    </label>
+    <Wrapper>
+      <label>
+        <span className={cx(label)}>{rest.label}</span>
+        <Input
+          /* className={cx(input)} */
+          onChange={({ target: { value } }) => onChange(value)}
+          {...rest}
+        />
+      </label>
+      {rest.error && <span className={cx(error)}>{rest.error}</span>}
+    </Wrapper>
   )
 }
 
-export default TextField;
+export default TextField

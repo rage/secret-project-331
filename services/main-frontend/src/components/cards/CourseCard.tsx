55c6feb4273103717e25dc680975bab44a339a7d
d471061c <cpastethat@gmail.com>
2021-11-16 16:28:21 +0200

    fix flags

--- services/main-frontend/src/components/cards/CourseCard.tsx
+++ services/main-frontend/src/components/cards/CourseCard.tsx
@@ -1,13 +1,10 @@
 import styled from "@emotion/styled"
-import Image from "next/image"
-import React, { useContext, useState } from "react"
+import React, { useContext } from "react"
 import { useTranslation } from "react-i18next"
 
 import Finland from "../../imgs/flags/Finland.svg"
 import SettingIcon from "../../imgs/setting.svg"
 import LoginStateContext from "../../shared-module/contexts/LoginStateContext"
-
-import LanguageSelector from "./LanguageSelector"
 
 interface CardExtraProps {
   title: string
@@ -40,56 +37,21 @@ const StyledSettingIcon = styled(SettingIcon)`
   top: 30px;
   right: 40px;
 `
-const StyledLanguageSelector = styled(LanguageSelector)`
-  position: absolute !important;
-  top: 0px;
-`
 
-const Languages = styled.div`
-  margin-top: 50px;
-  display: flex;
-  gap: 40px;
-  cursor: pointer;
-
-  div {
-    display: flex;
-  }
-
-  span {
-    font-family: "Josefin Sans", sans-serif;
-    font-size: 18px;
-    opacity: 0.7;
-  }
-
-  img {
-    width: 25px;
-    height: 25px;
-    border-radius: 100%;
-    margin-right: 10px;
-  }
-`
-
-const Info = styled.p`
-  font-family: "Josefin Sans", sans-serif;
-  font-size: 18px;
-  color: #1d9cf3;
-  line-height: 24px;
-  width: 60%;
-  margin-top: 1rem;
+const StyledFlag = styled(Finland)`
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  display: inline;
+  float: right;
 `
 
 export type CardProps = React.HTMLAttributes<HTMLDivElement> & CardExtraProps
 
 const CourseCard: React.FC<CardProps> = ({ title, description }) => {
   const { t } = useTranslation()
-  const [clicked, setClicked] = useState<boolean>(false)
   // If URL defined, the chapter is open
   const loginStateContext = useContext(LoginStateContext)
-
-  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.preventDefault()
-    setClicked(!clicked)
-  }
 
   return (
     <CourseGridWrapper>
@@ -97,16 +59,11 @@ const CourseCard: React.FC<CardProps> = ({ title, description }) => {
       <Content>
         <h2>{title}</h2>
         <span>{description}</span>
-        <Languages onClick={handleClick}>
-          <span>{t("choose-a-language")}</span>
-          <div>
-            <Image src={Finland} alt={t("finnish")} />
-            <p>{t("american-english")}</p>
-          </div>
-        </Languages>
-        <Info>{t("available-in-languages", { num: 8 })}</Info>
+        <div>
+          <StyledFlag />
+          <p>{t("american-english")}</p>
+        </div>
       </Content>
-      {clicked && <StyledLanguageSelector click={handleClick} />}
     </CourseGridWrapper>
   )
 }

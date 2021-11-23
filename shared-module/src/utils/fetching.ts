394242f345f3d604dd3bdc2f82e91404bcf59dee
Heliozoa <daniel.x.martinez@helsinki.fi>
2021-11-23 22:21:24 +0200

    Add missing functions

--- shared-module/src/utils/fetching.ts
+++ shared-module/src/utils/fetching.ts
@@ -4,14 +4,28 @@ import { AxiosResponse } from "axios"
 
 import { ErrorResponse } from "../bindings"
 
-// usage: validateResponse(response, isOrganization)
-// throws the response with an ErrorResponse if the data is invalid for useQuery to catch
 export function validateResponse<T>(
   response: AxiosResponse<unknown, unknown>,
   isT: (x: unknown) => x is T,
-): T {
+): T
+
+export function validateResponse<T, U>(
+  response: AxiosResponse<unknown, unknown>,
+  isT: (x: unknown) => x is T,
+  isU: (x: unknown) => x is U,
+): T
+
+// usage: validateResponse(response, isOrganization)
+// throws the response with an ErrorResponse if the data is invalid for useQuery to catch
+export function validateResponse<T, U>(
+  response: AxiosResponse<unknown, unknown>,
+  isT: (x: unknown) => x is T,
+  isU?: (x: unknown) => x is U,
+): T | U {
   const data = response.data
-  if (isT(data)) {
+  if (isU && isU(data)) {
+    return data
+  } else if (isT(data)) {
     return data
   } else {
     // alter the response data to contain an ErrorResponse
@@ -37,4 +51,17 @@ export function isString(x: unknown): x is string {
 
 export function isNumber(x: unknown): x is number {
   return typeof x === "number"
+}
+
+export function isNull(x: unknown): x is null {
+  return x === null
+}
+
+// usage: validateResponse(response, isUnion(isOrganization, isNull))
+export function isUnion<T, U>(
+  isT: (x: unknown) => x is T,
+  isU: (x: unknown) => x is U,
+): (x: unknown) => x is T | U {
+  // ts doesn't understand this so we need the explicit cast
+  return ((x) => isT(x) || isU(x)) as (x: unknown) => x is T | U
 }

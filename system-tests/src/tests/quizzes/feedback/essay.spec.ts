1774a71d688897b4c1592f9ed2f37dab357f1fb5
Heikki Pulli <heikki.pulli@helsinki.fi>
2021-12-21 11:05:03 +0200

    essay as read-only text

--- system-tests/src/tests/quizzes/feedback/essay.spec.ts
+++ system-tests/src/tests/quizzes/feedback/essay.spec.ts
@@ -46,19 +46,19 @@ test("test", async ({ headless, page }) => {
 
   await frame.fill(
     `textarea:below(:text("Word count"))`,
-    `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Laoreet sit amet cursus sit amet dictum sit. Et tortor consequat id porta nibh. Nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Maecenas ultricies mi eget mauris pharetra et ultrices neque ornare. Ultrices in iaculis nunc sed augue lacus viverra vitae congue. Egestas maecenas pharetra convallis posuere morbi leo urna. Tortor at auctor urna nunc id cursus metus aliquam eleifend. Vel eros donec ac odio tempor orci dapibus ultrices in. Ut diam quam nulla porttitor massa id neque aliquam. Egestas pretium aenean pharetra magna ac placerat vestibulum. Eu lobortis elementum nibh tellus molestie nunc non blandit. Euismod in pellentesque massa placerat duis. Non odio euismod lacinia at quis. Gravida quis blandit turpis cursus in hac. Odio facilisis mauris sit amet massa vitae tortor condimentum. In mollis nunc sed id semper risus in hendrerit gravida. Nunc sed blandit libero volutpat sed cras ornare. Augue mauris augue neque gravida in fermentum et sollicitudin ac.
+    `very hard essay about big data, by the guy who runs the NSA:
 
-  Elit at imperdiet dui accumsan. Sit amet nisl suscipit adipiscing bibendum est ultricies. Mauris rhoncus aenean vel elit. Consequat ac felis donec et odio. Tortor pretium viverra suspendisse potenti nullam ac. Aenean pharetra magna ac placerat vestibulum. Vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus. Ut sem nulla pharetra diam sit amet. Amet cursus sit amet dictum sit amet justo donec enim. Massa eget egestas purus viverra. Mattis vulputate enim nulla aliquet porttitor lacus luctus. Nunc mi ipsum faucibus vitae aliquet nec. Risus sed vulputate odio ut enim blandit volutpat. Sodales ut eu sem integer vitae justo eget magna fermentum. Ut morbi tincidunt augue interdum velit euismod in pellentesque. Diam donec adipiscing tristique risus nec feugiat in fermentum posuere.
+    There's also the whole myth that the Pentagon is more or less listening in on all sorts of communications, the things that are really important to US national security. What's so hard about this? It's because this is the very heart of US national security at the moment. A US president can no longer stand the kind of military, intelligence, financial muscle that was used to send US citizens to war zones. If we don't do something about it, then we have one of the world's worst privacy problems. It is also one of the very least-privileged parts of our society. The other problem that makes things difficult is the fact that there doesn't seem to be a big effort in Washington or at the NSA to solve the problem.
 
-  Malesuada fames ac turpis egestas sed tempus. Enim tortor at auctor urna nunc id. Consectetur a erat nam at lectus. Nec nam aliquam sem et tortor consequat id porta. Suspendisse faucibus interdum posuere lorem ipsum dolor. Mauris augue neque gravida in fermentum et sollicitudin. Quam viverra orci sagittis eu. Scelerisque eleifend donec pretium vulputate sapien nec. Enim nec dui nunc mattis enim ut. Neque egestas congue quisque egestas. Eget nunc lobortis mattis aliquam faucibus purus. Pellentesque sit amet porttitor eget dolor morbi. Dictum varius duis at consectetur lorem donec.
+    To put the Pentagon in the context of the Pentagon on steroids: "The Pentagon is the most militarized bureaucracy in the history of the planet." The National Security Council and the intelligence community should not make things more difficult for their citizens. To quote,
 
-  Morbi tristique senectus et netus et malesuada fames ac turpis. Sollicitudin ac orci phasellus egestas tellus rutrum tellus. Facilisis volutpat est velit egestas dui id ornare. Nisl nisi scelerisque eu ultrices vitae auctor eu augue. Elementum facilisis leo vel fringilla est ullamcorper eget nulla. Scelerisque purus semper eget duis at tellus at urna condimentum. Gravida dictum fusce ut placerat orci nulla pellentesque. Curabitur vitae nunc sed velit dignissim sodales. Magna fermentum iaculis eu non. Tincidunt id aliquet risus feugiat in ante metus dictum at. Augue eget arcu dictum varius. Aliquet eget sit amet tellus cras adipiscing enim eu turpis.`,
+    It has come to light that at least 75% of the staff is out of job. The US government has an astounding 75%, a total of 75%. The vast majority of US military personnel are not considered at all active members of the Army or Navy. That is not to say that the Navy is not also in conflict. The vast majority of their personnel have served in combat. There are more`,
   )
 
   await page.click("text=Submit")
 
   await expectScreenshotsToMatchSnapshots({
-    frame,
+    page,
     headless,
     snapshotName: "essay-feedback",
     waitForThisToBeVisibleAndStable: `text=your submit has been answered`,

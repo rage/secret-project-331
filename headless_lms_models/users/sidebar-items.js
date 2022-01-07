1ee727006ab5848ad0963d41f210fed3f0d9d3b7
nygrenh <nygrenh@gmail.com>
2022-01-07 15:20:22 +0000

    Deploying to gh-pages from  @ 1609a0fc661c8357eef57fdc1a09211d1ee634a3 🚀

--- headless_lms_models/users/sidebar-items.js
+++ headless_lms_models/users/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["authenticate_test_user",""],["find_by_upstream_id",""],["get_by_id",""],["insert",""],["insert_with_id",""],["insert_with_upstream_id",""]],"struct":[["User",""]]});
+initSidebarItems({"fn":[["authenticate_test_user",""],["find_by_upstream_id",""],["get_by_id",""],["insert",""],["insert_with_id",""],["insert_with_upstream_id_and_moocfi_id",""]],"struct":[["User",""]]});

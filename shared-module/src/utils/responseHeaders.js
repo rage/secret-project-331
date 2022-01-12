3aaaba6e44436d86bfb3cb72b61a64e2f827d5e4
Henrik Nygren <nygrenh@gmail.com>
2022-01-12 11:46:15 +0200

    Fix csp

--- shared-module/src/utils/responseHeaders.js
+++ shared-module/src/utils/responseHeaders.js
@@ -13,11 +13,10 @@ const normalResponseHeaders = [
     key: "Permissions-Policy",
     value: "fullscreen=(self)",
   },
-  //  default-src 'none'; connect-src 'self'; font-src 'self' https://cdn.jsdelivr.net; frame-src 'self'; img-src 'self' data: https://storage.googleapis.com; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/base/worker/workerMain.js https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/editor/editor.main.js https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/editor/editor.main.nls.js https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/language/json/jsonMode.js https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/language/json/jsonWorker.js https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/loader.js; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/editor/
   {
     key: "Content-Security-Policy",
     value:
-      "default-src 'self'; connect-src 'self'; font-src 'self' https://cdn.jsdelivr.net; frame-src *; img-src 'self' data: https://storage.googleapis.com; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net/npm/monaco-editor*; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net/npm/monaco-editor*",
+      "default-src 'self'; connect-src 'self'; font-src 'self' https://cdn.jsdelivr.net; frame-src *; img-src 'self' data: https://storage.googleapis.com; script-src 'self' 'unsafe-eval' 'unsafe-inline' blob: https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/base/worker/workerMain.js https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/editor/editor.main.js https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/editor/editor.main.nls.js https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/language/json/jsonMode.js https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/language/json/jsonWorker.js https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/loader.js; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/editor/",
   },
   {
     key: "X-Frame-Options",
@@ -43,7 +42,7 @@ const externallyEmbeddableIFrameResponseHeaders = [
   {
     key: "Content-Security-Policy",
     value:
-      "default-src 'self'; connect-src 'self'; font-src 'self' https://cdn.jsdelivr.net; frame-src *; img-src 'self' data: https://storage.googleapis.com; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net/npm/monaco-editor*; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net/npm/monaco-editor*",
+      "default-src 'self'; connect-src 'self'; font-src 'self' https://cdn.jsdelivr.net; frame-src *; img-src 'self' data: https://storage.googleapis.com; script-src 'self' 'unsafe-eval' 'unsafe-inline' blob: https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/base/worker/workerMain.js https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/editor/editor.main.js https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/editor/editor.main.nls.js https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/language/json/jsonMode.js https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/language/json/jsonWorker.js https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/loader.js; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/editor/",
   },
 ]
 

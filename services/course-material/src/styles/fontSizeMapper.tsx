a2cffab1c6a6946486d06c45bc33cc88e55e3f5b
Henrik Nygren <nygrenh@gmail.com>
2022-01-24 18:10:12 +0200

    Improve course material text typography (#562)
    * Improve course material text typography
    
    * Adjust font sizes

--- services/course-material/src/styles/fontSizeMapper.tsx
+++ services/course-material/src/styles/fontSizeMapper.tsx
@@ -3,14 +3,14 @@
 // Gutenberg specific, don't use in other contexes
 
 const fontSizes: { [colorName: string]: string | undefined } = {
-  small: "13px",
-  normal: "16px",
-  medium: "20px",
+  small: "16px",
+  normal: "20px",
+  medium: "24px",
   large: "36px",
   huge: "42px",
 }
 
-const DEFAULT_FONT_SIZE = "16px"
+const DEFAULT_FONT_SIZE = "20px"
 
 const fontSizeMapper = (fontName: string | undefined): string => {
   if (!fontName) {

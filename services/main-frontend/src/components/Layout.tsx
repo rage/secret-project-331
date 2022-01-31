a3693d72fa1f1f656871a820e56dff8dc7fa5e41
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2022-01-31 13:07:07 +0100

    useToastMutation for main-frontend

--- services/main-frontend/src/components/Layout.tsx
+++ services/main-frontend/src/components/Layout.tsx
@@ -2,10 +2,13 @@ import { css } from "@emotion/css"
 import Head from "next/head"
 import { useRouter } from "next/router"
 import React, { ReactNode } from "react"
+import { Toaster } from "react-hot-toast"
 
 import Centered from "../shared-module/components/Centering/Centered"
 import Footer from "../shared-module/components/Footer"
 import Navbar from "../shared-module/components/Navigation"
+import useMedia from "../shared-module/hooks/useMedia"
+import { respondToOrLarger } from "../shared-module/styles/respond"
 import basePath from "../shared-module/utils/base-path"
 
 type LayoutProps = {
@@ -18,6 +21,9 @@ type LayoutProps = {
   returnToPath?: string
 }
 
+const TOAST_BOTTOM_LEFT = "bottom-left"
+const TOAST_BOTTOM_CENTER = "bottom-center"
+
 const Layout: React.FC<LayoutProps> = ({
   children,
   title = "Secret Project 331",
@@ -28,6 +34,7 @@ const Layout: React.FC<LayoutProps> = ({
   returnToPath,
 }) => {
   const router = useRouter()
+  const notMobile = useMedia(respondToOrLarger.xs)
   // eslint-disable-next-line i18next/no-literal-string
   const returnPath = `/login?return_to=${encodeURIComponent(
     process.env.NEXT_PUBLIC_BASE_PATH + router.asPath,
@@ -66,6 +73,7 @@ const Layout: React.FC<LayoutProps> = ({
           <Centered variant="default">{children}</Centered>
         </main>
       </div>
+      <Toaster position={notMobile ? TOAST_BOTTOM_LEFT : TOAST_BOTTOM_CENTER} />
       <Footer licenseUrl={licenseUrl} />
     </>
   )

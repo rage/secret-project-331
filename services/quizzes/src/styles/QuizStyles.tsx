d3f60a8304d1b7a9d55204f3efd8434e98a35fb1
d471061c <cpastethat@gmail.com>
2022-02-01 19:04:32 +0200

    update colors for quizzes

--- services/quizzes/src/styles/QuizStyles.tsx
+++ services/quizzes/src/styles/QuizStyles.tsx
@@ -5,15 +5,15 @@ import { css } from "@emotion/css"
 import { baseTheme } from "../shared-module/styles"
 
 export const quizTheme = {
-  gradingCorrectItemBackground: baseTheme.colors.green[400],
-  gradingCorrectItemColor: baseTheme.colors.clear[700],
+  gradingCorrectItemBackground: baseTheme.colors.green[700],
+  gradingCorrectItemColor: baseTheme.colors.clear[100],
   gradingSelectedItemBackground: baseTheme.colors.grey[700],
   gradingSelectedItemColor: baseTheme.colors.clear[200],
-  gradingWrongItemBackground: "#fcc5cd",
-  gradingWrongItemColor: baseTheme.colors.clear[700],
+  gradingWrongItemBackground: baseTheme.colors.crimson[600],
+  gradingWrongItemColor: baseTheme.colors.clear[200],
   quizBodyColor: baseTheme.colors.grey[700],
   quizBodyFontSize: "24px",
-  quizItemBackground: "#E9E9ED",
+  quizItemBackground: baseTheme.colors.green[700],
   quizTitleFontSize: "16px",
   selectedItemBackground: baseTheme.colors.green[600],
   selectedItemColor: baseTheme.colors.clear[100],

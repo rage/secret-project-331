dbf270bf884e2fe0550cba44e18d0a994542961c
Jori Lampi <jori.lampi@helsinki.fi>
2022-02-04 17:46:03 +0200

    Fix seed and make more verbose

--- services/headless-lms/server/src/bin/seed.rs
+++ services/headless-lms/server/src/bin/seed.rs
@@ -1285,21 +1285,25 @@ async fn seed_sample_course(
     let block_id_4 = Uuid::new_v5(&course_id, b"09b327a8-8e65-437e-9678-554fc4d98dd4");
     let block_id_5 = Uuid::new_v5(&course_id, b"834648cc-72d9-42d1-bed7-cc6a2e186ae6");
     let block_id_6 = Uuid::new_v5(&course_id, b"223a4718-5287-49ff-853e-a67f4612c629");
-    let exercise_c1p1_1 = Uuid::new_v5(&course_id, b"cfb950a7-db4e-49e4-8ec4-d7a32b691b08");
-    let exercise_slide_c1p1_1 = Uuid::new_v5(&course_id, b"182c4128-c4e4-40c9-bc5a-1265bfd3654c");
-    let exercise_task_c1p1e1_1 = Uuid::new_v5(&course_id, b"f73dab3b-3549-422d-8377-ece1972e5576");
-    let spec_c1p1e1t1_1 = Uuid::new_v5(&course_id, b"5f6b7850-5034-4cef-9dcf-e3fd4831067f");
-    let spec_c1p1e1t1_2 = Uuid::new_v5(&course_id, b"c713bbfc-86bf-4877-bd39-53afaf4444b5");
-    let spec_c1p1e1t1_3 = Uuid::new_v5(&course_id, b"4027d508-4fad-422e-bb7f-15c613a02cc6");
+    let exercise_1_id = Uuid::new_v5(&course_id, b"cfb950a7-db4e-49e4-8ec4-d7a32b691b08");
+    let exercise_1_slide_1_id = Uuid::new_v5(&course_id, b"182c4128-c4e4-40c9-bc5a-1265bfd3654c");
+    let exercise_1_slide_1_task_1_id =
+        Uuid::new_v5(&course_id, b"f73dab3b-3549-422d-8377-ece1972e5576");
+    let exercise_1_slide_1_task_1_spec_1_id =
+        Uuid::new_v5(&course_id, b"5f6b7850-5034-4cef-9dcf-e3fd4831067f");
+    let exercise_1_slide_1_task_1_spec_2_id =
+        Uuid::new_v5(&course_id, b"c713bbfc-86bf-4877-bd39-53afaf4444b5");
+    let exercise_1_slide_1_task_1_spec_3_id =
+        Uuid::new_v5(&course_id, b"4027d508-4fad-422e-bb7f-15c613a02cc6");
     let (exercise_block_1, exercise_1, slide_1, task_1) = example_exercise(
-        exercise_c1p1_1,
-        exercise_slide_c1p1_1,
-        exercise_task_c1p1e1_1,
+        exercise_1_id,
+        exercise_1_slide_1_id,
+        exercise_1_slide_1_task_1_id,
         block_id_2,
         block_id_3,
-        spec_c1p1e1t1_1,
-        spec_c1p1e1t1_2,
-        spec_c1p1e1t1_3,
+        exercise_1_slide_1_task_1_spec_1_id,
+        exercise_1_slide_1_task_1_spec_2_id,
+        exercise_1_slide_1_task_1_spec_3_id,
     );
     let page_c1_1 = create_page(
         conn,
@@ -1324,57 +1328,66 @@ async fn seed_sample_course(
     )
     .await?;
 
-    let exercise_c1p2_1 = Uuid::new_v5(&course_id, b"36e7f0c2-e663-4382-a503-081866cfe7d0");
-    let exercise_slide_c1p2e1_1 = Uuid::new_v5(&course_id, b"0d85864d-a20d-4d65-9ace-9b4d377f38e8");
-    let exercise_task_c1p2e1_1 = Uuid::new_v5(&course_id, b"e7fca192-2161-4ab8-8533-8c41dbaa2d69");
-    let spec_c1p2e1t1_1 = Uuid::new_v5(&course_id, b"5898293f-2d41-43b1-9e44-92d487196ade");
-    // let spec_c1p2e1t1_2 = Uuid::new_v5(&course_id, b"93d27d79-f9a1-44ab-839f-484accc67e32");
-    // let spec_c1p2e1t1_3 = Uuid::new_v5(&course_id, b"81ec2df2-a5fd-4d7d-b85f-0c304e8d2030");
-    let exercise_c1p2_2 = Uuid::new_v5(&course_id, b"64d273eb-628f-4d43-a11a-e69ebe244942");
-    // let exercise_slide_c1p2e2_1 = Uuid::new_v5(&course_id, b"5441c7c0-60f1-4058-8223-7090c9cac7cb");
-    let exercise_task_c1p2e2_1 = Uuid::new_v5(&course_id, b"114caac5-006a-4afb-9806-785154263c11");
-    let spec_c1p2e2t1_1 = Uuid::new_v5(&course_id, b"28ea3062-bd6a-45f5-9844-03174e00a0a8");
-    // let spec_c1p2e2t1_2 = Uuid::new_v5(&course_id, b"1982f566-2d6a-485d-acb0-65d8b8864c7e");
-    // let spec_c1p2e2t1_3 = Uuid::new_v5(&course_id, b"01ec5329-2cf6-4d0f-92b2-d388360fb402");
-    // let exercise_c1p2_3 = Uuid::new_v5(&course_id, b"029688ec-c7be-4cb3-8928-85cfd6551083");
-    // let exercise_slide_c1p2e3_1 = Uuid::new_v5(&course_id, b"ab8a314b-ac03-497b-8ade-3d8512ed00c9");
-    // let exercise_task_c1p2e3_1 = Uuid::new_v5(&course_id, b"382fffce-f177-47d0-a5c0-cc8906d34c49");
-    let spec_c1p2e3t1_1 = Uuid::new_v5(&course_id, b"4bae54a3-d67c-428b-8996-290f70ae08fa");
-    // let spec_c1p2e3t1_2 = Uuid::new_v5(&course_id, b"c3f257c0-bdc2-4d81-99ff-a71c76fe670a");
-    // let spec_c1p2e3t1_3 = Uuid::new_v5(&course_id, b"fca5a8ba-50e0-4375-8d4b-9d02762d908c");
-    let (exercise_block_2_1, exercise_2_1, exercise_slide_2_1, exercise_task_2_1) =
-        example_exercise(
-            exercise_c1p2_1,
-            exercise_slide_c1p2e1_1,
-            exercise_task_c1p2e1_1,
-            Uuid::new_v5(&course_id, b"2dbb4649-bcac-47ab-a817-ca17dcd70378"),
-            Uuid::new_v5(&course_id, b"c0986981-c8ae-4c0b-b558-1163a16760ec"),
-            spec_c1p2e1t1_1,
-            Uuid::new_v5(&course_id, b"93d27d79-f9a1-44ab-839f-484accc67e32"),
-            Uuid::new_v5(&course_id, b"81ec2df2-a5fd-4d7d-b85f-0c304e8d2030"),
-        );
-    let (exercise_block_2_2, exercise_2_2, exercise_slide_2_2, exercise_task_2_2) =
-        example_exercise(
-            exercise_c1p2_2,
-            Uuid::new_v5(&course_id, b"5441c7c0-60f1-4058-8223-7090c9cac7cb"),
-            exercise_task_c1p2e2_1,
-            Uuid::new_v5(&course_id, b"fb26489d-ca49-4f76-a1c2-f759ed3146c0"),
-            Uuid::new_v5(&course_id, b"c0986981-c8ae-4c0b-b558-1163a16760ec"),
-            spec_c1p2e2t1_1,
-            Uuid::new_v5(&course_id, b"93d27d79-f9a1-44ab-839f-484accc67e32"),
-            Uuid::new_v5(&course_id, b"81ec2df2-a5fd-4d7d-b85f-0c304e8d2030"),
-        );
-    let (exercise_block_2_3, exercise_2_3, exercise_slide_2_3, exercise_task_2_3) =
-        example_exercise(
-            Uuid::new_v5(&course_id, b"029688ec-c7be-4cb3-8928-85cfd6551083"),
-            Uuid::new_v5(&course_id, b"ab8a314b-ac03-497b-8ade-3d8512ed00c9"),
-            Uuid::new_v5(&course_id, b"382fffce-f177-47d0-a5c0-cc8906d34c49"),
-            Uuid::new_v5(&course_id, b"334593ad-8ba5-4589-b1f7-b159e754bdc5"),
-            Uuid::new_v5(&course_id, b"389e80bd-5f91-40c7-94ff-7dda1eeb96fb"),
-            spec_c1p2e3t1_1,
-            Uuid::new_v5(&course_id, b"c3f257c0-bdc2-4d81-99ff-a71c76fe670a"),
-            Uuid::new_v5(&course_id, b"fca5a8ba-50e0-4375-8d4b-9d02762d908c"),
-        );
+    let exercise_2_id = Uuid::new_v5(&course_id, b"36e7f0c2-e663-4382-a503-081866cfe7d0");
+    let exercise_2_slide_1_id = Uuid::new_v5(&course_id, b"0d85864d-a20d-4d65-9ace-9b4d377f38e8");
+    let exercise_2_slide_1_task_1_id =
+        Uuid::new_v5(&course_id, b"e7fca192-2161-4ab8-8533-8c41dbaa2d69");
+    let exercise_2_slide_1_task_1_spec_1_id =
+        Uuid::new_v5(&course_id, b"5898293f-2d41-43b1-9e44-92d487196ade");
+    let exercise_2_slide_1_task_1_spec_2_id =
+        Uuid::new_v5(&course_id, b"93d27d79-f9a1-44ab-839f-484accc67e32");
+    let exercise_2_slide_1_task_1_spec_3_id =
+        Uuid::new_v5(&course_id, b"81ec2df2-a5fd-4d7d-b85f-0c304e8d2030");
+    let exercise_3_id = Uuid::new_v5(&course_id, b"64d273eb-628f-4d43-a11a-e69ebe244942");
+    let exercise_3_slide_1_id = Uuid::new_v5(&course_id, b"5441c7c0-60f1-4058-8223-7090c9cac7cb");
+    let exercise_3_slide_1_task_1_id =
+        Uuid::new_v5(&course_id, b"114caac5-006a-4afb-9806-785154263c11");
+    let exercise_3_slide_1_task_1_spec_1_id =
+        Uuid::new_v5(&course_id, b"28ea3062-bd6a-45f5-9844-03174e00a0a8");
+    let exercise_3_slide_1_task_1_spec_2_id =
+        Uuid::new_v5(&course_id, b"1982f566-2d6a-485d-acb0-65d8b8864c7e");
+    let exercise_3_slide_1_task_1_spec_3_id =
+        Uuid::new_v5(&course_id, b"01ec5329-2cf6-4d0f-92b2-d388360fb402");
+    let exercise_4_id = Uuid::new_v5(&course_id, b"029688ec-c7be-4cb3-8928-85cfd6551083");
+    let exercise_4_slide_1_id = Uuid::new_v5(&course_id, b"ab8a314b-ac03-497b-8ade-3d8512ed00c9");
+    let exercise_4_slide_1_task_1_id =
+        Uuid::new_v5(&course_id, b"382fffce-f177-47d0-a5c0-cc8906d34c49");
+    let exercise_4_slide_1_task_1_spec_1_id =
+        Uuid::new_v5(&course_id, b"4bae54a3-d67c-428b-8996-290f70ae08fa");
+    let exercise_4_slide_1_task_1_spec_2_id =
+        Uuid::new_v5(&course_id, b"c3f257c0-bdc2-4d81-99ff-a71c76fe670a");
+    let exercise_4_slide_1_task_1_spec_3_id =
+        Uuid::new_v5(&course_id, b"fca5a8ba-50e0-4375-8d4b-9d02762d908c");
+    let (exercise_block_2, exercise_2, slide_2, task_2) = example_exercise(
+        exercise_2_id,
+        exercise_2_slide_1_id,
+        exercise_2_slide_1_task_1_id,
+        Uuid::new_v5(&course_id, b"2dbb4649-bcac-47ab-a817-ca17dcd70378"),
+        Uuid::new_v5(&course_id, b"c0986981-c8ae-4c0b-b558-1163a16760ec"),
+        exercise_2_slide_1_task_1_spec_1_id,
+        exercise_2_slide_1_task_1_spec_2_id,
+        exercise_2_slide_1_task_1_spec_3_id,
+    );
+    let (exercise_block_3, exercise_3, slide_3, task_3) = example_exercise(
+        exercise_3_id,
+        exercise_3_slide_1_id,
+        exercise_3_slide_1_task_1_id,
+        Uuid::new_v5(&course_id, b"fb26489d-ca49-4f76-a1c2-f759ed3146c0"),
+        Uuid::new_v5(&course_id, b"c0986981-c8ae-4c0b-b558-1163a16760ec"),
+        exercise_3_slide_1_task_1_spec_1_id,
+        exercise_3_slide_1_task_1_spec_2_id,
+        exercise_3_slide_1_task_1_spec_3_id,
+    );
+    let (exercise_block_4, exercise_4, slide_4, task_4) = example_exercise(
+        exercise_4_id,
+        exercise_4_slide_1_id,
+        exercise_4_slide_1_task_1_id,
+        Uuid::new_v5(&course_id, b"334593ad-8ba5-4589-b1f7-b159e754bdc5"),
+        Uuid::new_v5(&course_id, b"389e80bd-5f91-40c7-94ff-7dda1eeb96fb"),
+        exercise_4_slide_1_task_1_spec_1_id,
+        exercise_4_slide_1_task_1_spec_2_id,
+        exercise_4_slide_1_task_1_spec_3_id,
+    );
     create_page(
         conn,
         course.id,
@@ -1384,17 +1397,17 @@ async fn seed_sample_course(
             url_path: "/chapter-1/page-2".to_string(),
             title: "page 2".to_string(),
             chapter_id: Some(chapter_1.id),
-            exercises: vec![exercise_2_1, exercise_2_2, exercise_2_3],
-            exercise_slides: vec![exercise_slide_2_1, exercise_slide_2_2, exercise_slide_2_3],
-            exercise_tasks: vec![exercise_task_2_1, exercise_task_2_2, exercise_task_2_3],
+            exercises: vec![exercise_2, exercise_3, exercise_4],
+            exercise_slides: vec![slide_2, slide_3, slide_4],
+            exercise_tasks: vec![task_2, task_3, task_4],
             content: serde_json::json!([
                 paragraph(
                     "First chapters second page.",
                     Uuid::new_v5(&course_id, b"9faf5a2d-f60d-4a70-af3d-0e7e3d6fe273"),
                 ),
-                exercise_block_2_1,
-                exercise_block_2_2,
-                exercise_block_2_3,
+                exercise_block_2,
+                exercise_block_3,
+                exercise_block_4,
             ]),
         },
     )
@@ -1783,23 +1796,26 @@ async fn seed_sample_course(
     )
     .await?;
 
-    let exercise_c2p1_1 = Uuid::new_v5(&course_id, b"8bb4faf4-9a34-4df7-a166-89ade530d0f6");
-    let exercise_slide_c2p1e1_1 = Uuid::new_v5(&course_id, b"b99d1041-7835-491e-a1c8-b47eee8e7ab4");
-    let exercise_task_c2p1e1_1 = Uuid::new_v5(&course_id, b"a6508b8a-f58e-43ac-9f02-785575e716f5");
-    let spec_c2p1e1t1_1 = Uuid::new_v5(&course_id, b"fe464d17-2365-4e65-8b33-e0ebb5a67836");
-    // let spec_c2p1e1t1_2 = Uuid::new_v5(&course_id, b"6633ffc7-c76e-4049-840e-90eefa6b49e8");
-    // let spec_c2p1e1t1_3 = Uuid::new_v5(&course_id, b"d77fb97d-322c-4c5f-a405-8978a8cfb0a9");
-    let (exercise_block_3_1, exercise_3_1, exercise_slide_3_1, exercise_task_3_1) =
-        example_exercise(
-            exercise_c2p1_1,
-            exercise_slide_c2p1e1_1,
-            exercise_task_c2p1e1_1,
-            Uuid::new_v5(&course_id, b"e869c471-b1b7-42a0-af05-dffd1d86a7bb"),
-            Uuid::new_v5(&course_id, b"fe464d17-2365-4e65-8b33-e0ebb5a67836"),
-            spec_c2p1e1t1_1,
-            Uuid::new_v5(&course_id, b"6633ffc7-c76e-4049-840e-90eefa6b49e8"),
-            Uuid::new_v5(&course_id, b"d77fb97d-322c-4c5f-a405-8978a8cfb0a9"),
-        );
+    let exercise_5_id = Uuid::new_v5(&course_id, b"8bb4faf4-9a34-4df7-a166-89ade530d0f6");
+    let exercise_5_slide_1_id = Uuid::new_v5(&course_id, b"b99d1041-7835-491e-a1c8-b47eee8e7ab4");
+    let exercise_5_slide_1_task_1_id =
+        Uuid::new_v5(&course_id, b"a6508b8a-f58e-43ac-9f02-785575e716f5");
+    let exercise_5_slide_1_task_1_spec_1_id =
+        Uuid::new_v5(&course_id, b"fe464d17-2365-4e65-8b33-e0ebb5a67836");
+    let exercise_5_slide_1_task_1_spec_2_id =
+        Uuid::new_v5(&course_id, b"6633ffc7-c76e-4049-840e-90eefa6b49e8");
+    let exercise_5_slide_1_task_1_spec_3_id =
+        Uuid::new_v5(&course_id, b"d77fb97d-322c-4c5f-a405-8978a8cfb0a9");
+    let (exercise_block_5, exercise_5, exercise_slide_5, exercise_task_5) = example_exercise(
+        exercise_5_id,
+        exercise_5_slide_1_id,
+        exercise_5_slide_1_task_1_id,
+        Uuid::new_v5(&course_id, b"e869c471-b1b7-42a0-af05-dffd1d86a7bb"),
+        Uuid::new_v5(&course_id, b"fe464d17-2365-4e65-8b33-e0ebb5a67836"),
+        exercise_5_slide_1_task_1_spec_1_id,
+        exercise_5_slide_1_task_1_spec_2_id,
+        exercise_5_slide_1_task_1_spec_3_id,
+    );
     create_page(
         conn,
         course.id,
@@ -1809,10 +1825,10 @@ async fn seed_sample_course(
             url_path: "/chapter-2/intro".to_string(),
             title: "In the second chapter...".to_string(),
             chapter_id: Some(chapter_2.id),
-            exercises: vec![exercise_3_1],
-            exercise_slides: vec![exercise_slide_3_1],
-            exercise_tasks: vec![exercise_task_3_1],
-            content: serde_json::json!([exercise_block_3_1]),
+            exercises: vec![exercise_5],
+            exercise_slides: vec![exercise_slide_5],
+            exercise_tasks: vec![exercise_task_5],
+            content: serde_json::json!([exercise_block_5]),
         },
     )
     .await?;
@@ -1833,13 +1849,13 @@ async fn seed_sample_course(
         submit_and_grade(
             conn,
             b"8c447aeb-1791-4236-8471-204d8bc27507",
-            exercise_c1p1_1,
-            exercise_slide_c2p1e1_1,
+            exercise_1_id,
+            exercise_1_slide_1_id,
             course.id,
-            exercise_task_c1p1e1_1,
+            exercise_1_slide_1_task_1_id,
             user_id,
             default_instance.id,
-            spec_c1p1e1t1_1.to_string(),
+            exercise_1_slide_1_task_1_spec_1_id.to_string(),
             100.0,
         )
         .await?;
@@ -1847,78 +1863,78 @@ async fn seed_sample_course(
         submit_and_grade(
             conn,
             b"a719fe25-5721-412d-adea-4696ccb3d883",
-            exercise_c1p1_1,
-            exercise_slide_c2p1e1_1,
+            exercise_1_id,
+            exercise_1_slide_1_id,
             course.id,
-            exercise_task_c1p1e1_1,
+            exercise_1_slide_1_task_1_id,
             user_id,
             default_instance.id,
-            spec_c1p1e1t1_2.to_string(),
+            exercise_1_slide_1_task_1_spec_2_id.to_string(),
             1.0,
         )
         .await?;
         submit_and_grade(
             conn,
             b"bbc16d4b-1f91-4bd0-a47f-047665a32196",
-            exercise_c1p1_1,
-            exercise_slide_c2p1e1_1,
+            exercise_1_id,
+            exercise_1_slide_1_id,
             course.id,
-            exercise_task_c1p1e1_1,
+            exercise_1_slide_1_task_1_id,
             user_id,
             default_instance.id,
-            spec_c1p1e1t1_3.to_string(),
+            exercise_1_slide_1_task_1_spec_3_id.to_string(),
             0.0,
         )
         .await?;
         submit_and_grade(
             conn,
             b"c60bf5e5-9b67-4f62-9df7-16d268c1b5f5",
-            exercise_c1p1_1,
-            exercise_slide_c1p1_1,
+            exercise_1_id,
+            exercise_1_slide_1_id,
             course.id,
-            exercise_task_c1p1e1_1,
+            exercise_1_slide_1_task_1_id,
             user_id,
             default_instance.id,
-            spec_c1p1e1t1_1.to_string(),
+            exercise_1_slide_1_task_1_spec_1_id.to_string(),
             60.0,
         )
         .await?;
         submit_and_grade(
             conn,
             b"e0ec1386-72aa-4eed-8b91-72bba420c23b",
-            exercise_c1p2_1,
-            exercise_slide_c1p2e1_1,
+            exercise_2_id,
+            exercise_2_slide_1_id,
             course.id,
-            exercise_task_c1p2e1_1,
+            exercise_2_slide_1_task_1_id,
             user_id,
             default_instance.id,
-            spec_c1p2e1t1_1.to_string(),
+            exercise_2_slide_1_task_1_spec_1_id.to_string(),
             70.0,
         )
         .await?;
         submit_and_grade(
             conn,
             b"02c9e1ad-6e4c-4473-a3e9-dbfab018a055",
-            exercise_c2p1_1,
-            exercise_slide_c2p1e1_1,
+            exercise_5_id,
+            exercise_5_slide_1_id,
             course.id,
-            exercise_task_c2p1e1_1,
+            exercise_5_slide_1_task_1_id,
             user_id,
             default_instance.id,
-            spec_c2p1e1t1_1.to_string(),
+            exercise_5_slide_1_task_1_spec_1_id.to_string(),
             80.0,
         )
         .await?;
         submit_and_grade(
             conn,
             b"75df4600-d337-4083-99d1-e8e3b6bf6192",
-            exercise_c1p1_1,
-            exercise_slide_c1p1_1,
+            exercise_1_id,
+            exercise_1_slide_1_id,
             course.id,
-            exercise_task_c1p1e1_1,
+            exercise_1_slide_1_task_1_id,
             user_id,
             default_instance.id,
-            spec_c1p1e1t1_1.to_string(),
+            exercise_1_slide_1_task_1_spec_1_id.to_string(),
             90.0,
         )
         .await?;

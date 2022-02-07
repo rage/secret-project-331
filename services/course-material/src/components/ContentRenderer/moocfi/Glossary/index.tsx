bee41b54ee894c4cb81f15eab54f364f793a6d81
Martinez <Heliozoa@users.noreply.github.com>
2022-02-07 14:42:44 +0200

    Acronyms (#570)
    * Implemented basic acronym view and adding new acronyms
    
    * Renamed acronyms to glossary, added management view
    
    * system test for glossary
    
    * updated snapshots
    
    * Addressed feedback
    
    * Updated system tests
    
    * Added generated docs

---
+++ services/course-material/src/components/ContentRenderer/moocfi/Glossary/index.tsx
@@ -0,0 +1,26 @@
+import React, { useContext } from "react"
+import { useTranslation } from "react-i18next"
+
+import PageContext from "../../../../contexts/PageContext"
+import ErrorBanner from "../../../../shared-module/components/ErrorBanner"
+import Spinner from "../../../../shared-module/components/Spinner"
+import withErrorBoundary from "../../../../shared-module/utils/withErrorBoundary"
+
+import Glossary from "./Glossary"
+
+const GlossaryBlock: React.FC = () => {
+  const { t } = useTranslation()
+  const pageContext = useContext(PageContext)
+
+  if (pageContext.state !== "ready") {
+    return <Spinner variant="small" />
+  }
+
+  if (pageContext.pageData.course_id === null) {
+    return <ErrorBanner variant="readOnly" error={t("block-invalid-without-course")} />
+  }
+
+  return <Glossary courseId={pageContext.pageData.course_id} />
+}
+
+export default withErrorBoundary(GlossaryBlock)

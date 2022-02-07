ef6c3903b6449e80ae8a81b7b6bb123aa239e483
d471061c <cpastethat@gmail.com>
2022-02-07 12:40:30 +0200

    update colors

--- services/quizzes/src/styles/QuizStyles.tsx
+++ services/quizzes/src/styles/QuizStyles.tsx
@@ -5,11 +5,11 @@ import { css } from "@emotion/css"
 import { baseTheme } from "../shared-module/styles"
 
 export const quizTheme = {
-  gradingCorrectItemBackground: baseTheme.colors.green[700],
+  gradingCorrectItemBackground: baseTheme.colors.green[600],
   gradingCorrectItemColor: baseTheme.colors.clear[100],
   gradingSelectedItemBackground: baseTheme.colors.grey[700],
   gradingSelectedItemColor: baseTheme.colors.clear[200],
-  gradingWrongItemBackground: baseTheme.colors.red[700],
+  gradingWrongItemBackground: baseTheme.colors.red[600],
   gradingWrongItemColor: baseTheme.colors.clear[200],
   quizBodyColor: baseTheme.colors.grey[700],
   quizBodyFontSize: "24px",

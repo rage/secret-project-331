e83fbd2b2208db5e70cf0ec52e55b8f775a93e29
Henrik Nygren <nygrenh@gmail.com>
2022-02-22 16:45:57 +0200

    Fix mobile view on the front page and course list (#630)

--- shared-module/src/styles/respond.ts
+++ shared-module/src/styles/respond.ts
@@ -2,11 +2,13 @@
 const mediaQuery = (minWidth: number) => `@media (min-width: ${minWidth}rem)`
 
 export const respondToOrLarger = {
-  xxs: mediaQuery(25),
-  xs: mediaQuery(30),
-  sm: mediaQuery(36),
-  md: mediaQuery(48),
-  lg: mediaQuery(62),
-  xl: mediaQuery(75),
-  xxl: mediaQuery(87.5),
+  xxs: mediaQuery(25), // 400px
+  xs: mediaQuery(30), // 480px
+  sm: mediaQuery(36), // 576px
+  md: mediaQuery(48), // 768px
+  lg: mediaQuery(62), // 992px
+  xl: mediaQuery(75), // 1200px
+  xxl: mediaQuery(87.5), // 1400px
+  xxxl: mediaQuery(100), // 1600px
+  xxxxl: mediaQuery(125), // 2000px
 }
---
+++ system-tests/src/tests/organization-list.spec.ts
@@ -0,0 +1,17 @@
+import { test } from "@playwright/test"
+
+import expectScreenshotsToMatchSnapshots from "../utils/screenshot"
+
+test.use({
+  storageState: "src/states/admin@example.com.json",
+})
+
+test("Organization list renders", async ({ page, headless }) => {
+  await page.goto("http://project-331.local/")
+  await expectScreenshotsToMatchSnapshots({
+    page,
+    headless,
+    snapshotName: "frontpage-organizations-list",
+    waitForThisToBeVisibleAndStable: "text=learn the basics in Computer Science",
+  })
+})

192d10a10b3fa271cfe66a26e1681d90078e99f6
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2022-03-10 22:07:13 +0100

    Enable connect-src vimeo oembed api, fetch from vimeo

--- services/course-material/src/components/ContentRenderer/core/embeds/variants/VimeoEmbedBlock.tsx
+++ services/course-material/src/components/ContentRenderer/core/embeds/variants/VimeoEmbedBlock.tsx
@@ -1,32 +1,41 @@
 import { css } from "@emotion/css"
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { useTranslation } from "react-i18next"
 
 import { EmbedAttributes } from "../../../../../../types/GutenbergBlockAttributes"
 import BreakFromCentered from "../../../../../shared-module/components/Centering/BreakFromCentered"
 import ErrorBanner from "../../../../../shared-module/components/ErrorBanner"
+import Spinner from "../../../../../shared-module/components/Spinner"
 import aspectRatioFromClassName from "../../../../../utils/aspectRatioFromClassName"
 
 export const VimeoEmbedBlock: React.FC<EmbedAttributes> = (props) => {
-  const [embedHtml, setEmbedHtml] = useState(null)
+  const [embedHtml, setEmbedHtml] = useState(undefined)
+  const [fetching, setFetching] = useState(true)
+  const { t } = useTranslation()
   useEffect(() => {
     const fetchEmbed = async () => {
       if (props.url) {
+        // maxWidth and maxHeight same as backend url_to_oembed_endpoint.rs
         const response = await axios.get(
-          `/oembed/1.0/proxy?url=${encodeURIComponent(props.url)}&_locale=user`,
+          `https://vimeo.com/api/oembed.json?url=${encodeURIComponent(
+            props.url,
+          )}&maxwidth=780&maxheight=440`,
         )
         const data = await response.data
         if (data.html) {
           setEmbedHtml(data.html)
         }
       }
+      setFetching(false)
     }
     fetchEmbed()
   })
 
   return (
     <>
-      {embedHtml ? (
+      {fetching && <Spinner variant="medium" />}
+      {embedHtml && !fetching && (
         <BreakFromCentered sidebar={false}>
           <div
             className={css`
@@ -42,11 +51,14 @@ export const VimeoEmbedBlock: React.FC<EmbedAttributes> = (props) => {
             }}
           ></div>
         </BreakFromCentered>
-      ) : (
+      )}
+      {!embedHtml && !fetching && (
         <ErrorBanner
           variant="readOnly"
-          // eslint-disable-next-line i18next/no-literal-string
-          error={`Could not fetch VIMEO oEmbed with url: ${props.url}`}
+          error={t("could-not-fetch-embed", {
+            provider: props.providerNameSlug,
+            url: props.url ?? "undefined",
+          })}
         />
       )}
     </>

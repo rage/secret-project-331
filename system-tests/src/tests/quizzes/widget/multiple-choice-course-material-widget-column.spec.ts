2d580eef6596bf4f89fe9c9fb8ee083a6bb69d98
iPegii <51372604+iPegii@users.noreply.github.com>
2022-03-25 01:23:21 +0200

    add name to tests and fix screenshot

--- system-tests/src/tests/quizzes/widget/multiple-choice-course-material-widget-column.spec.ts
+++ system-tests/src/tests/quizzes/widget/multiple-choice-course-material-widget-column.spec.ts
@@ -6,7 +6,7 @@ import waitForFunction from "../../../utils/waitForFunction"
 test.use({
   storageState: "src/states/user@example.com.json",
 })
-test("test", async ({ page, headless }) => {
+test("multiple-choice course material column test", async ({ page, headless }) => {
   // Go to http://project-331.local/
   await page.goto("http://project-331.local/")
   // Click text=University of Helsinki, Department of Computer Science

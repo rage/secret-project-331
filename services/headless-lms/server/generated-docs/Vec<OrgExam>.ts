145a3accf03a736007e8e56c5394635c7a531d7b
HegePI <19744083+HegePI@users.noreply.github.com>
2022-03-28 16:40:09 +0300

    create and copy exams (#639)
    * initial commit
    
    * duplicate Exam
    
    * copy exam sql
    
    * useForm, useToast
    
    * separated copy functionality
    
    * copy exam functionality
    
    * use new functionality in controller
    
    * duplicate exam
    
    * links in exam cards
    
    * exam list snapshots
    
    * remove duplicate button, hacky way to duplicate with useForm
    
    * exam list
    
    * creating exam also creates page, dialog styling
    
    * exam and manage link in examlist
    
    * master merged
    
    * docs generated
    
    * snapshots updated
    
    * action on resource endpoint
    
    * ActionOnResource binding generated
    
    * render if authorized
    
    * removed unneeded snapshots
    
    * better bindigs and return value from endpoint
    
    * bool.json
    
    * requested changes
    
    * no instructions in new exam
    
    * dialog padding, input field margin bottom
    
    * create exam dialog snapshots
    
    * deleted new exam manage page, beforescreenshot
    
    * new course instance dialog padding
    
    * snapshots updated
    
    * snapshots updated

---
+++ services/headless-lms/server/generated-docs/Vec<OrgExam>.ts
@@ -0,0 +1,9 @@
+type Vec<OrgExam> = Array<{
+  id: string
+  name: string
+  instructions: unknown
+  starts_at: Date | null
+  ends_at: Date | null
+  time_minutes: number
+  organization_id: string
+}>

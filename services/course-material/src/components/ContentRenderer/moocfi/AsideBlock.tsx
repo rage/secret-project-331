79e830adb7eebd11ba03c006fe9ffaf62ede73ec
Pekka <51372604+iPegii@users.noreply.github.com>
2022-04-07 15:06:55 +0300

    Add infobox (#709)
    * add caption to Youtube and Vimeo
    
    * initialize InfoBox
    
    * Add infobox and update snapshots
    
    * remove broken history screenshots from tests
    
    * update InfoBox styling to left aligned
    
    * update snapshots
    
    * add cargo clean
    
    * exam-list snapshot to only include the dialog
    
    * update snapshots
    
    * update snapshots
    
    * update snapshots

--- services/course-material/src/components/ContentRenderer/moocfi/AsideBlock.tsx
+++ services/course-material/src/components/ContentRenderer/moocfi/AsideBlock.tsx
@@ -4,8 +4,8 @@ import { BlockRendererProps } from ".."
 import Aside, { AsideComponentProps } from "../../../shared-module/components/Aside"
 import withErrorBoundary from "../../../shared-module/utils/withErrorBoundary"
 
-const HeroSectionBlock: React.FC<BlockRendererProps<AsideComponentProps>> = (props) => {
+const AsideBLock: React.FC<BlockRendererProps<AsideComponentProps>> = (props) => {
   return <Aside title={props.data.attributes.title} bodyText={props.data.attributes.bodyText} />
 }
 
-export default withErrorBoundary(HeroSectionBlock)
+export default withErrorBoundary(AsideBLock)

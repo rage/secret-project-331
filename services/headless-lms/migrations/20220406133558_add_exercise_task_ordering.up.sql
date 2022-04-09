f9e32721a8a7824262edeaae709537999c97684e
Jori Lampi <jori.lampi@helsinki.fi>
2022-04-10 02:07:57 +0300

    Add column comment

--- services/headless-lms/migrations/20220406133558_add_exercise_task_ordering.up.sql
+++ services/headless-lms/migrations/20220406133558_add_exercise_task_ordering.up.sql
@@ -3,5 +3,6 @@ ALTER TABLE exercise_tasks
 ADD COLUMN order_number INTEGER NOT NULL DEFAULT 0;
 ALTER TABLE exercise_tasks
 ALTER COLUMN order_number DROP DEFAULT;
+COMMENT ON TABLE exercise_tasks.order_number IS 'The order in which the exercise task should appear in a slide.';
 CREATE UNIQUE INDEX exercise_tasks_order_number_uniqueness ON exercise_tasks (exercise_slide_id, order_number)
 WHERE deleted_at IS NULL;

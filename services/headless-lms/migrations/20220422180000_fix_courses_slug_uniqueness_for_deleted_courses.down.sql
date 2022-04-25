db7ab31b40718d47fac78c23d47bf2c2554a1765
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2022-04-25 11:51:06 +0200

    Try up

--- services/headless-lms/migrations/20220422180000_fix_courses_slug_uniqueness_for_deleted_courses.down.sql
+++ services/headless-lms/migrations/20220422180000_fix_courses_slug_uniqueness_for_deleted_courses.down.sql
@@ -1,4 +1,4 @@
 -- Add down migration script here
 DROP INDEX courses_slug_key_when_not_deleted;
 ALTER TABLE courses
-ADD CONSTRANT courses_slug_key UNIQUE (slug);
+ADD CONSTRAINT courses_slug_key UNIQUE (slug);
--- services/headless-lms/migrations/20220422180000_fix_courses_slug_uniqueness_for_deleted_courses.up.sql
+++ services/headless-lms/migrations/20220422180000_fix_courses_slug_uniqueness_for_deleted_courses.up.sql
@@ -1,3 +1,3 @@
 ALTER TABLE courses DROP CONSTRAINT courses_slug_key;
 CREATE UNIQUE INDEX courses_slug_key_when_not_deleted ON courses (slug)
-WHERE deleted_at IS NOT NULL;
+WHERE deleted_at IS NT NULL;

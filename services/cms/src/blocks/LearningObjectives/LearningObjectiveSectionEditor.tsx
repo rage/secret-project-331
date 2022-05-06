bdc96c7a9b4f8221c6723ec866af80d868c4b157
iammisan <george.eyoyibo@gmail.com>
2022-05-06 13:18:09 +0300

    + learning-objective integration

---
+++ services/cms/src/blocks/LearningObjectives/LearningObjectiveSectionEditor.tsx
@@ -0,0 +1,82 @@
+import { css } from "@emotion/css"
+import { InnerBlocks, RichText } from "@wordpress/block-editor"
+import { BlockEditProps, Template } from "@wordpress/blocks"
+import React from "react"
+
+import BreakFromCentered from "../../shared-module/components/Centering/BreakFromCentered"
+import { baseTheme } from "../../shared-module/styles"
+import {
+  CMS_EDITOR_SIDEBAR_THRESHOLD,
+  CMS_EDITOR_SIDEBAR_WIDTH,
+} from "../../shared-module/utils/constants"
+import BlockWrapper from "../BlockWrapper"
+
+import { LearningObjectiveSectionAttributes } from "."
+
+const ALLOWED_NESTED_BLOCKS = ["core/heading", "core/buttons", "core/button", "core/paragraph", "core/list", "core/list-item"]
+const LEARNING_OBJECTIVE_SECTION_TEMPLATE: Template[] = [
+  [
+    "core/columns",
+    { isStackedOnMobile: true },
+    [
+      [
+        "core/column",
+        {},
+        [
+          ["core/list-item", { placeholder: "Insert text...", align: "center" }],
+        ],
+      ],
+      [
+        "core/column",
+        {},
+        [
+          ["core/list-item", { placeholder: "Insert text...", align: "center" }],
+        ],
+      ],
+    ],
+  ],
+]
+
+const LearningObjectiveSectionEditor: React.FC<BlockEditProps<LearningObjectiveSectionAttributes>> = ({
+  clientId,
+  attributes,
+  setAttributes,
+}) => {
+  const { title } = attributes
+  return (
+    <BlockWrapper id={clientId}>
+      <BreakFromCentered
+        sidebar
+        sidebarPosition="right"
+        sidebarWidth={CMS_EDITOR_SIDEBAR_WIDTH}
+        sidebarThreshold={CMS_EDITOR_SIDEBAR_THRESHOLD}
+      >
+        <div
+          className={css`
+            background: ${baseTheme.colors.clear[100]};
+            width: 100%;
+            border-radius: 1px;
+            transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+            padding: 7.5em 1em;
+          `}
+        >
+          <RichText
+            className="has-text-align-center wp-block-heading"
+            // eslint-disable-next-line i18next/no-literal-string
+            tagName="h2"
+            value={title}
+            onChange={(value: string) => setAttributes({ title: value })}
+            // eslint-disable-next-line i18next/no-literal-string
+            placeholder={"What you will learn in this chapter..."}
+          />
+          <InnerBlocks
+            template={LEARNING_OBJECTIVE_SECTION_TEMPLATE}
+            allowedBlocks={ALLOWED_NESTED_BLOCKS}
+          />
+        </div>
+      </BreakFromCentered>
+    </BlockWrapper>
+  )
+}
+
+export default LearningObjectiveSectionEditor

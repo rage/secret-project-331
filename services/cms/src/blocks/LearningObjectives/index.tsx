daec4e9e3433de34b3c333591c61c9f0f4675509
iammisan <george.eyoyibo@gmail.com>
2022-05-09 14:13:28 +0300

    update learningObjectives

--- services/cms/src/blocks/LearningObjectives/index.tsx
+++ services/cms/src/blocks/LearningObjectives/index.tsx
@@ -10,20 +10,22 @@ export interface LearningObjectiveSectionAttributes {
   title: string
 }
 
-const LearningObjectiveSectionConfiguration: BlockConfiguration<LearningObjectiveSectionAttributes> = {
-  title: "Learning Objective Section",
-  description: "Learning Objective section where you describe what you will learn in this chapter/page.",
-  category: MOOCFI_CATEGORY_SLUG,
-  attributes: {
-    title: {
-      type: "string",
-      source: "html",
-      selector: "h2",
-      default: "What you will learn in this chapter...",
+const LearningObjectiveSectionConfiguration: BlockConfiguration<LearningObjectiveSectionAttributes> =
+  {
+    title: "Learning Objective Section",
+    description:
+      "Learning Objective section where you describe what you will learn in this chapter/page.",
+    category: MOOCFI_CATEGORY_SLUG,
+    attributes: {
+      title: {
+        type: "string",
+        source: "html",
+        selector: "h2",
+        default: "What you will learn in this chapter...",
+      },
     },
-  },
-  edit: LearningObjectiveSectionEditor,
-  save: LearningObjectiveSectionSave,
-}
+    edit: LearningObjectiveSectionEditor,
+    save: LearningObjectiveSectionSave,
+  }
 
 export default LearningObjectiveSectionConfiguration

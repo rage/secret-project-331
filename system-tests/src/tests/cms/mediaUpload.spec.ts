4c397e541fa7530a8b5e86755c704724dad53fa4
Henrik Nygren <nygrenh@gmail.com>
2022-05-09 13:43:32 +0300

    Fix tests

--- system-tests/src/tests/cms/mediaUpload.spec.ts
+++ system-tests/src/tests/cms/mediaUpload.spec.ts
@@ -50,11 +50,7 @@ test.describe("Uploading media as admin", async () => {
     ])
     expect(page.url().startsWith("http://project-331.local/cms/pages/")).toBe(true)
 
-    await page
-      .locator(
-        '[aria-label="Empty block\\; start writing or type forward slash to choose a block"]',
-      )
-      .type(`/image`)
+    await page.locator(`[aria-label="Add default block"]`).type(`/image`)
 
     // Click :nth-match(:text("Image"), 2)
     await page.click('text="Image"')

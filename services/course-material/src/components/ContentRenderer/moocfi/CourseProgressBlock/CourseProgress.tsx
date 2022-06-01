80fe475ecec376f474d251237fd9a7f906071ecf
Jori Lampi <jori.lampi@helsinki.fi>
2022-06-01 17:17:59 +0300

    Refactor CourseProgressBlock

--- services/course-material/src/components/ContentRenderer/moocfi/CourseProgressBlock/CourseProgress.tsx
+++ services/course-material/src/components/ContentRenderer/moocfi/CourseProgressBlock/CourseProgress.tsx
@@ -2,17 +2,14 @@ import { css } from "@emotion/css"
 import styled from "@emotion/styled"
 import React from "react"
 import { useTranslation } from "react-i18next"
-import { useQuery } from "react-query"
 
-import { fetchUserCourseProgress } from "../../../../services/backend"
+import { UserCourseInstanceProgress } from "../../../../shared-module/bindings"
 import Progress from "../../../../shared-module/components/CourseProgress"
-import ErrorBanner from "../../../../shared-module/components/ErrorBanner"
-import Spinner from "../../../../shared-module/components/Spinner"
 
 import ModuleProgress from "./ModuleProgress"
 
-interface CourseProgressProps {
-  courseInstanceId: string
+export interface CourseProgressProps {
+  userCourseInstanceProgress: UserCourseInstanceProgress
 }
 
 const Wrapper = styled.div`
@@ -21,56 +18,42 @@ const Wrapper = styled.div`
   padding: 2rem 4rem;
 `
 
-const CourseProgress: React.FC<CourseProgressProps> = ({ courseInstanceId }) => {
+const CourseProgress: React.FC<CourseProgressProps> = ({ userCourseInstanceProgress }) => {
   const { t } = useTranslation()
-  const getUserCourseProgress = useQuery(`course-instance-${courseInstanceId}-progress`, () =>
-    fetchUserCourseProgress(courseInstanceId),
-  )
-
   return (
     <>
-      {getUserCourseProgress.isError && (
-        <ErrorBanner variant={"readOnly"} error={getUserCourseProgress.error} />
-      )}
-      {(getUserCourseProgress.isLoading || getUserCourseProgress.isIdle) && (
-        <Spinner variant={"medium"} />
-      )}
-      {getUserCourseProgress.isSuccess && (
-        <>
-          <Wrapper>
-            <ModuleProgress
-              exercisesAnswered={getUserCourseProgress.data.attempted_exercises ?? ""}
-              exercisesNeededToAnswer={getUserCourseProgress.data.total_exercises ?? ""}
-              totalExercises={getUserCourseProgress.data.total_exercises ?? ""}
-            />
-          </Wrapper>
-          <Wrapper>
-            <div
-              className={css`
-                width: 100%;
-                margin: 0 auto;
-                text-align: center;
-                padding: 2em 0;
-              `}
-            >
-              {/* TODO: Verify how it looks when score_given is a floating number */}
-              <Progress
-                variant={"circle"}
-                max={getUserCourseProgress.data.score_maximum}
-                given={getUserCourseProgress.data.score_given}
-                point={50}
-                label={t("course-progress")}
-              />
-              <Progress
-                variant={"bar"}
-                showAsPercentage={true}
-                exercisesAttempted={getUserCourseProgress.data.attempted_exercises}
-                exercisesTotal={getUserCourseProgress.data.total_exercises}
-              />
-            </div>
-          </Wrapper>
-        </>
-      )}
+      <Wrapper>
+        <ModuleProgress
+          exercisesAnswered={userCourseInstanceProgress.attempted_exercises ?? ""}
+          exercisesNeededToAnswer={userCourseInstanceProgress.total_exercises ?? ""}
+          totalExercises={userCourseInstanceProgress.total_exercises ?? ""}
+        />
+      </Wrapper>
+      <Wrapper>
+        <div
+          className={css`
+            width: 100%;
+            margin: 0 auto;
+            text-align: center;
+            padding: 2em 0;
+          `}
+        >
+          {/* TODO: Verify how it looks when score_given is a floating number */}
+          <Progress
+            variant={"circle"}
+            max={userCourseInstanceProgress.score_maximum}
+            given={userCourseInstanceProgress.score_given}
+            point={50}
+            label={t("course-progress")}
+          />
+          <Progress
+            variant={"bar"}
+            showAsPercentage={true}
+            exercisesAttempted={userCourseInstanceProgress.attempted_exercises}
+            exercisesTotal={userCourseInstanceProgress.total_exercises}
+          />
+        </div>
+      </Wrapper>
     </>
   )
 }
--- services/course-material/src/components/ContentRenderer/moocfi/CourseProgressBlock/index.tsx
+++ services/course-material/src/components/ContentRenderer/moocfi/CourseProgressBlock/index.tsx
@@ -1,8 +1,11 @@
 import { useContext } from "react"
 import { useTranslation } from "react-i18next"
+import { useQuery } from "react-query"
 
 import { BlockRendererProps } from "../.."
 import PageContext from "../../../../contexts/PageContext"
+import { fetchUserCourseProgress } from "../../../../services/backend"
+import ErrorBanner from "../../../../shared-module/components/ErrorBanner"
 import Spinner from "../../../../shared-module/components/Spinner"
 import withErrorBoundary from "../../../../shared-module/utils/withErrorBoundary"
 
@@ -20,7 +23,30 @@ const CourseProgressBlock: React.FC<BlockRendererProps<unknown>> = () => {
     return <div>{t("select-course-version-to-see-your-progress")}</div>
   }
 
-  return <CourseProgress courseInstanceId={pageContext.instance.id} />
+  return <QueryWrapper courseInstanceId={pageContext.instance.id} />
 }
 
 export default withErrorBoundary(CourseProgressBlock)
+
+interface QueryWrapperProps {
+  courseInstanceId: string
+}
+
+const QueryWrapper: React.FC<QueryWrapperProps> = ({ courseInstanceId }) => {
+  const getUserCourseProgress = useQuery(`course-instance-${courseInstanceId}-progress`, () =>
+    fetchUserCourseProgress(courseInstanceId),
+  )
+  return (
+    <>
+      {getUserCourseProgress.isError && (
+        <ErrorBanner variant={"readOnly"} error={getUserCourseProgress.error} />
+      )}
+      {(getUserCourseProgress.isLoading || getUserCourseProgress.isIdle) && (
+        <Spinner variant={"medium"} />
+      )}
+      {getUserCourseProgress.isSuccess && (
+        <CourseProgress userCourseInstanceProgress={getUserCourseProgress.data} />
+      )}
+    </>
+  )
+}

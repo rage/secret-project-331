2735567c65e3c810422df2155f81ebcca259922a
Jori Lampi <jori.lampi@helsinki.fi>
2022-06-09 18:19:34 +0300

    Add study registry registrars table

--- services/headless-lms/migrations/20220609124028_add_course_module_completion_tables.down.sql
+++ services/headless-lms/migrations/20220609124028_add_course_module_completion_tables.down.sql
@@ -1,2 +1,4 @@
+-- 2. Drop stydy_registry_registrars
+DROP TABLE study_registry_registrars;
 -- 1. Remove course_module_completions
 DROP TABLE course_module_completions;
--- services/headless-lms/models/src/lib.rs
+++ services/headless-lms/models/src/lib.rs
@@ -41,6 +41,7 @@ pub mod proposed_block_edits;
 pub mod proposed_page_edits;
 pub mod regradings;
 pub mod roles;
+pub mod study_registry_registrars;
 pub mod url_redirections;
 pub mod user_course_settings;
 pub mod user_exercise_slide_states;

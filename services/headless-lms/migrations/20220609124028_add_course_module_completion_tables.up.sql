35669dd7452499ad25f03b6d36f0e5c07fbf7edd
Jori Lampi <jori.lampi@helsinki.fi>
2022-06-09 19:19:15 +0300

    Add language to completions, fix delete query

--- services/headless-lms/migrations/20220609124028_add_course_module_completion_tables.up.sql
+++ services/headless-lms/migrations/20220609124028_add_course_module_completion_tables.up.sql
@@ -9,12 +9,13 @@ CREATE TABLE course_module_completions (
   user_id UUID NOT NULL REFERENCES users(id),
   completion_date TIMESTAMP WITH TIME ZONE,
   completion_registration_attempt_date TIMESTAMP WITH TIME ZONE,
+  completion_language VARCHAR(255) NOT NULL,
   eligible_for_ects BOOLEAN NOT NULL,
   email VARCHAR(255) NOT NULL
 );
 CREATE TRIGGER set_timestamp BEFORE
 UPDATE ON course_module_completions FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
-COMMENT ON TABLE course_module_completions IS '';
+COMMENT ON TABLE course_module_completions IS 'Internal student completions for course modules.';
 COMMENT ON COLUMN course_module_completions.id IS 'A unique, stable identifier for the record.';
 COMMENT ON COLUMN course_module_completions.created_at IS 'Timestamp when the record was created.';
 COMMENT ON COLUMN course_module_completions.updated_at IS 'Timestamp when the record was last updated. The field is updated automatically by the set_timestamp trigger.';
@@ -24,6 +25,7 @@ COMMENT ON COLUMN course_module_completions.course_module_id IS 'Part of a cours
 COMMENT ON COLUMN course_module_completions.user_id IS 'User who the completion is registered for.';
 COMMENT ON COLUMN course_module_completions.completion_date IS 'The date when a teacher has manually marked the course module as completed. For example, this may be the day of when an exam for the course took place.';
 COMMENT ON COLUMN course_module_completions.completion_registration_attempt_date IS 'For courses with a student self-registration. As per convention stated in course materials, this is actually the date of the course registration.';
+COMMENT ON COLUMN course_module_completions.completion_language IS 'The language used in the completion of the course.';
 COMMENT ON COLUMN course_module_completions.eligible_for_ects IS 'Whether or not the student can receive study credits for this completion.';
 COMMENT ON COLUMN course_module_completions.email IS 'Email at the time of completing the course. Used to match the student to the data that they will fill to the open university and it will remain unchanged in the event of email change because changing this would break the matching.';
 -- 2. Create table study_registry_registrars

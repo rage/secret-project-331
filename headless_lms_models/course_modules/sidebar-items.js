02606403a3dec4822b7c365a0c1f984e42fb7adb
nygrenh <nygrenh@gmail.com>
2022-06-09 13:26:17 +0000

    Deploying to gh-pages from  @ 61751cbeeea7d1024416600f76121ed7c928318c 🚀

--- headless_lms_models/course_modules/sidebar-items.js
+++ headless_lms_models/course_modules/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["delete",""],["for_course",""],["new",""],["rename",""],["reorder",""]],"struct":[["Module",""]]});
+initSidebarItems({"fn":[["delete",""],["get_by_course_id",""],["get_by_course_id_as_map","Gets course modules for the given course as a map, indexed by the `id` field."],["get_default_by_course_id",""],["insert",""],["insert_default_for_course",""],["rename",""],["reorder",""]],"struct":[["Module",""]]});

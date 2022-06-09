777f30e515d84a6e0bebce5d0f000b059038f831
Jori Lampi <jori.lampi@helsinki.fi>
2022-06-09 18:19:34 +0300

    Add study registry registrars table

--- services/headless-lms/migrations/20220609124028_add_course_module_completion_tables.up.sql
+++ services/headless-lms/migrations/20220609124028_add_course_module_completion_tables.up.sql
@@ -26,3 +26,21 @@ COMMENT ON COLUMN course_module_completions.completion_date IS 'The date when a 
 COMMENT ON COLUMN course_module_completions.completion_registration_attempt_date IS 'For courses with a student self-registration. As per convention stated in course materials, this is actually the date of the course registration.';
 COMMENT ON COLUMN course_module_completions.eligible_for_ects IS 'Whether or not the student can receive study credits for this completion.';
 COMMENT ON COLUMN course_module_completions.email IS 'Email at the time of completing the course. Used to match the student to the data that they will fill to the open university and it will remain unchanged in the event of email change because changing this would break the matching.';
+-- 2. Add table study_registry_registrars
+CREATE TABLE study_registry_registrars (
+  id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+  deleted_at TIMESTAMP WITH TIME ZONE,
+  secret_key VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+CREATE TRIGGER set_timestamp BEFORE
+UPDATE ON study_registry_registrars FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
+COMMENT ON TABLE study_registry_registrars IS 'Authorized third parties that can access course module completions for study registration purposes.';
+COMMENT ON COLUMN study_registry_registrars.id IS 'A unique, stable identifier for the record.';
+COMMENT ON COLUMN study_registry_registrars.created_at IS 'Timestamp when the record was created.';
+COMMENT ON COLUMN study_registry_registrars.updated_at IS 'Timestamp when the record was last updated. The field is updated automatically by the set_timestamp trigger.';
+COMMENT ON COLUMN study_registry_registrars.deleted_at IS 'Timestamp when the record was deleted. If null, the record is not deleted.';
+COMMENT ON COLUMN study_registry_registrars.secret_key IS 'The secret key used to authenticate for the registry.';
+COMMENT ON COLUMN study_registry_registrars.name IS 'The name of the registrar.';
---
+++ services/headless-lms/models/src/study_registry_registrars.rs
@@ -0,0 +1,62 @@
+use crate::prelude::*;
+
+#[derive(Clone, PartialEq, Eq, Deserialize, Serialize)]
+pub struct StudyRegistryRegistrar {
+    pub id: Uuid,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+    pub deleted_at: Option<DateTime<Utc>>,
+    pub name: String,
+    pub secret_key: String,
+}
+
+pub async fn insert(
+    conn: &mut PgConnection,
+    name: &str,
+    secret_key: &str,
+    test_only_fixed_id: Option<Uuid>,
+) -> ModelResult<Uuid> {
+    let res = sqlx::query!(
+        "
+INSERT INTO study_registry_registrars (id, name, secret_key)
+VALUES (COALESCE($1, uuid_generate_v4()), $2, $3)
+RETURNING id
+    ",
+        test_only_fixed_id,
+        name,
+        secret_key
+    )
+    .fetch_one(conn)
+    .await?;
+    Ok(res.id)
+}
+
+pub async fn get_by_id(conn: &mut PgConnection, id: Uuid) -> ModelResult<StudyRegistryRegistrar> {
+    let res = sqlx::query_as!(
+        StudyRegistryRegistrar,
+        "
+SELECT *
+FROM study_registry_registrars
+WHERE id = $1
+  AND deleted_at IS NULL
+        ",
+        id,
+    )
+    .fetch_one(conn)
+    .await?;
+    Ok(res)
+}
+
+pub async fn delete(conn: &mut PgConnection, id: Uuid) -> ModelResult<()> {
+    sqlx::query!(
+        "
+UPDATE study_registry_registrars
+SET deleted_at = now()
+WHERE id = $1
+        ",
+        id,
+    )
+    .execute(conn)
+    .await?;
+    Ok(())
+}

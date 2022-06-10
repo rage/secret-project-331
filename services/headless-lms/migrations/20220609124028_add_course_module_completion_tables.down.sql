81d9604a8d615c782d435c2a4a9c8f74abc1c42c
Jori Lampi <jori.lampi@helsinki.fi>
2022-06-10 16:39:22 +0300

    Add generated passed-field

--- services/headless-lms/migrations/20220609124028_add_course_module_completion_tables.down.sql
+++ services/headless-lms/migrations/20220609124028_add_course_module_completion_tables.down.sql
@@ -1,7 +1,9 @@
--- 4. 3. 2. Drop new tables
+-- 5. 4. 3. Drop new tables
 DROP TABLE course_module_completion_study_registry_registrations;
 DROP TABLE study_registry_registrars;
 DROP TABLE course_module_completions;
+-- 2. Drop new function
+DROP FUNCTION check_grade_passed;
 -- 1. Drop new types
 DROP TYPE grade_local_id;
 DROP TYPE grade_scale_id;

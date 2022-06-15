0e41de096405306c8f501bef2a4f0f8b840494cc
Jori Lampi <jori.lampi@helsinki.fi>
2022-06-15 14:49:14 +0300

    Authorize accessing completions

--- services/headless-lms/server/src/controllers/study_registry/completions.rs
+++ services/headless-lms/server/src/controllers/study_registry/completions.rs
@@ -7,20 +7,23 @@ use tokio_stream::{wrappers::UnboundedReceiverStream, StreamExt};
 
 use crate::{
     controllers::prelude::*,
-    domain::{
-        authorization::skip_authorize,
-        csv_export::{make_authorized_streamable, JsonStreamer},
-    },
+    domain::csv_export::{make_authorized_streamable, JsonStreamer},
 };
 
-#[instrument(skip(pool))]
+#[instrument(skip(req, pool))]
 async fn get_completions(
+    req: HttpRequest,
     course_id: web::Path<Uuid>,
     pool: web::Data<PgPool>,
 ) -> ControllerResult<HttpResponse> {
     let mut conn = pool.acquire().await?;
-    // TODO: No skip, only doing like this because merging master
-    let token = skip_authorize()?;
+    let raw_token = req
+        .headers()
+        .get("Authorization")
+        .map_or(Ok(""), |x| x.to_str())
+        .map_err(|_| ControllerError::Forbidden("Access denied.".to_string()))?;
+    let secret_key = parse_secret_key_from_token(raw_token)?.to_string();
+    let token = authorize(&mut conn, Act::View, None, Res::StudyRegistry(secret_key)).await?;
     let course_module =
         models::course_modules::get_default_by_course_id(&mut conn, *course_id).await?;
     let (sender, receiver) = tokio::sync::mpsc::unbounded_channel::<ControllerResult<Bytes>>();
@@ -39,6 +42,17 @@ async fn get_completions(
                 receiver,
             ))),
     )
+}
+
+fn parse_secret_key_from_token(token: &str) -> Result<&str, ControllerError> {
+    if !token.starts_with("Basic") {
+        return Err(ControllerError::Forbidden("Access denied".to_string()));
+    }
+    let secret_key = token
+        .split(' ')
+        .nth(1)
+        .ok_or_else(|| anyhow::anyhow!("Malformed authorization token"))?;
+    Ok(secret_key)
 }
 
 // I tired to have most of this in JsonStreamer where you pass a stream but it broke and I don't know why :(

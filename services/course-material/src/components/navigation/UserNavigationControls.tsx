d95cec9cb866b27d1d7ffb13cdc248e1ff6305ef
iammisan <george.eyoyibo@gmail.com>
2022-06-16 09:54:14 +0300

    + mooc signup link in course-material

--- services/course-material/src/components/navigation/UserNavigationControls.tsx
+++ services/course-material/src/components/navigation/UserNavigationControls.tsx
@@ -64,9 +64,11 @@ const UserNavigationControls: React.FC<UserNavigationControlsProps> = ({
   ) : (
     <>
       <li className={cx(styles)}>
-        <Button size="medium" variant="primary">
-          {t("create-new-account")}
-        </Button>
+        <a href="https://www.mooc.fi/en/signup/">
+          <Button size="medium" variant="primary">
+            {t("create-new-account")}
+          </Button>
+        </a>
       </li>
       <li className={cx(styles)}>
         <a href={returnToPath}>

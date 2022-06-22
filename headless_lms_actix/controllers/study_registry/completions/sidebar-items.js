4f287b7d313157ad5af182ecd9186d9cd2566d9f
nygrenh <nygrenh@gmail.com>
2022-06-22 08:30:54 +0000

    Deploying to gh-pages from  @ 04947011c56e64e384a633fae4d2da6252073540 🚀

---
+++ headless_lms_actix/controllers/study_registry/completions/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["_add_routes","Add a route for each controller in this module."],["get_completions","GET `/api/v0/main-frontend/study-registry/completions/:course_id`"],["get_module_completions","GET `/api/v0/main-frontend/study-registry/completions/:course_id/:course_module_id`"]]});

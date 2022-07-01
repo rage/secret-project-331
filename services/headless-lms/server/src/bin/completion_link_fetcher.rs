e49dcc3ff70ddcaf6062bcb43a82ec4fdab7c569
Jori Lampi <jori.lampi@helsinki.fi>
2022-07-01 11:39:45 +0300

    Read course codes from database

--- services/headless-lms/server/src/bin/completion_link_fetcher.rs
+++ services/headless-lms/server/src/bin/completion_link_fetcher.rs
@@ -3,6 +3,8 @@ use std::env;
 use anyhow::Result;
 use dotenv::dotenv;
 use headless_lms_actix::setup_tracing;
+use headless_lms_models as models;
+use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -11,6 +13,13 @@ async fn main() -> Result<()> {
     setup_tracing()?;
 
     tracing::info!("Hello from fetcher");
+
+    let database_url = env::var("DATABASE_URL")
+        .unwrap_or_else(|_| "postgres://localhost/headless_lms_dev".to_string());
+    let db_pool = PgPool::connect(&database_url).await?;
+    let mut conn = db_pool.acquire().await?;
+    let course_codes = models::course_modules::get_all_uh_course_codes(&mut conn).await?;
+    tracing::info!("Course codes {:?}", course_codes);
 
     Ok(())
 }

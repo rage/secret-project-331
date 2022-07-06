34bcb27a11408e4831eca68b16055e66f0bef667
nygrenh <nygrenh@gmail.com>
2022-07-06 07:23:00 +0000

    Deploying to gh-pages from  @ 0764e14a49b02fa1946e36d7ef59fdb55942eb95 🚀

--- seed/sidebar-items.js
+++ seed/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["create_best_exercise",""],["create_exam",""],["create_page",""],["example_exercise_flexible",""],["main",""],["paragraph",""],["quizzes_exercise",""],["seed_cs_course_material",""],["seed_sample_course",""],["submit_and_grade",""]]});
+initSidebarItems({"fn":[["create_best_exercise",""],["create_exam",""],["create_page",""],["example_exercise_flexible",""],["heading",""],["main",""],["paragraph",""],["quizzes_exercise",""],["seed_cs_course_material",""],["seed_sample_course",""],["submit_and_grade",""]]});

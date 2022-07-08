4809c9c25d60b1674683f04cb73519e2dda3fa69
iammisan <george.eyoyibo@gmail.com>
2022-07-08 13:13:31 +0300

    Add top-level-pages placeholder in CMS

--- services/cms/src/blocks/TopLevelPage/TopLevelPageEditor.tsx
+++ services/cms/src/blocks/TopLevelPage/TopLevelPageEditor.tsx
@@ -1,18 +1,20 @@
 import { InnerBlocks } from "@wordpress/block-editor"
 import { BlockEditProps } from "@wordpress/blocks"
 import React from "react"
+import { useTranslation } from "react-i18next"
 
-import BlockWrapper from "../BlockWrapper"
+import BlockPlaceholderWrapper from "../BlockPlaceholderWrapper"
 
 const ALLOWED_NESTED_BLOCKS = [""]
 
 const TopLevelPageEditor: React.FC<BlockEditProps<Record<string, never>>> = ({ clientId }) => {
+  const { t } = useTranslation()
   return (
-    <BlockWrapper id={clientId}>
-      <div>
-        <InnerBlocks allowedBlocks={ALLOWED_NESTED_BLOCKS} />
-      </div>
-    </BlockWrapper>
+    <BlockPlaceholderWrapper id={clientId}>
+      <h3>{t("top-level-block-placeholder")}</h3>
+      <p>{t("top-level-block-placeholder-explanation")}</p>
+      <InnerBlocks allowedBlocks={ALLOWED_NESTED_BLOCKS} />
+    </BlockPlaceholderWrapper>
   )
 }
 

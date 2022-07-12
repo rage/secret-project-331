9f1df7a52cd85946ef9ca3a6df64a72d85cba414
Jori Lampi <jori.lampi@helsinki.fi>
2022-07-12 10:31:10 +0300

    Improve study registry documentation (#873)

--- services/headless-lms/server/src/controllers/study_registry/mod.rs
+++ services/headless-lms/server/src/controllers/study_registry/mod.rs
@@ -1,8 +1,16 @@
 /*!
 Handlers for HTTP requests to `/api/v0/study-registry`.
 
-This documents all endpoints. Select a module below for a category.
+The study registry provides an access to student completion records. It is generally only available
+to authorized study registries, meaning that most endpoints will require a valid authorization token
+to access.
 
+When accessing study registry, the authorization token should be given as the following header:
+```http
+Authorization: Basic documentationOnlyExampleSecretKey-12345
+```
+
+For more details, please view the submodules.
 */
 
 use actix_web::web::{self, ServiceConfig};
@@ -11,6 +19,7 @@ pub mod completion_registered_to_study_registry;
 pub mod completions;
 
 /// Add controllers from all the submodules.
+#[doc(hidden)]
 pub fn _add_routes(cfg: &mut ServiceConfig) {
     cfg.service(
         web::scope("/completion-registered-to-study-registry")

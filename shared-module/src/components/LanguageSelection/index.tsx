a5fee9cdd7ee616ce10c90e409a946f1d2d2f161
george-misan <george.eyoyibo@gmail.com>
2022-07-25 12:39:57 +0100

    retain selectedlang in cookie on page reload--no-verify

--- shared-module/src/components/LanguageSelection/index.tsx
+++ shared-module/src/components/LanguageSelection/index.tsx
@@ -36,10 +36,14 @@ const LanguageSelection: React.FC<LanguageSelectionProps> = ({ placement }) => {
     modifiers: [{ name: ARROW, options: { element: arrowElement } }],
   })
   const { i18n, t } = useTranslation()
+  const LANGUAGE_COOKIE_KEY = "selected-language"
 
   const handleLanguageChange = (newLanguage: string) => {
     i18n.changeLanguage(newLanguage)
     setVisible(false)
+    const selectedLanguage = newLanguage.split("-")
+    // eslint-disable-next-line i18next/no-literal-string
+    document.cookie = `${LANGUAGE_COOKIE_KEY}=${selectedLanguage[0]}; path=/; SameSite=Strict; max-age=31536000;`
   }
 
   return (

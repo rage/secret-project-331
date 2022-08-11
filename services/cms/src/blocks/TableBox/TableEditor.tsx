479ca13af9ee6084d4d3851bdd719d52b9701319
george-misan <george.eyoyibo@gmail.com>
2022-08-11 10:22:21 +0100

    + caption

--- services/cms/src/blocks/TableBox/TableEditor.tsx
+++ services/cms/src/blocks/TableBox/TableEditor.tsx
@@ -1,7 +1,10 @@
+import { css } from "@emotion/css"
 import { InnerBlocks } from "@wordpress/block-editor"
 import { BlockEditProps, Template } from "@wordpress/blocks"
 import React from "react"
+import { useTranslation } from "react-i18next"
 
+import { baseTheme, headingFont } from "../../shared-module/styles"
 import BlockWrapper from "../BlockWrapper"
 
 const ALLOWED_NESTED_BLOCKS = ["core/table"]
@@ -11,9 +14,30 @@ const TABLE_TEMPLATE: Template[] = [["core/table", { title: "TableBox" }]]
 const TableEditor: React.FC<React.PropsWithChildren<BlockEditProps<Record<string, unknown>>>> = ({
   clientId,
 }) => {
+  const { t } = useTranslation()
   return (
     <BlockWrapper id={clientId}>
       <div>
+        <div
+          className={css`
+            padding: 1rem;
+            background: #ecf3f2;
+            text-align: center;
+            font-family: ${headingFont};
+          `}
+        >
+          <h4>{t("table-box")}</h4>
+          <span
+            className={css`
+              color: ${baseTheme.colors.green[600]};
+              text-align: center;
+              font-weight: 600;
+              font-family: ${headingFont};
+            `}
+          >
+            {t("table-box-description")}
+          </span>
+        </div>
         <InnerBlocks
           allowedBlocks={ALLOWED_NESTED_BLOCKS}
           template={TABLE_TEMPLATE}
--- services/course-material/src/components/ContentRenderer/moocfi/TableBox.tsx
+++ services/course-material/src/components/ContentRenderer/moocfi/TableBox.tsx
@@ -12,98 +12,43 @@ interface Cell {
 
 const TableBox: React.FC<React.PropsWithChildren<BlockRendererProps<unknown>>> = (props) => {
   const innerBlocks: any = props.data.innerBlocks[0]
+
   return (
     innerBlocks && (
       <div
         className={css`
-          background-color: ${baseTheme.colors.green[200]};
           margin: 1rem 0;
-          min-height: 100%;
-          padding-bottom: 6px;
-          overflow-x: auto;
         `}
       >
         <div
           className={css`
-            display: grid;
-            grid-template-columns: repeat(${innerBlocks.attributes?.head[0].cells.length}, 1fr);
-            min-height: 45px;
-            grid-gap: 5px;
+            background-color: ${baseTheme.colors.green[200]};
+            margin: 1rem 0;
+            min-height: 100%;
+            padding-bottom: 6px;
+            overflow-x: auto;
           `}
         >
-          {innerBlocks.attributes?.head[0].cells.map((item: Cell) => (
-            <div
-              className={css`
-                background-color: ${baseTheme.colors.green[500]};
-                display: flex;
-                align-items: center;
-                margin-bottom: 5px;
-                font-family: ${headingFont};
-                font-size: 18px;
-                font-weight: bold;
-                color: #ffffff;
-                padding: 12px 10px;
-              `}
-              key={item.content}
-            >
-              {item.content}
-            </div>
-          ))}
-        </div>
-
-        {innerBlocks.attributes?.body.map((item: any) => (
           <div
             className={css`
               display: grid;
-              grid-template-columns: repeat(${item.cells.length}, 1fr);
-              gap: 5px;
-              margin-bottom: 5px;
-
-              :last-of-type {
-                margin-bottom: 0;
-              }
-            `}
-            key={item.id}
-          >
-            {item.cells.map((o: Cell) => (
-              <div
-                className={css`
-                  background-color: #f9f9f9;
-                  display: flex;
-                  align-items: center;
-                  padding: 10px;
-                  color: #1a2333;
-                  font-family: ${primaryFont};
-                  font-size: 18px;
-                  font-weight: 500;
-                  height: auto;
-                `}
-                key={o.content}
-              >
-                {o.content}
-              </div>
-            ))}
-          </div>
-        ))}
-        {innerBlocks.attributes?.foot[0] && (
-          <div
-            className={css`
-              display: grid;
-              grid-template-columns: repeat(${innerBlocks.attributes?.foot[0].cells.length}, 1fr);
-              height: 45px;
+              grid-template-columns: repeat(${innerBlocks.attributes?.head[0].cells.length}, 1fr);
+              min-height: 45px;
               grid-gap: 5px;
             `}
           >
-            {innerBlocks.attributes?.foot[0].cells.map((item: Cell) => (
+            {innerBlocks.attributes?.head[0]?.cells.map((item: Cell) => (
               <div
                 className={css`
-                  background-color: ${baseTheme.colors.green[100]};
+                  background-color: ${baseTheme.colors.green[500]};
                   display: flex;
                   align-items: center;
                   margin-bottom: 5px;
+                  font-family: ${headingFont};
                   font-size: 18px;
-                  color: #1a2333;
-                  padding: 10px;
+                  font-weight: bold;
+                  color: #ffffff;
+                  padding: 12px 10px;
                 `}
                 key={item.content}
               >
@@ -111,6 +56,80 @@ const TableBox: React.FC<React.PropsWithChildren<BlockRendererProps<unknown>>> =
               </div>
             ))}
           </div>
+
+          {innerBlocks.attributes?.body.map((item: any) => (
+            <div
+              className={css`
+                display: grid;
+                grid-template-columns: repeat(${item.cells.length}, 1fr);
+                gap: 5px;
+                margin-bottom: 5px;
+
+                :last-of-type {
+                  margin-bottom: 0;
+                }
+              `}
+              key={item.id}
+            >
+              {item.cells.map((o: Cell) => (
+                <div
+                  className={css`
+                    background-color: #f9f9f9;
+                    display: flex;
+                    align-items: center;
+                    padding: 10px;
+                    color: ${baseTheme.colors.grey[600]};
+                    font-family: ${primaryFont};
+                    font-size: 18px;
+                    font-weight: 500;
+                    height: auto;
+                  `}
+                  key={o.content}
+                >
+                  {o.content}
+                </div>
+              ))}
+            </div>
+          ))}
+          {innerBlocks.attributes?.foot && innerBlocks.attributes?.foot[0] && (
+            <div
+              className={css`
+                display: grid;
+                grid-template-columns: repeat(${innerBlocks.attributes?.foot[0].cells.length}, 1fr);
+                height: 45px;
+                grid-gap: 5px;
+              `}
+            >
+              {innerBlocks.attributes?.foot[0].cells.map((item: Cell) => (
+                <div
+                  className={css`
+                    background-color: ${baseTheme.colors.green[100]};
+                    display: flex;
+                    align-items: center;
+                    margin-bottom: 5px;
+                    font-size: 18px;
+                    color: #1a2333;
+                    padding: 10px;
+                  `}
+                  key={item.content}
+                >
+                  {item.content}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+        {innerBlocks.attributes?.caption && (
+          <span
+            className={css`
+              font-size: 16px;
+              display: block;
+              text-align: center;
+              color: ${baseTheme.colors.grey[600]};
+            `}
+          >
+            {innerBlocks.attributes.caption}
+          </span>
         )}
       </div>
     )

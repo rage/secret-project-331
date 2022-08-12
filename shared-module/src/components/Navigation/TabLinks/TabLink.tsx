ebbf723b3f5b91a6b4c7e3e2e21b7e768539e3d3
george-misan <george.eyoyibo@gmail.com>
2022-08-12 15:08:38 +0100

    fix bug in Tablink isActiver hover state

--- shared-module/src/components/Navigation/TabLinks/TabLink.tsx
+++ shared-module/src/components/Navigation/TabLinks/TabLink.tsx
@@ -67,7 +67,7 @@ const TabLink: React.FC<React.PropsWithChildren<React.PropsWithChildren<TabLinkP
             box-shadow: rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px;
           }
           :hover {
-            background: ${theme.secondary.hoverBg};
+            background: ${isActive ? theme.secondary.activeBg : theme.secondary.hoverBg};
             color: ${theme.secondary.hoverText};
           }
         `}

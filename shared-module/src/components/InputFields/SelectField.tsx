04bdb3e74178dbd86d9cb5d8d28f7a2e51d79058
george-misan <george.eyoyibo@gmail.com>
2022-08-16 13:12:07 +0100

    update input fields padding

--- shared-module/src/components/InputFields/SelectField.tsx
+++ shared-module/src/components/InputFields/SelectField.tsx
@@ -67,7 +67,7 @@ const SelectField = <T extends string>({
             width: 100%;
             border: 1px solid #e0e0e0;
             border-radius: 3px;
-            padding: 10px 12px;
+            padding: 8px 10px 10px 10px;
             font-size: 18px;
             cursor: pointer;
             background: #f9f9f9;

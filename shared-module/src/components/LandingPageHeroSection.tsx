f2e4436784cad564f21a2d8e36376aad581d93c8
george-misan <george.eyoyibo@gmail.com>
2022-08-26 12:48:38 +0100

    make hero-section SVG less busy

--- shared-module/src/components/LandingPageHeroSection.tsx
+++ shared-module/src/components/LandingPageHeroSection.tsx
@@ -90,8 +90,9 @@ const LandingPageHeroSection: React.FC<
         border-radius: 1px;
         transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
         padding: 5em 1em;
-        background: ${baseTheme.colors.clear[100]};
-        ${backgroundColor && `background-color: ${backgroundColor};`}
+        ${backgroundColor
+          ? `background-color: ${backgroundColor};`
+          : `background: ${baseTheme.colors.clear[100]};`}
         ${backgroundImage &&
         `background-image: url(${backgroundImage});
         background-repeat: no-repeat;

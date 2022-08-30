7536db95ea43174a082079159a9f0858b8557716
Henrik Nygren <nygrenh@gmail.com>
2022-08-30 17:19:13 +0300

    Visualize completion requirements more clearly in the points visualization

---
+++ services/course-material/src/components/ContentRenderer/moocfi/CourseProgressBlock/CompletionRequirementsTabulation/index.tsx
@@ -0,0 +1,46 @@
+import styled from "@emotion/styled"
+import { useTranslation } from "react-i18next"
+
+import { baseTheme } from "../../../../../shared-module/styles"
+
+import HighlightItem from "./HilightItem"
+
+const HilightContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 1rem 0;
+`
+
+export interface CompletionRequirementsTabulationProps {
+  attemptedExercisesRequiredForCompletion: number | null
+  pointsRequiredForCompletion: number | null
+}
+
+const CompletionRequirementsTabulation: React.FC<
+  React.PropsWithChildren<CompletionRequirementsTabulationProps>
+> = ({ attemptedExercisesRequiredForCompletion, pointsRequiredForCompletion }) => {
+  const { t } = useTranslation()
+  return (
+    <>
+      <HilightContainer>
+        {pointsRequiredForCompletion && (
+          <HighlightItem
+            highlightColor={baseTheme.colors.green[700]}
+            highlightDescription={t("points-required-for-completion")}
+            highlightText={pointsRequiredForCompletion}
+          />
+        )}
+        {attemptedExercisesRequiredForCompletion && (
+          <HighlightItem
+            highlightColor={baseTheme.colors.blue[700]}
+            highlightDescription={t("attempted-exercises-required-for-completion")}
+            highlightText={attemptedExercisesRequiredForCompletion}
+            leftBorder={true}
+          />
+        )}
+      </HilightContainer>
+    </>
+  )
+}
+
+export default CompletionRequirementsTabulation

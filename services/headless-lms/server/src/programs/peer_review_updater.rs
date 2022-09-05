7cef15bb6f922d68c1dbd3f2c78751289b298816
Henrik Nygren <nygrenh@gmail.com>
2022-09-05 17:36:30 +0300

    Fixes

--- services/headless-lms/server/src/programs/peer_review_updater.rs
+++ services/headless-lms/server/src/programs/peer_review_updater.rs
@@ -1,8 +1,6 @@
 use crate::setup_tracing;
 use dotenv::dotenv;
-use headless_lms_models::{
-    peer_review_queue_entries,
-};
+use headless_lms_models::peer_review_queue_entries;
 use sqlx::{Connection, PgConnection};
 use std::env;
 
@@ -15,6 +13,7 @@ pub async fn main() -> anyhow::Result<()> {
     let mut conn = PgConnection::connect(&db_url).await?;
     let now = chrono::offset::Utc::now();
     let manual_review_cutoff = now - chrono::Duration::days(7 * 3);
+    // After this date, we will assume that the teacher has given up on reviewing answers, so we will consider queue entries older than this to have passed the peer review.
     let pass_automatically_cutoff = manual_review_cutoff - chrono::Duration::days(90);
     info!("Peer review updater started");
     // Doing the update in small parts so that we don't end up constructing too heavy queries and so that we can get more frequeent log messages about the progress
@@ -25,14 +24,14 @@ pub async fn main() -> anyhow::Result<()> {
         all_course_instances.len()
     );
 
-    for course_instance in all_course_instances {
+    for course_instance in all_course_instances.iter() {
         info!(
-            "Processing course instance {:?} from course {:?}",
+            "Finding answers to move to manual review for course instance {:?} from course {:?}",
             course_instance.id, course_instance.course_id
         );
 
         let should_be_added_to_manual_review = headless_lms_models::peer_review_queue_entries::get_entries_that_need_reviews_and_are_older_than(&mut conn, course_instance.id, manual_review_cutoff).await?;
-        info!("Found {:?} answers that have been added to the peer review queue before {:?} and have not received enough peer reviews. Adding them to be manually reviewed by the teachers", should_be_added_to_manual_review.len(), manual_review_cutoff);
+        info!("Found {:?} answers that have been added to the peer review queue before {:?} and have not received enough peer reviews or have not been reviewed manually. Adding them to be manually reviewed by the teachers.", should_be_added_to_manual_review.len(), manual_review_cutoff);
         for peer_review_queue_entry in should_be_added_to_manual_review {
             peer_review_queue_entries::remove_from_queue_and_add_to_manual_review(
                 &mut conn,
@@ -40,9 +39,15 @@ pub async fn main() -> anyhow::Result<()> {
             )
             .await?;
         }
+    }
 
+    for course_instance in all_course_instances.iter() {
+        info!(
+            "Finding answers that have been in peer review for so long that we need to give them full points for course instance {:?} from course {:?}",
+            course_instance.id, course_instance.course_id
+        );
         let should_pass = headless_lms_models::peer_review_queue_entries::get_entries_that_need_reviews_and_are_older_than(&mut conn, course_instance.id, pass_automatically_cutoff).await?;
-        info!("Found {:?} answers that have been added to the peer review queue before {:?} and have not received enough peer reviews. Adding them to be manually reviewed by the teachers", should_pass.len(), pass_automatically_cutoff);
+        info!("Found {:?} answers that have been added to the peer review queue before {:?} and have not received enough peer reviews or have not been reviewed manually. Giving them full points.", should_pass.len(), pass_automatically_cutoff);
         for peer_review_queue_entry in should_pass {
             let _res = peer_review_queue_entries::remove_from_queue_and_give_full_points(
                 &mut conn,
@@ -54,5 +59,3 @@ pub async fn main() -> anyhow::Result<()> {
     info!("All done!");
     Ok(())
 }
-
-// 3491 -3468

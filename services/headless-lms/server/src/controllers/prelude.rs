4efc0b97db03c3d7662e46fd338150af6e23b1a2
Henrik Nygren <nygrenh@gmail.com>
2022-09-13 15:06:10 +0300

    WIP

--- services/headless-lms/server/src/controllers/prelude.rs
+++ services/headless-lms/server/src/controllers/prelude.rs
@@ -2,13 +2,12 @@
 //! Intended to be glob-imported like `use crate::controllers::prelude::*;`.
 
 pub use crate::controllers::helpers::media::{upload_media, StoreKind};
-pub use crate::controllers::{
-    ControllerError, ControllerErrorType, ControllerResult, UploadResult,
-};
+pub use crate::controllers::UploadResult;
 pub use crate::domain::authorization::{
     authorize, parse_secret_key_from_header, skip_authorize, Action as Act, AuthUser,
     Resource as Res,
 };
+pub use crate::domain::error::{ControllerError, ControllerErrorType, ControllerResult};
 pub use crate::generated_docs;
 pub use actix_multipart::Multipart;
 pub use actix_web::web::{self, ServiceConfig};
--- services/headless-lms/server/src/domain/mod.rs
+++ services/headless-lms/server/src/domain/mod.rs
@@ -1,5 +1,6 @@
 pub mod authorization;
 pub mod csv_export;
+pub mod error;
 pub mod exercise_repositories;
 pub mod file_uploading;
 pub mod rate_limit_middleware_builder;

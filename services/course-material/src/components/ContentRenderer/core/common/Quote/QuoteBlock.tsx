58fa8a3b8ad7f35c66b923a2f7cd7c642c91f429
Henrik Nygren <nygrenh@gmail.com>
2022-10-04 17:20:35 +0300

    Remove quote is-style-large

--- services/course-material/src/components/ContentRenderer/core/common/Quote/QuoteBlock.tsx
+++ services/course-material/src/components/ContentRenderer/core/common/Quote/QuoteBlock.tsx
@@ -4,12 +4,11 @@ import { useContext } from "react"
 import { BlockRendererProps } from "../../.."
 import { QuoteAttributes } from "../../../../../../types/GutenbergBlockAttributes"
 import { GlossaryContext } from "../../../../../contexts/GlossaryContext"
-import { sanitizeCourseMaterialHtml } from "../../../../../utils/sanitizeCourseMaterialHtml"
 import InnerBlocks from "../../../util/InnerBlocks"
 import { parseText } from "../../../util/textParsing"
 
 const QuoteBlock: React.FC<BlockRendererProps<QuoteAttributes>> = (props) => {
-  const { citation, value, anchor, className, align } = props.data.attributes
+  const { citation, value, anchor, align } = props.data.attributes
   const { terms } = useContext(GlossaryContext)
 
   const styleLeftDefault = css`
@@ -29,69 +28,35 @@ const QuoteBlock: React.FC<BlockRendererProps<QuoteAttributes>> = (props) => {
     margin-bottom: 1.75em;
     text-align: center;
   `
-  if (className && className.includes("is-style-large")) {
-    return (
-      <>
-        <blockquote
-          className={css`
-            margin-bottom: 1rem;
-            padding: 0 1rem;
-          `}
-          cite={citation}
-          {...(anchor && { id: anchor })}
-        >
-          <div
-            className={css`
-              font-size: 1.5rem;
-              ${align && `text-align: ${align};`}
-              font-style: italic;
-            `}
-          >
-            {value && value}
-            <InnerBlocks parentBlockProps={props} />
-          </div>
-          <cite
-            className={css`
-              text-align: right;
-              display: block;
-              font-size: 1.125rem;
-              font-style: normal;
-            `}
-            dangerouslySetInnerHTML={{ __html: sanitizeCourseMaterialHtml(citation) }}
-          ></cite>
-        </blockquote>
-      </>
-    )
-  } else {
-    return (
-      <>
-        <blockquote
-          className={css`
-            ${((align && align === "left") || !align) && styleLeftDefault}
-            ${align && align === "right" && styleRightDefault}
+
+  return (
+    <>
+      <blockquote
+        className={css`
+          ${((align && align === "left") || !align) && styleLeftDefault}
+          ${align && align === "right" && styleRightDefault}
           ${align && align === "center" && styleCenterDefault}
           margin-bottom: 1.75rem;
+        `}
+        cite={citation}
+        {...(anchor && { id: anchor })}
+      >
+        <div>
+          {value && (!props.data.innerBlocks || props.data.innerBlocks.length === 0) && value}
+          <InnerBlocks parentBlockProps={props} />
+        </div>
+        <cite
+          className={css`
+            font-style: normal;
+            font-size: 0.8125rem;
           `}
-          cite={citation}
-          {...(anchor && { id: anchor })}
-        >
-          <div>
-            {value && (!props.data.innerBlocks || props.data.innerBlocks.length === 0) && value}
-            <InnerBlocks parentBlockProps={props} />
-          </div>
-          <cite
-            className={css`
-              font-style: normal;
-              font-size: 0.8125rem;
-            `}
-            dangerouslySetInnerHTML={{
-              __html: parseText(citation, terms).parsedText,
-            }}
-          />
-        </blockquote>
-      </>
-    )
-  }
+          dangerouslySetInnerHTML={{
+            __html: parseText(citation, terms).parsedText,
+          }}
+        />
+      </blockquote>
+    </>
+  )
 }
 
 export default QuoteBlock

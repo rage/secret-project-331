71baa5e0524bf0cad06816428f063d93a2b916c2
Heliozoa <daniel.x.martinez@helsinki.fi>
2022-11-21 10:46:06 +0200

    Use SpecRequest for model-solutions and public-spec requests

--- services/quizzes/src/pages/api/model-solution.ts
+++ services/quizzes/src/pages/api/model-solution.ts
@@ -1,9 +1,15 @@
+/* eslint-disable i18next/no-literal-string */
 import { NextApiRequest, NextApiResponse } from "next"
 
 import { ModelSolutionQuiz, Quiz } from "../../../types/types"
+import { SpecRequest } from "../../shared-module/bindings"
 
 export default (req: NextApiRequest, res: NextApiResponse<ModelSolutionQuiz>): void => {
-  const quiz: Quiz = req.body
+  const specRequest = req.body as SpecRequest
+  const quiz = specRequest.private_spec as Quiz | null
+  if (quiz === null) {
+    throw "Private spec cannot be null"
+  }
 
   const modelSolution = createModelSolution(quiz)
 

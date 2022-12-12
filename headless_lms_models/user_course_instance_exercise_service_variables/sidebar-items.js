46a4799745028d94ac6d5675d1518b76db6ca309
nygrenh <nygrenh@gmail.com>
2022-12-12 15:50:00 +0000

    Deploying to gh-pages from  @ b7b54fb7d394375df6e1ac0ffb61bbca4e297f3c 🚀

---
+++ headless_lms_models/user_course_instance_exercise_service_variables/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["get_all_variables_for_user_and_course_instance_or_exam",""],["insert_after_exercise_task_graded",""]],"struct":[["UserCourseInstanceExerciseServiceVariable",""]]};

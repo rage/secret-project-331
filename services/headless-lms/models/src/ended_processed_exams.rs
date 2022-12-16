a4dcbe7f8ae8acb566e0bc9fb33bb4e913c323d3
Jori Lampi <jori.lampi@helsinki.fi>
2022-12-16 16:19:15 +0200

    Add ended_processed_exams table

---
+++ services/headless-lms/models/src/ended_processed_exams.rs
@@ -0,0 +1,42 @@
+use crate::prelude::*;
+
+/// Creates a record for the given `exam_id` or makes sure that it is undeleted.
+pub async fn upsert(conn: &mut PgConnection, exam_id: Uuid) -> ModelResult<Uuid> {
+    let res = sqlx::query!(
+        "
+INSERT INTO ended_processed_exams(exam_id)
+VALUES ($1) ON CONFLICT (exam_id) DO
+UPDATE
+SET deleted_at = NULL
+RETURNING exam_id
+        ",
+        exam_id
+    )
+    .map(|x| x.exam_id)
+    .fetch_one(conn)
+    .await?;
+    Ok(res)
+}
+
+/// Get ids for exams that have ended but haven't yet been added to the table for processed ones.
+pub async fn get_unprocessed_ended_exams_by_timestamp(
+    conn: &mut PgConnection,
+    timestamp: DateTime<Utc>,
+) -> ModelResult<Vec<Uuid>> {
+    let res = sqlx::query!(
+        "
+SELECT exams.id
+FROM exams
+  LEFT JOIN ended_processed_exams ON (ended_processed_exams.exam_id = exams.id)
+WHERE exams.ends_at <= $1
+  AND ended_processed_exams.created_at IS NULL
+  AND exams.deleted_at IS NULL
+  AND ended_processed_exams.deleted_at IS NULL
+        ",
+        timestamp,
+    )
+    .map(|x| x.id)
+    .fetch_all(conn)
+    .await?;
+    Ok(res)
+}
--- services/headless-lms/server/src/programs/ended_exams_processor.rs
+++ services/headless-lms/server/src/programs/ended_exams_processor.rs
@@ -1,12 +1,26 @@
 use std::env;
 
 use crate::setup_tracing;
+use chrono::Utc;
 use dotenv::dotenv;
+use headless_lms_models as models;
+use sqlx::PgPool;
 
 pub async fn main() -> anyhow::Result<()> {
     env::set_var("RUST_LOG", "info,actix_web=info,sqlx=warn");
     dotenv().ok();
     setup_tracing()?;
-    tracing::info!("Hello, ended exams!");
+    let database_url = env::var("DATABASE_URL")
+        .unwrap_or_else(|_| "postgres://localhost/headless_lms_dev".to_string());
+    let db_pool = PgPool::connect(&database_url).await?;
+    let mut conn = db_pool.acquire().await?;
+    let now = Utc::now();
+    let exams =
+        models::ended_processed_exams::get_unprocessed_ended_exams_by_timestamp(&mut conn, now)
+            .await?;
+    tracing::info!(
+        "Found {} ended exams for completions processing.",
+        exams.len()
+    );
     Ok(())
 }

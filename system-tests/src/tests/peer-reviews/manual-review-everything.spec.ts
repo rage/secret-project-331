c3464a4f77bb75de4fcd135427dcfd57efed02db
Heikki Pulli <heikki.pulli@helsinki.fi>
2023-01-10 15:09:46 +0200

    comments

--- system-tests/src/tests/peer-reviews/manual-review-everything.spec.ts
+++ system-tests/src/tests/peer-reviews/manual-review-everything.spec.ts
@@ -178,6 +178,7 @@ test.describe("test ManualReviewEverything behavior", () => {
     await login("student2@example.com", "student.2", page2, true)
     await login("teacher@example.com", "teacher", page3, true)
 
+    // student 1 submits an answer
     await page1.goto("http://project-331.local/")
     await page1
       .getByRole("link", { name: "University of Helsinki, Department of Computer Science" })
@@ -201,6 +202,7 @@ test.describe("test ManualReviewEverything behavior", () => {
     await page1.frameLocator("iframe").getByRole("checkbox", { name: "a" }).click()
     await page1.getByRole("button", { name: "Submit" }).click()
 
+    // student 2 submits an answer
     await page2.goto("http://project-331.local/")
     await page2
       .getByRole("link", { name: "University of Helsinki, Department of Computer Science" })
@@ -220,6 +222,7 @@ test.describe("test ManualReviewEverything behavior", () => {
     await page2.frameLocator("iframe").getByRole("checkbox", { name: "b" }).click()
     await page2.getByRole("button", { name: "Submit" }).click()
 
+    // student 2 starts a peer review
     await expectScreenshotsToMatchSnapshots({
       headless,
       snapshotName: "student-2-before-filling-peer-review-1-multiple-submission",
@@ -242,6 +245,7 @@ test.describe("test ManualReviewEverything behavior", () => {
         await page2.getByText("ManualReviewEverything2").scrollIntoViewIfNeeded(),
     })
 
+    // student 1 starts a peer review
     await expectScreenshotsToMatchSnapshots({
       headless,
       snapshotName: "student-1-before-filling-peer-review-multiple-submission",
@@ -264,6 +268,7 @@ test.describe("test ManualReviewEverything behavior", () => {
         await page1.getByText("ManualReviewEverything2").scrollIntoViewIfNeeded(),
     })
 
+    // teacher checks the answers
     await page3.goto("http://project-331.local/")
     await page3.waitForTimeout(1000)
     await page3
@@ -280,6 +285,7 @@ test.describe("test ManualReviewEverything behavior", () => {
     await page3.getByRole("button", { name: "Full points" }).first().click()
     await page3.reload()
 
+    // Student 1 seeing the score
     await page1.reload()
     await expectScreenshotsToMatchSnapshots({
       headless,
@@ -293,6 +299,7 @@ test.describe("test ManualReviewEverything behavior", () => {
         await page1.getByText("ManualReviewEverything2").scrollIntoViewIfNeeded(),
     })
 
+    // Student 2 seeing the score
     await page2.reload()
     await expectScreenshotsToMatchSnapshots({
       headless,

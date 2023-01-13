22f7623d104c546a35f8eff77ee3a9990bc9aa1d
Henrik Nygren <nygrenh@gmail.com>
2023-01-13 16:10:23 +0200

    Optimize creating login states

--- system-tests/src/setup/globalSetup.ts
+++ system-tests/src/setup/globalSetup.ts
@@ -1,5 +1,6 @@
 import { chromium, FullConfig } from "@playwright/test"
 import { spawnSync } from "child_process"
+import { stat, statSync } from "fs"
 import path from "path"
 import which from "which"
 
@@ -13,6 +14,8 @@ async function globalSetup(config: FullConfig): Promise<void> {
   await setupSystemTestDb()
   await createLoginStates()
 }
+
+const ONE_WEEK_MS = 10 * 10 * 10 * 60 * 60 * 24 * 7
 
 async function makeSureNecessaryProgramsAreInstalled(config: FullConfig) {
   if (config.updateSnapshots === "all") {
@@ -36,17 +39,49 @@ async function makeSureNpmCiHasBeenRan() {
   }
 }
 
-// Create session states for each user, state will be named as username, e.g. admin.json
+/** Create session states for each user, state will be named as username, e.g. admin.json
+ * See https://playwright.dev/docs/auth#reuse-signed-in-state
+ */
 async function createLoginStates() {
+  const usersLoginInformationToCache = [
+    { email: "admin@example.com", password: "admin" },
+    { email: "teacher@example.com", password: "teacher" },
+    { email: "language.teacher@example.com", password: "language.teacher" },
+    { email: "user@example.com", password: "user" },
+    { email: "assistant@example.com", password: "assistant" },
+    { email: "creator@example.com", password: "creator" },
+  ]
+  // Creating the storage states for different users takes some time, so we'll avoid doing it again if the stored state has been already created recently.
+  // Using older storage states would run into problems with cookie expiry. A different solution could modify the saved storage states
+  // to remove the cookie expiry times
+  const allStorageStatesRecentlyCreated = usersLoginInformationToCache.every((loginInformation) => {
+    try {
+      const fileStats = statSync(path.join(__dirname, `../states/${loginInformation.email}.json`))
+      if (new Date().getTime() - fileStats.mtime.getTime() < ONE_WEEK_MS) {
+        return true
+      }
+      return false
+    } catch (e) {
+      return false
+    }
+  })
+  if (allStorageStatesRecentlyCreated) {
+    console.info(
+      "All login states (=saved cookies for logging in) have been generated within the last week. Skipping generation and using existing ones.",
+    )
+    console.info(
+      "Note: If you want to regenerate the login statesnow, delete the json files in the 'src/states' folder.",
+    )
+    console.info()
+    return
+  }
   console.log("Creating login states for supported test users.")
   const browser = await chromium.launch()
   const page = await browser.newPage()
-  await login("admin@example.com", "admin", page)
-  await login("teacher@example.com", "teacher", page)
-  await login("language.teacher@example.com", "language.teacher", page)
-  await login("user@example.com", "user", page)
-  await login("assistant@example.com", "assistant", page)
-  await login("creator@example.com", "creator", page)
+  for (const userLoginInformation of usersLoginInformationToCache) {
+    await login(userLoginInformation.email, userLoginInformation.password, page)
+    console.log(`Created login state for ${userLoginInformation.email}`)
+  }
   await browser.close()
 }
 

541898408c643473be7060dd8e1d878c1d34c224
Eyoyibo George <eyoyibo@lx2-500-141.ad.helsinki.fi>
2023-02-19 19:35:37 +0200

    update pullquote component

--- services/course-material/src/components/ContentRenderer/core/formatting/PullquoteBlock.tsx
+++ services/course-material/src/components/ContentRenderer/core/formatting/PullquoteBlock.tsx
@@ -6,6 +6,14 @@ import withErrorBoundary from "../../../../shared-module/utils/withErrorBoundary
 import colorMapper from "../../../../styles/colorMapper"
 import { sanitizeCourseMaterialHtml } from "../../../../utils/sanitizeCourseMaterialHtml"
 
+const FONT_SIZES: { [key: string]: string } = {
+  small: "10px",
+  normal: "20px",
+  medium: "30px",
+  large: "34px",
+  huge: "38px",
+}
+
 const PullquoteBlock: React.FC<
   React.PropsWithChildren<BlockRendererProps<PullquoteAttributes>>
 > = ({ data }) => {
@@ -20,11 +28,14 @@ const PullquoteBlock: React.FC<
     // style,
     textAlign,
     textColor,
+    fontSize,
     value,
   } = data.attributes
 
   const textAlignNotCenterWidth =
     textAlign && textAlign !== "center" && !align ? "max-width: 26.25rem;" : null
+
+  const size = FONT_SIZES[fontSize]
 
   return (
     <div
@@ -39,8 +50,8 @@ const PullquoteBlock: React.FC<
           ${gradient && `background: ${colorMapper(gradient)};`}
           text-align: center;
           ${textAlign && `text-align: ${textAlign};`}
-          border-top: 0.25rem solid currentColor;
-          border-bottom: 0.25rem solid currentColor;
+          border-top: 0.25rem solid #d5dbdf;
+          border-bottom: 0.25rem solid #d5dbdf;
           padding: 3rem 0rem !important;
           margin-bottom: 1rem;
           ${align && `float: ${align};`}
@@ -50,15 +61,16 @@ const PullquoteBlock: React.FC<
       >
         <blockquote
           className={css`
-            font-size: 1.75rem;
+            font-size: ${size};
             line-height: 1.6;
+            margin-bottom: 1rem;
           `}
           dangerouslySetInnerHTML={{ __html: sanitizeCourseMaterialHtml(value ?? "") }}
         />
         <cite
           className={css`
-            font-style: normal;
-            text-transform: uppercase;
+            font-style: 20px;
+            text-transform: capitalize !important;
           `}
           dangerouslySetInnerHTML={{ __html: sanitizeCourseMaterialHtml(citation) }}
         ></cite>

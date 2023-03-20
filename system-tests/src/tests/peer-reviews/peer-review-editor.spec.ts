0460bc49a33720aeb95dbc597d7280e9e36a5bd5
Henrik Nygren <nygrenh@gmail.com>
2023-03-20 15:44:50 +0200

    Test fixes

--- system-tests/src/tests/peer-reviews/peer-review-editor.spec.ts
+++ system-tests/src/tests/peer-reviews/peer-review-editor.spec.ts
@@ -1,5 +1,6 @@
 import { test } from "@playwright/test"
 
+import { showToastInfinitely, showToastNormally } from "../../utils/notificationUtils"
 import expectScreenshotsToMatchSnapshots from "../../utils/screenshot"
 test.use({
   storageState: "src/states/admin@example.com.json",
@@ -33,6 +34,7 @@ test("create peer review", async ({ page, headless }, testInfo) => {
   await page.locator("text=Insert question here").fill("first question")
 
   await page.locator("text=Save").nth(3).click()
+  await page.getByText(`Operation successful`).waitFor()
 
   await expectScreenshotsToMatchSnapshots({
     headless,
@@ -43,24 +45,69 @@ test("create peer review", async ({ page, headless }, testInfo) => {
     clearNotifications: true,
     axeSkip: ["aria-allowed-attr", "aria-allowed-role"],
   })
+})
 
-  // Check text=Use course global peer reviewPeer reviews to receivePeer reviews to givePeer rev >> input[type="checkbox"]
-  await page.locator("text=Use course default peer review config").click()
+test("default peer review editing", async ({ page, headless }, testInfo) => {
+  await page.goto("http://project-331.local/")
+
+  await page
+    .locator(
+      "text=University of Helsinki, Department of Computer ScienceOrganization for Computer ",
+    )
+    .click()
+
+  await page.locator("[aria-label=\"Manage course \\'Introduction to everything\\'\"] path").click()
+
+  await page.locator("text=Pages").click()
+
+  await page.locator("text=Page One/chapter-1/page-1Edit page >> button").first().click()
+
+  await page.getByText("Use course default peer review config").click()
 
   const [page1] = await Promise.all([
     page.waitForEvent("popup"),
     page.locator(`a:has-text("Course default peer review config")`).click(),
   ])
 
-  await page1.locator("text=Save").click()
-
   await expectScreenshotsToMatchSnapshots({
+    screenshotTarget: page1,
     headless,
     testInfo,
-    snapshotName: "peer-review-editor-question-deleted",
-    waitForTheseToBeVisibleAndStable: [page.locator(`text="Add peer review"`)],
-    screenshotTarget: page,
-    clearNotifications: true,
-    axeSkip: ["aria-allowed-attr", "aria-allowed-role"],
+    snapshotName: "default-peer-review-editor",
+    waitForTheseToBeVisibleAndStable: [page1.locator('text="Configure review answers option"')],
   })
+
+  await page1.locator('input[type="number"]').first().click()
+  // Fill input[type="number"] >> nth=0
+  await page1.locator('input[type="number"]').first().fill("3")
+
+  await page1.locator('input[type="number"]').nth(1).click()
+  // Fill input[type="number"] >> nth=1
+  await page1.locator('input[type="number"]').nth(1).fill("4")
+  // Select ManualReviewEverything
+  await page1
+    .locator(
+      "text=Peer review accepting strategyAutomatically accept or reject by averageAutomatic >> select",
+    )
+    .selectOption("ManualReviewEverything")
+
+  await page1.locator('input[type="number"]').nth(2).click()
+  // Fill input[type="number"] >> nth=2
+  await page1.locator('input[type="number"]').nth(2).fill("2.5")
+
+  await page1.getByRole("button", { name: "Delete" }).nth(1).click()
+  // Fill text=General comments
+  await page1.locator("text=General comments").fill("test")
+
+  await showToastInfinitely(page1)
+  await page1.locator("text=Save").click()
+  await page1.getByText(`Operation successful`).waitFor()
+  await expectScreenshotsToMatchSnapshots({
+    screenshotTarget: page1,
+    headless,
+    testInfo,
+    snapshotName: "default-peer-review-editor-after-save",
+    waitForTheseToBeVisibleAndStable: [page1.locator('text="Add peer review question"')],
+  })
+  await showToastNormally(page1)
 })

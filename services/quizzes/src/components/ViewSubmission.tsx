77f4883046f2d61fa426e8f3313aa769a9e9ebc2
d471061c <cpastethat@gmail.com>
2023-04-25 14:14:41 +0300

    update submissions to support new types

--- services/quizzes/src/components/ViewSubmission.tsx
+++ services/quizzes/src/components/ViewSubmission.tsx
@@ -1,18 +1,40 @@
 import { css } from "@emotion/css"
+import styled from "@emotion/styled"
 import React from "react"
 import { useTranslation } from "react-i18next"
 
-import { UserAnswer } from "../../types/quizTypes/answer"
+import {
+  UserAnswer,
+  UserItemAnswerCheckbox,
+  UserItemAnswerChooseN,
+  UserItemAnswerClosedEndedQuestion,
+  UserItemAnswerEssay,
+  UserItemAnswerMatrix,
+  UserItemAnswerMultiplechoice,
+  UserItemAnswerMultiplechoiceDropdown,
+  UserItemAnswerScale,
+  UserItemAnswerTimeline,
+} from "../../types/quizTypes/answer"
 import { ModelSolutionQuiz } from "../../types/quizTypes/modelSolutionSpec"
-import { PublicSpecQuiz } from "../../types/quizTypes/publicSpec"
+import {
+  PublicSpecQuiz,
+  PublicSpecQuizItemCheckbox,
+  PublicSpecQuizItemChooseN,
+  PublicSpecQuizItemClosedEndedQuestion,
+  PublicSpecQuizItemEssay,
+  PublicSpecQuizItemMatrix,
+  PublicSpecQuizItemMultiplechoice,
+  PublicSpecQuizItemMultiplechoiceDropdown,
+  PublicSpecQuizItemScale,
+  PublicSpecQuizItemTimeline,
+} from "../../types/quizTypes/publicSpec"
 import { ItemAnswerFeedback } from "../pages/api/grade"
 import { UserInformation } from "../shared-module/exercise-service-protocol-types"
 import { baseTheme } from "../shared-module/styles"
-import { sanitizeFlexDirection } from "../shared-module/utils/css-sanitization"
+import { FlexDirection, sanitizeFlexDirection } from "../shared-module/utils/css-sanitization"
 import { COLUMN } from "../util/constants"
 
 import FlexWrapper from "./FlexWrapper"
-import { QuizItemSubmissionComponentProps } from "./SubmissionComponents"
 import CheckBoxFeedback from "./SubmissionComponents/Checkbox"
 import EssayFeedback from "./SubmissionComponents/Essay"
 import MatrixSubmission from "./SubmissionComponents/Matrix"
@@ -22,6 +44,7 @@ import MultipleChoiceDropdownFeedback from "./SubmissionComponents/MultipleChoic
 import OpenFeedback from "./SubmissionComponents/Open"
 import ScaleSubmissionViewComponent from "./SubmissionComponents/Scale"
 import Timeline from "./SubmissionComponents/Timeline"
+import Unsupported from "./SubmissionComponents/Unsupported"
 
 interface SubmissionProps {
   user_answer: UserAnswer
@@ -42,7 +65,17 @@ type QuizItemType =
   | "timeline"
 
 interface QuizItemSubmissionComponentDescriptor {
-  component: React.ComponentClass<QuizItemSubmissionComponentProps>
+  component:
+    | typeof EssayFeedback
+    | typeof MultipleChoiceSubmission
+    | typeof CheckBoxFeedback
+    | typeof ScaleSubmissionViewComponent
+    | typeof OpenFeedback
+    | typeof Unsupported
+    | typeof MultipleChoiceDropdownFeedback
+    | typeof MultipleChoiceClickableFeedback
+    | typeof MatrixSubmission
+    | typeof Timeline
   shouldDisplayCorrectnessMessageAfterAnswer: boolean
 }
 
@@ -83,6 +116,10 @@ const componentDescriptorByTypeName = (
   return mapTypeToComponent[typeName]
 }
 
+const FlexItem = styled.div`
+  flex: 1;
+`
+
 const Submission: React.FC<React.PropsWithChildren<SubmissionProps>> = ({
   publicAlternatives,
   modelSolutions,
@@ -102,7 +139,15 @@ const Submission: React.FC<React.PropsWithChildren<SubmissionProps>> = ({
     }
   }
 
-  const direction = sanitizeFlexDirection(publicAlternatives.direction, COLUMN)
+  // set wide screen direction to row if there is multiple-choice item
+  // in quiz items
+  let direction: FlexDirection = COLUMN
+  publicAlternatives.items.every((item) => {
+    if (item.type == "multiple-choice") {
+      direction = sanitizeFlexDirection(item.direction, COLUMN)
+      return
+    }
+  })
 
   return (
     <FlexWrapper wideScreenDirection={direction}>
@@ -113,7 +158,6 @@ const Submission: React.FC<React.PropsWithChildren<SubmissionProps>> = ({
           if (!componentDescriptor) {
             return <div key={item.id}>{t("quiz-type-not-supported")}</div>
           }
-          const Component = componentDescriptor.component
           const itemFeedback = feedback_json
             ? feedback_json.filter((itemFeedback) => itemFeedback.quiz_item_id === item.id)[0]
             : null
@@ -125,57 +169,198 @@ const Submission: React.FC<React.PropsWithChildren<SubmissionProps>> = ({
           const quizItemAnswer = user_answer.itemAnswers.filter(
             (ia) => ia.quizItemId === item.id,
           )[0]
-          return (
+          const feedback = itemFeedback &&
+            componentDescriptor.shouldDisplayCorrectnessMessageAfterAnswer && (
+              <div
+                className={css`
+                  background: ${itemFeedback.quiz_item_correct ? "#f1fff2" : "#fff4f5"};
+                  border: 1px solid ${itemFeedback.quiz_item_correct ? "#cbf3cd" : "#f3cbcf"};
+                  box-sizing: border-box;
+                  border-radius: 4px;
+                  color: ${itemFeedback.quiz_item_correct ? "#1c850d" : "#d52a3c"};
+                  margin: 1.5rem auto;
+                  margin-bottom: 0;
+                  padding: 0.25rem 1.5rem;
+                  width: fit-content;
+                `}
+              >
+                {mapScoreToFeedback(itemFeedback.score)}
+              </div>
+            )
+          const missingQuizItemAnswer = !quizItemAnswer && (
             <div
               className={css`
-                flex: 1;
+                padding: 1rem;
+                background-color: ${baseTheme.colors.gray[100]};
               `}
-              key={item.id}
             >
-              {quizItemAnswer && (
-                <>
-                  <Component
-                    key={item.id}
-                    public_quiz_item={item}
-                    quiz_direction={direction}
-                    quiz_item_feedback={itemFeedback}
-                    quiz_item_model_solution={itemModelSolution}
-                    user_quiz_item_answer={quizItemAnswer}
-                    user_information={user_information}
-                  />
-                  {itemFeedback &&
-                    componentDescriptor.shouldDisplayCorrectnessMessageAfterAnswer && (
-                      <div
-                        className={css`
-                          background: ${itemFeedback.quiz_item_correct ? "#f1fff2" : "#fff4f5"};
-                          border: 1px solid
-                            ${itemFeedback.quiz_item_correct ? "#cbf3cd" : "#f3cbcf"};
-                          box-sizing: border-box;
-                          border-radius: 4px;
-                          color: ${itemFeedback.quiz_item_correct ? "#1c850d" : "#d52a3c"};
-                          margin: 1.5rem auto;
-                          margin-bottom: 0;
-                          padding: 0.25rem 1.5rem;
-                          width: fit-content;
-                        `}
-                      >
-                        {mapScoreToFeedback(itemFeedback.score)}
-                      </div>
-                    )}{" "}
-                </>
-              )}
-              {!quizItemAnswer && (
-                <div
-                  className={css`
-                    padding: 1rem;
-                    background-color: ${baseTheme.colors.gray[100]};
-                  `}
-                >
-                  {t("error-quiz-item-added-after-answering")}
-                </div>
-              )}
+              {t("error-quiz-item-added-after-answering")}
             </div>
           )
+          switch (item.type) {
+            case "checkbox":
+              return (
+                quizItemAnswer && (
+                  <FlexItem key={item.id}>
+                    <CheckBoxFeedback
+                      key={item.id}
+                      public_quiz_item={item as PublicSpecQuizItemCheckbox}
+                      quiz_direction={direction}
+                      quiz_item_feedback={itemFeedback}
+                      quiz_item_model_solution={itemModelSolution}
+                      user_quiz_item_answer={quizItemAnswer as UserItemAnswerCheckbox}
+                      user_information={user_information}
+                    />
+                    {feedback}
+                    {missingQuizItemAnswer}
+                  </FlexItem>
+                )
+              )
+            case "choose-n":
+              return (
+                quizItemAnswer && (
+                  <FlexItem key={item.id}>
+                    <MultipleChoiceClickableFeedback
+                      key={item.id}
+                      public_quiz_item={item as PublicSpecQuizItemChooseN}
+                      quiz_direction={direction}
+                      quiz_item_feedback={itemFeedback}
+                      quiz_item_model_solution={itemModelSolution}
+                      user_quiz_item_answer={quizItemAnswer as UserItemAnswerChooseN}
+                      user_information={user_information}
+                    />
+                    {feedback}
+                    {missingQuizItemAnswer}
+                  </FlexItem>
+                )
+              )
+            case "closed-ended-question":
+              return (
+                quizItemAnswer && (
+                  <FlexItem key={item.id}>
+                    <OpenFeedback
+                      key={item.id}
+                      public_quiz_item={item as PublicSpecQuizItemClosedEndedQuestion}
+                      quiz_direction={direction}
+                      quiz_item_feedback={itemFeedback}
+                      quiz_item_model_solution={itemModelSolution}
+                      user_quiz_item_answer={quizItemAnswer as UserItemAnswerClosedEndedQuestion}
+                      user_information={user_information}
+                    />
+                    {feedback}
+                    {missingQuizItemAnswer}
+                  </FlexItem>
+                )
+              )
+            case "essay":
+              return (
+                quizItemAnswer && (
+                  <FlexItem key={item.id}>
+                    <EssayFeedback
+                      key={item.id}
+                      public_quiz_item={item as PublicSpecQuizItemEssay}
+                      quiz_direction={direction}
+                      quiz_item_feedback={itemFeedback}
+                      quiz_item_model_solution={itemModelSolution}
+                      user_quiz_item_answer={quizItemAnswer as UserItemAnswerEssay}
+                      user_information={user_information}
+                    />
+                    {feedback}
+                    {missingQuizItemAnswer}
+                  </FlexItem>
+                )
+              )
+            case "matrix":
+              return (
+                quizItemAnswer && (
+                  <FlexItem key={item.id}>
+                    <MatrixSubmission
+                      key={item.id}
+                      public_quiz_item={item as PublicSpecQuizItemMatrix}
+                      quiz_direction={direction}
+                      quiz_item_feedback={itemFeedback}
+                      quiz_item_model_solution={itemModelSolution}
+                      user_quiz_item_answer={quizItemAnswer as UserItemAnswerMatrix}
+                      user_information={user_information}
+                    />
+                    {feedback}
+                    {missingQuizItemAnswer}
+                  </FlexItem>
+                )
+              )
+            case "multiple-choice":
+              return (
+                quizItemAnswer && (
+                  <FlexItem key={item.id}>
+                    <MultipleChoiceSubmission
+                      key={item.id}
+                      public_quiz_item={item as PublicSpecQuizItemMultiplechoice}
+                      quiz_direction={sanitizeFlexDirection(item.direction, COLUMN)}
+                      quiz_item_feedback={itemFeedback}
+                      quiz_item_model_solution={itemModelSolution}
+                      user_quiz_item_answer={quizItemAnswer as UserItemAnswerMultiplechoice}
+                      user_information={user_information}
+                    />
+                    {feedback}
+                    {missingQuizItemAnswer}
+                  </FlexItem>
+                )
+              )
+            case "multiple-choice-dropdown":
+              return (
+                quizItemAnswer && (
+                  <FlexItem key={item.id}>
+                    <MultipleChoiceDropdownFeedback
+                      key={item.id}
+                      public_quiz_item={item as PublicSpecQuizItemMultiplechoiceDropdown}
+                      quiz_direction={direction}
+                      quiz_item_feedback={itemFeedback}
+                      quiz_item_model_solution={itemModelSolution}
+                      user_quiz_item_answer={quizItemAnswer as UserItemAnswerMultiplechoiceDropdown}
+                      user_information={user_information}
+                    />
+                    {feedback}
+                    {missingQuizItemAnswer}
+                  </FlexItem>
+                )
+              )
+            case "scale":
+              return (
+                quizItemAnswer && (
+                  <FlexItem key={item.id}>
+                    <ScaleSubmissionViewComponent
+                      key={item.id}
+                      public_quiz_item={item as PublicSpecQuizItemScale}
+                      quiz_direction={direction}
+                      quiz_item_feedback={itemFeedback}
+                      quiz_item_model_solution={itemModelSolution}
+                      user_quiz_item_answer={quizItemAnswer as UserItemAnswerScale}
+                      user_information={user_information}
+                    />
+                    {feedback}
+                    {missingQuizItemAnswer}
+                  </FlexItem>
+                )
+              )
+            case "timeline":
+              return (
+                quizItemAnswer && (
+                  <FlexItem key={item.id}>
+                    <Timeline
+                      key={item.id}
+                      public_quiz_item={item as PublicSpecQuizItemTimeline}
+                      quiz_direction={direction}
+                      quiz_item_feedback={itemFeedback}
+                      quiz_item_model_solution={itemModelSolution}
+                      user_quiz_item_answer={quizItemAnswer as UserItemAnswerTimeline}
+                      user_information={user_information}
+                    />
+                    {feedback}
+                    {missingQuizItemAnswer}
+                  </FlexItem>
+                )
+              )
+          }
         })}
     </FlexWrapper>
   )

1dfd81f245c4be8c5cdd35d5ca1e86311abb4889
george-misan <george.eyoyibo@gmail.com>
2023-04-26 13:07:37 +0300

    WIP

---
+++ services/cms/src/blocks/AudioUpload/AudioUploadSave.tsx
@@ -0,0 +1,11 @@
+import { InnerBlocks } from "@wordpress/block-editor"
+
+const AudioUploadSave: React.FC<unknown> = () => {
+  return (
+    <div>
+      <InnerBlocks.Content />
+    </div>
+  )
+}
+
+export default AudioUploadSave

ad5db6eb6c32bfae353f425bd4be36b36d833733
Henrik Nygren <nygrenh@gmail.com>
2023-06-20 16:50:41 +0300

    Don't use regconfig (#1147)
    * Don't use regconfig
    
    * Fix

---
+++ services/headless-lms/migrations/20230620100405_remove_regconfig.down.sql
@@ -0,0 +1,52 @@
+--
+ALTER TABLE courses
+ALTER COLUMN content_search_language
+SET DEFAULT 'simple'::REGCONFIG;
+ALTER TABLE pages
+ALTER COLUMN content_search_language
+SET DEFAULT 'simple'::REGCONFIG;
+ALTER TABLE courses
+ALTER COLUMN content_search_language TYPE REGCONFIG USING content_search_language::REGCONFIG;
+ALTER TABLE pages
+ALTER COLUMN content_search_language TYPE REGCONFIG USING content_search_language::REGCONFIG;
+-- Automatically updates pages.content_search and pages.content_search_original_text columns using the function extract_searchable_text_from_document_schema.
+CREATE OR REPLACE FUNCTION trigger_set_pages_content_search() RETURNS TRIGGER AS $$ BEGIN IF (
+    (
+      row (NEW.content) IS DISTINCT
+      FROM row (OLD.content)
+    )
+    OR OLD.content_search IS NULL
+    OR NEW.content_search IS NULL
+  ) THEN
+declare begin NEW.content_search = setweight(
+    to_tsvector(
+      NEW.content_search_language,
+      NEW.title
+    ),
+    'A'
+  ) || setweight (
+    (
+      SELECT coalesce(
+          to_tsvector(
+            NEW.content_search_language,
+            jsonb_agg(value)
+          ),
+          to_tsvector('')
+        )
+      FROM (
+          SELECT *
+          FROM extract_searchable_text_from_document_schema(NEW.content)
+        ) as value
+    ),
+    'B'
+  );
+NEW.content_search_original_text = (
+  SELECT string_agg(res.value->>0, ' ') as original_text
+  FROM (
+      SELECT extract_searchable_text_from_document_schema(NEW.content) as value
+    ) as res
+);
+end;
+END IF;
+RETURN NEW;
+END $$ language 'plpgsql';

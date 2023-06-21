14853557118ee668959730842eee70ff435c0414
Maija Y <maija.ylitalo@helsinki.fi>
2023-06-21 17:12:38 +0300

    updated submissions page front

--- services/main-frontend/src/components/page-specific/manage/exercises/id/submissions/ExerciseSubmissionList.tsx
+++ services/main-frontend/src/components/page-specific/manage/exercises/id/submissions/ExerciseSubmissionList.tsx
@@ -1,8 +1,16 @@
+import { css } from "@emotion/css"
 import Link from "next/link"
 import React from "react"
 import { useTranslation } from "react-i18next"
 
 import { ExerciseSlideSubmission } from "../../../../../../shared-module/bindings"
+import LinkIcon from "../../../../../../shared-module/img/link.svg"
+import {
+  baseTheme,
+  fontWeights,
+  headingFont,
+  secondaryFont,
+} from "../../../../../../shared-module/styles"
 
 interface Props {
   exerciseSubmissions: ExerciseSlideSubmission[]
@@ -17,9 +25,33 @@ const ExerciseSubmissionList: React.FC<React.PropsWithChildren<Props>> = ({
   }
   return (
     <>
-      <table>
+      <table
+        className={css`
+          border-collapse: collapse;
+          border: 1px solid ${baseTheme.colors.clear[300]};
+          margin-top: 1.5rem;
+          td,
+          th {
+            border-left: 1px solid ${baseTheme.colors.clear[300]};
+            border-right: 1px solid ${baseTheme.colors.clear[300]};
+            padding-left: 2rem;
+            padding-right: 2rem;
+            text-align: left;
+          }
+        `}
+      >
         <thead>
-          <tr>
+          <tr
+            className={css`
+              font-family: ${secondaryFont};
+              font-weight: ${fontWeights.semibold};
+              font-size: ${baseTheme.fontSizes[14]};
+              color: ${baseTheme.colors.gray[600]};
+              text-transform: uppercase;
+              opacity: 0.8;
+              height: 4rem;
+            `}
+          >
             <th>{t("label-link")}</th>
             <th>{t("label-submission-time")}</th>
             <th>{t("label-student")}</th>
@@ -27,9 +59,25 @@ const ExerciseSubmissionList: React.FC<React.PropsWithChildren<Props>> = ({
             <th>{t("label-exam")}</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody
+          className={css`
+            tr:nth-child(odd) {
+              background-color: ${baseTheme.colors.clear[100]};
+            }
+          `}
+        >
           {exerciseSubmissions.map((x) => (
-            <tr key={x.id}>
+            <tr
+              key={x.id}
+              className={css`
+                font-family: ${headingFont};
+                font-weight: ${fontWeights.normal};
+                font-size: ${baseTheme.fontSizes[16]};
+                color: ${baseTheme.colors.gray[400]};
+                height: 4.4rem;
+                line-height: 1rem;
+              `}
+            >
               <td>
                 <Link
                   href={{
@@ -37,10 +85,10 @@ const ExerciseSubmissionList: React.FC<React.PropsWithChildren<Props>> = ({
                     query: { id: x.id },
                   }}
                 >
-                  {t("link")}
+                  <LinkIcon />
                 </Link>
               </td>
-              <td>{x.created_at.toISOString()}</td>
+              <td>{x.created_at.toLocaleString()}</td>
               <td>{x.user_id}</td>
               <td>{x.course_instance_id}</td>
               <td>{x.exam_id}</td>

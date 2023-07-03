bf8dbc3721688c484cc0b073de03232a0d3f9222
George Misan Eyoyibo <57463554+george-misan@users.noreply.github.com>
2023-07-03 10:06:21 +0300

    Improvements to the Map component (Map beta) (#1160)
    * + new get-country-by-id endpoint
    
    * refacotr code
    
    * code refactor
    
    * remove unused code
    
    * update get-countries endpoint
    
    * remove unused code
    
    * update useQuery string
    
    * review comment
    
    * review comments
    
    * remove unused code
    
    * remove unused code
    
    * remove unused code
    
    * remove unused code

--- services/headless-lms/models/src/student_countries.rs
+++ services/headless-lms/models/src/student_countries.rs
@@ -57,6 +57,7 @@ WHERE id = $1
 pub async fn get_countries(
     conn: &mut PgConnection,
     course_id: Uuid,
+    course_instance_id: Uuid,
 ) -> ModelResult<Vec<StudentCountry>> {
     let student_countries = sqlx::query_as!(
         StudentCountry,
@@ -64,11 +65,35 @@ pub async fn get_countries(
 SELECT *
 FROM student_countries
 WHERE course_id = $1
+AND course_instance_id = $2
 AND deleted_at IS NULL;
 ",
-        course_id
+        course_id,
+        course_instance_id,
     )
     .fetch_all(conn)
     .await?;
     Ok(student_countries)
+}
+
+pub async fn get_selected_country_by_user_id(
+    conn: &mut PgConnection,
+    user_id: Uuid,
+    course_instance_id: Uuid,
+) -> ModelResult<StudentCountry> {
+    let country = sqlx::query_as!(
+        StudentCountry,
+        "
+SELECT *
+FROM student_countries
+WHERE user_id = $1
+AND course_instance_id = $2
+AND deleted_at IS NULL;
+",
+        user_id,
+        course_instance_id,
+    )
+    .fetch_one(conn)
+    .await?;
+    Ok(country)
 }
--- services/headless-lms/server/src/controllers/course_material/courses.rs
+++ services/headless-lms/server/src/controllers/course_material/courses.rs
@@ -737,18 +737,52 @@ async fn student_country(
 }
 
 /**
-GET `/api/v0/{course_id}/student-countries - Returns countries of student registered in a course.
+GET `/api/v0/{course_id}/course-instances/{course_instance_id}/student-countries - Returns countries of student registered in a course.
  */
 #[generated_doc]
 #[instrument(skip(pool))]
 async fn get_student_countries(
-    course_id: web::Path<Uuid>,
+    query: web::Path<(Uuid, Uuid)>,
     pool: web::Data<PgPool>,
     user: AuthUser,
-) -> ControllerResult<web::Json<Vec<StudentCountry>>> {
+) -> ControllerResult<web::Json<HashMap<String, u32>>> {
     let mut conn = pool.acquire().await?;
     let token = skip_authorize();
-    let res = models::student_countries::get_countries(&mut conn, *course_id).await?;
+    let (course_id, course_instance_id) = query.into_inner();
+
+    let country_codes: Vec<String> =
+        models::student_countries::get_countries(&mut conn, course_id, course_instance_id)
+            .await?
+            .into_iter()
+            .map(|c| (c.country_code))
+            .collect();
+
+    let mut frequency: HashMap<String, u32> = HashMap::new();
+    for code in country_codes {
+        *frequency.entry(code).or_insert(0) += 1
+    }
+
+    token.authorized_ok(web::Json(frequency))
+}
+
+/**
+GET `/api/v0/{course_id}/student-country - Returns country of a student registered in a course.
+ */
+#[generated_doc]
+#[instrument(skip(pool))]
+async fn get_student_country(
+    course_instance_id: web::Path<Uuid>,
+    pool: web::Data<PgPool>,
+    user: AuthUser,
+) -> ControllerResult<web::Json<StudentCountry>> {
+    let mut conn = pool.acquire().await?;
+    let token = skip_authorize();
+    let res = models::student_countries::get_selected_country_by_user_id(
+        &mut conn,
+        user.id,
+        *course_instance_id,
+    )
+    .await?;
 
     token.authorized_ok(web::Json(res))
 }
@@ -812,7 +846,11 @@ pub fn _add_routes(cfg: &mut ServiceConfig) {
             web::post().to(student_country),
         )
         .route(
-            "/{course_id}/student-countries",
+            "/{course_instance_id}/student-country",
+            web::get().to(get_student_country),
+        )
+        .route(
+            "/{course_id}/course-instances/{course_instance_id}/student-countries",
             web::get().to(get_student_countries),
         );
 }

cbf34e654ab5b3ffe3ec33786d423094ae9202c3
Henrik Nygren <nygrenh@gmail.com>
2023-07-03 15:09:13 +0300

    Typescript fixes

--- services/course-material/types/i18next.d.ts
+++ services/course-material/types/i18next.d.ts
@@ -3,11 +3,10 @@ import "i18next"
 import ownTranslations from "../src/shared-module/locales/en/course-material.json"
 import sharedModule from "../src/shared-module/locales/en/shared-module.json"
 
-const allAvailableTranslations = { ...sharedModule, ...ownTranslations }
-
 declare module "i18next" {
   interface CustomTypeOptions {
-    defaultNS: typeof allAvailableTranslations
+    defaultNS: "course-material"
+    fallbackNS: "shared-module"
     resources: {
       "course-material": typeof ownTranslations
       "shared-module": typeof sharedModule
--- shared-module/src/utils/testing/i18nTest.ts
+++ shared-module/src/utils/testing/i18nTest.ts
@@ -17,6 +17,7 @@ i18n.use(initReactI18next).init({
     escapeValue: false, // not needed for react!!
   },
 
+  fallbackNS: "shared-module",
   resources: { en: { translationsNS: {} } },
 })
 

d6354edce4454cc93f4e45bf7ce20b83c2aa8bd8
Henrik Nygren <nygrenh@gmail.com>
2023-07-31 14:54:03 +0300

    Fix pages list dropdown menu going off screen

--- shared-module/src/components/DropdownMenu/index.tsx
+++ shared-module/src/components/DropdownMenu/index.tsx
@@ -1,8 +1,9 @@
 import { css } from "@emotion/css"
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import React, { useState } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import { useTranslation } from "react-i18next"
+import { usePopper } from "react-popper"
 
 import { baseTheme } from "../../styles"
 
@@ -17,16 +18,67 @@ const DropdownMenu: React.FC<
 > = ({ items }) => {
   const { t } = useTranslation()
   const [expanded, setExpanded] = useState(false)
+
+  // This is the right way according to popper.js docs
+  const [referenceElement, setReferenceElement] = useState<HTMLButtonElement | null>(null)
+  const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null)
+
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: "bottom-start",
+    strategy: "fixed",
+    modifiers: [
+      {
+        name: "offset",
+        enabled: true,
+        options: {
+          offset: [0, 5],
+        },
+      },
+      {
+        name: "preventOverflow",
+        enabled: true,
+      },
+      {
+        name: "flip",
+        enabled: true,
+      },
+    ],
+  })
+
+  const handleOutsideClick = useCallback(
+    (event: MouseEvent) => {
+      if (!expanded) {
+        return
+      }
+      if (
+        referenceElement &&
+        (referenceElement.contains(event.target as Node) || referenceElement === event.target)
+      ) {
+        return
+      }
+      setExpanded(false)
+    },
+    [expanded, popperElement],
+  )
+
+  useEffect(() => {
+    document.addEventListener("click", handleOutsideClick)
+    return () => {
+      document.removeEventListener("click", handleOutsideClick)
+    }
+  }, [handleOutsideClick])
+
   return (
     <div
       className={css`
-        position: relative;
         display: flex;
         align-items: center;
       `}
     >
       <button
         aria-expanded={expanded}
+        // @ts-expect-error: popper is supposed to be used this way, see the documentation
+        ref={setReferenceElement}
         onClick={() => {
           setExpanded(!expanded)
         }}
@@ -47,47 +99,53 @@ const DropdownMenu: React.FC<
         <FontAwesomeIcon icon={faEllipsisV} />
       </button>
       {expanded && (
-        <ul
+        <div
+          // eslint-disable-next-line react/forbid-dom-props
+          style={styles.popper}
+          {...attributes.popper}
           className={css`
             z-index: 200;
-            box-shadow: 0px 8px 40px rgba(0, 0, 0, 0.1);
-            width: fit-content;
-            list-style: none;
-            padding: 0;
-            border-radius: 4px;
-            overflow: hidden;
-            margin: 0;
-            font-size: 17px;
-            position: absolute;
-            top: 33px;
-            left: 0;
-
-            li + li {
-              border-top: 2px solid ${baseTheme.colors.clear[200]};
-            }
-
-            li:hover {
-              filter: brightness(92%) contrast(110%);
-            }
           `}
+          // @ts-expect-error: popper is supposed to be used this way, see the documentation
+          ref={setPopperElement}
         >
-          {items
-            .filter((item) => item !== null)
-            .map((item) => {
-              if (item === null) {
-                return null
+          <ul
+            className={css`
+              box-shadow: 0px 8px 40px rgba(0, 0, 0, 0.1);
+              width: fit-content;
+              list-style: none;
+              padding: 0;
+              border-radius: 4px;
+              margin: 0;
+              font-size: 17px;
+
+              li + li {
+                border-top: 2px solid ${baseTheme.colors.clear[200]};
               }
-              return (
-                <DropdownMenuItem
-                  key={item.label}
-                  item={item}
-                  closeMenu={() => {
-                    setExpanded(false)
-                  }}
-                />
-              )
-            })}
-        </ul>
+
+              li:hover {
+                filter: brightness(92%) contrast(110%);
+              }
+            `}
+          >
+            {items
+              .filter((item) => item !== null)
+              .map((item) => {
+                if (item === null) {
+                  return null
+                }
+                return (
+                  <DropdownMenuItem
+                    key={item.label}
+                    item={item}
+                    closeMenu={() => {
+                      setExpanded(false)
+                    }}
+                  />
+                )
+              })}
+          </ul>
+        </div>
       )}
     </div>
   )

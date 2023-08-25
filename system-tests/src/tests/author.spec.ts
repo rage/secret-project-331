964592c1a909d0bc9ea143b35b8556417efb1bfa
george-misan <george.eyoyibo@gmail.com>
2023-08-25 13:35:34 +0300

    add test

---
+++ system-tests/src/tests/author.spec.ts
@@ -0,0 +1,25 @@
+import { test } from "@playwright/test"
+
+import { selectCourseInstanceIfPrompted } from "../utils/courseMaterialActions"
+import expectScreenshotsToMatchSnapshots from "../utils/screenshot"
+
+test.use({
+  storageState: "src/states/teacher@example.com.json",
+})
+
+test("glossary-tooltip", async ({ page, headless }, testInfo) => {
+  await page.goto(
+    "http://project-331.local/org/uh-cs/courses/introduction-to-everything/chapter-1/the-authors",
+  )
+
+  await selectCourseInstanceIfPrompted(page)
+
+  await expectScreenshotsToMatchSnapshots({
+    screenshotTarget: page,
+    headless,
+    testInfo,
+    snapshotName: "author-block",
+    waitForTheseToBeVisibleAndStable: [page.locator("text=Authors")],
+    beforeScreenshot: async () => page.locator("text=Authors").scrollIntoViewIfNeeded(),
+  })
+})

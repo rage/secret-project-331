039fb1e9611a2d93b5ad95b839eb2ec74b34f657
Henrik Nygren <nygrenh@gmail.com>
2023-09-11 09:07:01 +0300

    Add missing doc files

---
+++ services/headless-lms/server/generated-docs/ResearchFormQuestion.ts
@@ -0,0 +1,9 @@
+type ResearchFormQuestion = {
+  id: string
+  course_id: string
+  research_consent_form_id: string
+  question: string
+  created_at: Date
+  updated_at: Date
+  deleted_at: Date | null
+}

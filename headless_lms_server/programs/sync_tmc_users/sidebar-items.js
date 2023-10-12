9b58dceba721950449cd6528d53ceff28851e2a7
nygrenh <nygrenh@gmail.com>
2023-10-12 14:20:06 +0000

    Deploying to gh-pages from  @ 77f8ed52da05a3179f68c58b65a219ab7af3fbb6 🚀

--- headless_lms_server/programs/sync_tmc_users/sidebar-items.js
+++ headless_lms_server/programs/sync_tmc_users/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["URL"],"fn":["delete_users","fetch_recently_changed_user_details","main"],"struct":["Change","TMCRecentChanges"]};
+window.SIDEBAR_ITEMS = {"constant":["URL"],"fn":["delete_users","fetch_recently_changed_user_details","main","update_users"],"struct":["Change","TMCRecentChanges"]};

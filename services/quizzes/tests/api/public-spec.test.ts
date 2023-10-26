964bf7b1adbd8ee1ba2b33367a27169fb6766982
Henrik Nygren <nygrenh@gmail.com>
2023-10-26 09:35:18 +0300

    Add more tests for the public spec

--- services/quizzes/tests/api/public-spec.test.ts
+++ services/quizzes/tests/api/public-spec.test.ts
@@ -2,29 +2,50 @@ import handler from "../../src/pages/api/public-spec"
 import { SpecRequest } from "../../src/shared-module/bindings"
 import {
   PrivateSpecQuiz,
+  PrivateSpecQuizItemCheckbox,
+  PrivateSpecQuizItemChooseN,
+  PrivateSpecQuizItemClosedEndedQuestion,
+  PrivateSpecQuizItemEssay,
+  PrivateSpecQuizItemMatrix,
   PrivateSpecQuizItemMultiplechoice,
+  PrivateSpecQuizItemMultiplechoiceDropdown,
+  PrivateSpecQuizItemScale,
+  PrivateSpecQuizItemTimeline,
   QuizItemOption,
 } from "../../types/quizTypes/privateSpec"
 import {
   PublicSpecQuiz,
+  PublicSpecQuizItemChooseN,
   PublicSpecQuizItemClosedEndedQuestion,
   PublicSpecQuizItemMultiplechoice,
+  PublicSpecQuizItemMultiplechoiceDropdown,
 } from "../../types/quizTypes/publicSpec"
 
 import {
   ADDITIONAL_CORRECTNESS_EXPLANATION_ON_MODEL_SOLUTION_CANARY_FOR_TESTS,
+  FAILURE_MESSAGE_CANARY_FOR_TESTS,
+  generatePrivateSpecWithOneCheckboxQuizItem,
+  generatePrivateSpecWithOneChooseNQuizItem,
   generatePrivateSpecWithOneClosedEndedQuestionQuizItem,
+  generatePrivateSpecWithOneEssayQuizItem,
+  generatePrivateSpecWithOneMatrixQuizItem,
+  generatePrivateSpecWithOneMultipleChoiceDropdownQuizItem,
   generatePrivateSpecWithOneMultipleChoiceQuizItem,
+  generatePrivateSpecWithOneScaleQuizItem,
+  generatePrivateSpecWithOneTimelineQuizItem,
   MESSAGE_AFTER_SUBMISSION_CANARY_FOR_TESTS,
+  OPTION_CELLS_CANARY_FOR_TESTS,
   SHARED_OPTION_FEEDBACK_MESSAGE_CANARY_FOR_TESTS,
   SUCCESS_MESSAGE_CANARY_FOR_TESTS,
+  VALIDITY_REGEX_CANARY_FOR_TESTS,
 } from "./utils/privateSpecGenerator"
 import testClient from "./utils/testClient"
 
 const client = testClient(handler)
 const MODEL_SOLUTION_SPEC_ENDPOINT = "/api/public-spec"
 
-/** This function checks that the object does not have the properties in the notAllowedProperties array.
+/**
+ * This function checks that the object does not have the properties in the notAllowedProperties array.
  * T is a type that has the properties in the notAllowedProperties array.
  */
 function expectPropertiesHaveBeenRemoved<T>(object: unknown, notAllowedProperties: (keyof T)[]) {
@@ -37,25 +58,26 @@ function expectPropertiesHaveBeenRemoved<T>(object: unknown, notAllowedPropertie
   }
 }
 
-describe("Public spec generation", () => {
-  it("Generating with a closed ended question doesn't add illegal properties", async () => {
-    const privateSpec = generatePrivateSpecWithOneClosedEndedQuestionQuizItem()
-    const specRequest: SpecRequest = {
-      request_id: "1",
-      private_spec: privateSpec,
-      upload_url: null,
-    }
-    const response = await client.post(MODEL_SOLUTION_SPEC_ENDPOINT).send(specRequest)
-    // response should be succesful
-    expect(response.status).toEqual(200)
-    const publicSpec = response.body as PublicSpecQuiz
-    const closedEndedQuestionQuizItemModelSolution = publicSpec
-      .items[0] as PublicSpecQuizItemClosedEndedQuestion
-    // Make sure we don't accidentally leak the validity regex
-    // @ts-expect-error: Checking that a property that should not exist does not exist
-    expect(closedEndedQuestionQuizItemModelSolution.validityRegex).toBeUndefined()
-  })
+/**
+ * Stringifies the object and checks that no known canary strings are in the output.
+ * Canary strings are special strings for which we know that they should not be in the output.
+ * If they somehow end up in the output, we know that something has been leaked.
+ */
+function expectNoCanariesInOutput(object: unknown) {
+  // Additional checking just to be sure. If we see a canary string, we know something has been leaked.
+  const objectAsString = JSON.stringify(object)
+  expect(objectAsString).not.toContain(MESSAGE_AFTER_SUBMISSION_CANARY_FOR_TESTS)
+  expect(objectAsString).not.toContain(
+    ADDITIONAL_CORRECTNESS_EXPLANATION_ON_MODEL_SOLUTION_CANARY_FOR_TESTS,
+  )
+  expect(objectAsString).not.toContain(SUCCESS_MESSAGE_CANARY_FOR_TESTS)
+  expect(objectAsString).not.toContain(SHARED_OPTION_FEEDBACK_MESSAGE_CANARY_FOR_TESTS)
+  expect(objectAsString).not.toContain(FAILURE_MESSAGE_CANARY_FOR_TESTS)
+  expect(objectAsString).not.toContain(VALIDITY_REGEX_CANARY_FOR_TESTS)
+  expect(objectAsString).not.toContain(OPTION_CELLS_CANARY_FOR_TESTS)
+}
 
+describe("Public spec generation", () => {
   it("Generating with a multiple choice doesn't add illegal properties", async () => {
     const privateSpec = generatePrivateSpecWithOneMultipleChoiceQuizItem()
     const specRequest: SpecRequest = {
@@ -64,7 +86,6 @@ describe("Public spec generation", () => {
       upload_url: null,
     }
     const response = await client.post(MODEL_SOLUTION_SPEC_ENDPOINT).send(specRequest)
-    // response should be succesful
     expect(response.status).toEqual(200)
     const publicSpec = response.body as PublicSpecQuiz
 
@@ -84,13 +105,199 @@ describe("Public spec generation", () => {
         ])
       }
     }
-    // Additional checking just to be sure. If we see a canary string, we know something has been leaked.
-    const specAsString = JSON.stringify(publicSpec)
-    expect(specAsString).not.toContain(MESSAGE_AFTER_SUBMISSION_CANARY_FOR_TESTS)
-    expect(specAsString).not.toContain(
-      ADDITIONAL_CORRECTNESS_EXPLANATION_ON_MODEL_SOLUTION_CANARY_FOR_TESTS,
-    )
-    expect(specAsString).not.toContain(SUCCESS_MESSAGE_CANARY_FOR_TESTS)
-    expect(specAsString).not.toContain(SHARED_OPTION_FEEDBACK_MESSAGE_CANARY_FOR_TESTS)
+    expectNoCanariesInOutput(publicSpec)
+  })
+
+  it("Generating with a essay doesn't add illegal properties", async () => {
+    const privateSpec = generatePrivateSpecWithOneEssayQuizItem()
+    const specRequest: SpecRequest = {
+      request_id: "1",
+      private_spec: privateSpec,
+      upload_url: null,
+    }
+    const response = await client.post(MODEL_SOLUTION_SPEC_ENDPOINT).send(specRequest)
+    expect(response.status).toEqual(200)
+    const publicSpec = response.body as PublicSpecQuiz
+
+    expectPropertiesHaveBeenRemoved<PrivateSpecQuiz>(publicSpec, ["submitMessage"])
+    // verify quiz items
+    for (const quizItem of publicSpec.items) {
+      expectPropertiesHaveBeenRemoved<PrivateSpecQuizItemEssay>(quizItem, [
+        "successMessage",
+        "failureMessage",
+      ])
+    }
+    expectNoCanariesInOutput(publicSpec)
+  })
+
+  it("Generating with a scale doesn't add illegal properties", async () => {
+    const privateSpec = generatePrivateSpecWithOneScaleQuizItem()
+    const specRequest: SpecRequest = {
+      request_id: "1",
+      private_spec: privateSpec,
+      upload_url: null,
+    }
+    const response = await client.post(MODEL_SOLUTION_SPEC_ENDPOINT).send(specRequest)
+    expect(response.status).toEqual(200)
+    const publicSpec = response.body as PublicSpecQuiz
+
+    expectPropertiesHaveBeenRemoved<PrivateSpecQuiz>(publicSpec, ["submitMessage"])
+    // verify quiz items
+    for (const quizItem of publicSpec.items) {
+      expectPropertiesHaveBeenRemoved<PrivateSpecQuizItemScale>(quizItem, [
+        "successMessage",
+        "failureMessage",
+      ])
+    }
+    expectNoCanariesInOutput(publicSpec)
+  })
+
+  it("Generating with a checkbox doesn't add illegal properties", async () => {
+    const privateSpec = generatePrivateSpecWithOneCheckboxQuizItem()
+    const specRequest: SpecRequest = {
+      request_id: "1",
+      private_spec: privateSpec,
+      upload_url: null,
+    }
+    const response = await client.post(MODEL_SOLUTION_SPEC_ENDPOINT).send(specRequest)
+    expect(response.status).toEqual(200)
+    const publicSpec = response.body as PublicSpecQuiz
+
+    expectPropertiesHaveBeenRemoved<PrivateSpecQuiz>(publicSpec, ["submitMessage"])
+    // verify quiz items
+    for (const quizItem of publicSpec.items) {
+      expectPropertiesHaveBeenRemoved<PrivateSpecQuizItemCheckbox>(quizItem, [
+        "successMessage",
+        "failureMessage",
+      ])
+    }
+    expectNoCanariesInOutput(publicSpec)
+  })
+
+  it("Generating with a closed ended question doesn't add illegal properties", async () => {
+    const privateSpec = generatePrivateSpecWithOneClosedEndedQuestionQuizItem()
+    const specRequest: SpecRequest = {
+      request_id: "1",
+      private_spec: privateSpec,
+      upload_url: null,
+    }
+    const response = await client.post(MODEL_SOLUTION_SPEC_ENDPOINT).send(specRequest)
+    expect(response.status).toEqual(200)
+    const publicSpec = response.body as PublicSpecQuiz
+
+    expectPropertiesHaveBeenRemoved<PrivateSpecQuiz>(publicSpec, ["submitMessage"])
+    // verify quiz items
+    for (const quizItem of publicSpec.items) {
+      expectPropertiesHaveBeenRemoved<PrivateSpecQuizItemClosedEndedQuestion>(quizItem, [
+        "successMessage",
+        "failureMessage",
+        "validityRegex",
+      ])
+    }
+    expectNoCanariesInOutput(publicSpec)
+  })
+
+  it("Generating with a matrix doesn't add illegal properties", async () => {
+    const privateSpec = generatePrivateSpecWithOneMatrixQuizItem()
+    const specRequest: SpecRequest = {
+      request_id: "1",
+      private_spec: privateSpec,
+      upload_url: null,
+    }
+    const response = await client.post(MODEL_SOLUTION_SPEC_ENDPOINT).send(specRequest)
+    expect(response.status).toEqual(200)
+    const publicSpec = response.body as PublicSpecQuiz
+
+    expectPropertiesHaveBeenRemoved<PrivateSpecQuiz>(publicSpec, ["submitMessage"])
+    // verify quiz items
+    for (const quizItem of publicSpec.items) {
+      expectPropertiesHaveBeenRemoved<PrivateSpecQuizItemMatrix>(quizItem, [
+        "successMessage",
+        "failureMessage",
+        "optionCells",
+      ])
+    }
+    expectNoCanariesInOutput(publicSpec)
+  })
+
+  it("Generating with a timeline doesn't add illegal properties", async () => {
+    const privateSpec = generatePrivateSpecWithOneTimelineQuizItem()
+    const specRequest: SpecRequest = {
+      request_id: "1",
+      private_spec: privateSpec,
+      upload_url: null,
+    }
+    const response = await client.post(MODEL_SOLUTION_SPEC_ENDPOINT).send(specRequest)
+    expect(response.status).toEqual(200)
+    const publicSpec = response.body as PublicSpecQuiz
+
+    expectPropertiesHaveBeenRemoved<PrivateSpecQuiz>(publicSpec, ["submitMessage"])
+    // verify quiz items
+    for (const quizItem of publicSpec.items) {
+      expectPropertiesHaveBeenRemoved<PrivateSpecQuizItemTimeline>(quizItem, [
+        "successMessage",
+        "failureMessage",
+        "timelineItems",
+      ])
+    }
+    expectNoCanariesInOutput(publicSpec)
+  })
+
+  it("Generating with a choose n doesn't add illegal properties", async () => {
+    const privateSpec = generatePrivateSpecWithOneChooseNQuizItem()
+    const specRequest: SpecRequest = {
+      request_id: "1",
+      private_spec: privateSpec,
+      upload_url: null,
+    }
+    const response = await client.post(MODEL_SOLUTION_SPEC_ENDPOINT).send(specRequest)
+    expect(response.status).toEqual(200)
+    const publicSpec = response.body as PublicSpecQuiz
+
+    expectPropertiesHaveBeenRemoved<PrivateSpecQuiz>(publicSpec, ["submitMessage"])
+    // verify quiz items
+    for (const quizItem of publicSpec.items) {
+      expectPropertiesHaveBeenRemoved<PrivateSpecQuizItemChooseN>(quizItem, [
+        "successMessage",
+        "failureMessage",
+      ])
+      for (const option of (quizItem as PublicSpecQuizItemChooseN).options) {
+        expectPropertiesHaveBeenRemoved<QuizItemOption>(option, [
+          "correct",
+          "messageAfterSubmissionWhenSelected",
+          "additionalCorrectnessExplanationOnModelSolution",
+        ])
+      }
+    }
+    expectNoCanariesInOutput(publicSpec)
+  })
+
+  it("Generating with a multiple choice dropdown doesn't add illegal properties", async () => {
+    const privateSpec = generatePrivateSpecWithOneMultipleChoiceDropdownQuizItem()
+    const specRequest: SpecRequest = {
+      request_id: "1",
+      private_spec: privateSpec,
+      upload_url: null,
+    }
+    const response = await client.post(MODEL_SOLUTION_SPEC_ENDPOINT).send(specRequest)
+    expect(response.status).toEqual(200)
+    const publicSpec = response.body as PublicSpecQuiz
+
+    expectPropertiesHaveBeenRemoved<PrivateSpecQuiz>(publicSpec, ["submitMessage"])
+    // verify quiz items
+    for (const quizItem of publicSpec.items) {
+      expectPropertiesHaveBeenRemoved<PrivateSpecQuizItemMultiplechoiceDropdown>(quizItem, [
+        "successMessage",
+        "failureMessage",
+      ])
+      for (const option of (quizItem as PublicSpecQuizItemMultiplechoiceDropdown).options) {
+        expectPropertiesHaveBeenRemoved<QuizItemOption>(option, [
+          "correct",
+          "messageAfterSubmissionWhenSelected",
+          "additionalCorrectnessExplanationOnModelSolution",
+        ])
+      }
+    }
+    expectNoCanariesInOutput(publicSpec)
   })
 })
--- services/quizzes/tests/api/utils/privateSpecGenerator.ts
+++ services/quizzes/tests/api/utils/privateSpecGenerator.ts
@@ -1,7 +1,14 @@
 import {
   PrivateSpecQuiz,
+  PrivateSpecQuizItemCheckbox,
+  PrivateSpecQuizItemChooseN,
   PrivateSpecQuizItemClosedEndedQuestion,
+  PrivateSpecQuizItemEssay,
+  PrivateSpecQuizItemMatrix,
   PrivateSpecQuizItemMultiplechoice,
+  PrivateSpecQuizItemMultiplechoiceDropdown,
+  PrivateSpecQuizItemScale,
+  PrivateSpecQuizItemTimeline,
 } from "../../../types/quizTypes/privateSpec"
 
 export const MESSAGE_AFTER_SUBMISSION_CANARY_FOR_TESTS = "You should see this after a submission"
@@ -10,6 +17,10 @@ export const ADDITIONAL_CORRECTNESS_EXPLANATION_ON_MODEL_SOLUTION_CANARY_FOR_TES
 export const SUCCESS_MESSAGE_CANARY_FOR_TESTS =
   "You got it right! This spoils the answer, and should only be visible when you answer correctly."
 export const SHARED_OPTION_FEEDBACK_MESSAGE_CANARY_FOR_TESTS = "This may spoil the answer"
+export const FAILURE_MESSAGE_CANARY_FOR_TESTS =
+  "You got it wrong! But this might also spoil something"
+export const VALIDITY_REGEX_CANARY_FOR_TESTS = "^This one spoils the correct answer$"
+export const OPTION_CELLS_CANARY_FOR_TESTS = "234223523"
 
 export function generateEmptyPrivateSpecQuiz(): PrivateSpecQuiz {
   return {
@@ -30,12 +41,12 @@ export function generatePrivateSpecWithOneClosedEndedQuestionQuizItem(): Private
     type: "closed-ended-question",
     id: "988b9c17-9f03-4062-b5ff-c6071d3c6f06",
     order: 0,
-    validityRegex: "^blue$",
+    validityRegex: VALIDITY_REGEX_CANARY_FOR_TESTS,
     formatRegex: "[a-z]+",
     title: "What color is the sky?",
     body: null,
-    successMessage: null,
-    failureMessage: null,
+    successMessage: SUCCESS_MESSAGE_CANARY_FOR_TESTS,
+    failureMessage: FAILURE_MESSAGE_CANARY_FOR_TESTS,
   }
   return { ...emptyQuiz, items: [closedEndedQuestionQuizItem] }
 }
@@ -73,10 +84,174 @@ export function generatePrivateSpecWithOneMultipleChoiceQuizItem(): PrivateSpecQ
     title: null,
     body: null,
     successMessage: SUCCESS_MESSAGE_CANARY_FOR_TESTS,
-    failureMessage: "You got it wrong!",
-    sharedOptionFeedbackMessage: "This spoils the answer",
+    failureMessage: FAILURE_MESSAGE_CANARY_FOR_TESTS,
+    sharedOptionFeedbackMessage: SHARED_OPTION_FEEDBACK_MESSAGE_CANARY_FOR_TESTS,
     optionDisplayDirection: "vertical",
     multipleChoiceMultipleOptionsGradingPolicy: "default",
+  }
+
+  return { ...emptyQuiz, items: [closedEndedQuestionQuizItem] }
+}
+
+export function generatePrivateSpecWithOneEssayQuizItem(): PrivateSpecQuiz {
+  const emptyQuiz = generateEmptyPrivateSpecQuiz()
+  const closedEndedQuestionQuizItem: PrivateSpecQuizItemEssay = {
+    type: "essay",
+    id: "988b9c17-9f03-4062-b5ff-c6071d3c6f06",
+    order: 0,
+    title: "What color is the sky?",
+    body: null,
+    successMessage: SUCCESS_MESSAGE_CANARY_FOR_TESTS,
+    failureMessage: FAILURE_MESSAGE_CANARY_FOR_TESTS,
+    minWords: 20,
+    maxWords: 100,
+  }
+
+  return { ...emptyQuiz, items: [closedEndedQuestionQuizItem] }
+}
+
+export function generatePrivateSpecWithOneScaleQuizItem(): PrivateSpecQuiz {
+  const emptyQuiz = generateEmptyPrivateSpecQuiz()
+  const closedEndedQuestionQuizItem: PrivateSpecQuizItemScale = {
+    type: "scale",
+    id: "988b9c17-9f03-4062-b5ff-c6071d3c6f06",
+    order: 0,
+    maxValue: 1,
+    minValue: 5,
+    maxLabel: "Max label",
+    minLabel: "Min label",
+    title: null,
+    body: null,
+    successMessage: SUCCESS_MESSAGE_CANARY_FOR_TESTS,
+    failureMessage: FAILURE_MESSAGE_CANARY_FOR_TESTS,
+  }
+
+  return { ...emptyQuiz, items: [closedEndedQuestionQuizItem] }
+}
+
+export function generatePrivateSpecWithOneCheckboxQuizItem(): PrivateSpecQuiz {
+  const emptyQuiz = generateEmptyPrivateSpecQuiz()
+  const closedEndedQuestionQuizItem: PrivateSpecQuizItemCheckbox = {
+    type: "checkbox",
+    id: "988b9c17-9f03-4062-b5ff-c6071d3c6f06",
+    order: 0,
+    title: "Will you check this box?",
+    body: null,
+    successMessage: SUCCESS_MESSAGE_CANARY_FOR_TESTS,
+    failureMessage: FAILURE_MESSAGE_CANARY_FOR_TESTS,
+  }
+
+  return { ...emptyQuiz, items: [closedEndedQuestionQuizItem] }
+}
+
+export function generatePrivateSpecWithOneMatrixQuizItem(): PrivateSpecQuiz {
+  const emptyQuiz = generateEmptyPrivateSpecQuiz()
+  const closedEndedQuestionQuizItem: PrivateSpecQuizItemMatrix = {
+    type: "matrix",
+    id: "988b9c17-9f03-4062-b5ff-c6071d3c6f06",
+    order: 0,
+    successMessage: SUCCESS_MESSAGE_CANARY_FOR_TESTS,
+    failureMessage: FAILURE_MESSAGE_CANARY_FOR_TESTS,
+    optionCells: [
+      ["1", "2"],
+      ["3", OPTION_CELLS_CANARY_FOR_TESTS],
+    ],
+  }
+
+  return { ...emptyQuiz, items: [closedEndedQuestionQuizItem] }
+}
+
+export function generatePrivateSpecWithOneTimelineQuizItem(): PrivateSpecQuiz {
+  const emptyQuiz = generateEmptyPrivateSpecQuiz()
+  const closedEndedQuestionQuizItem: PrivateSpecQuizItemTimeline = {
+    type: "timeline",
+    id: "988b9c17-9f03-4062-b5ff-c6071d3c6f06",
+    order: 0,
+    successMessage: SUCCESS_MESSAGE_CANARY_FOR_TESTS,
+    failureMessage: FAILURE_MESSAGE_CANARY_FOR_TESTS,
+    timelineItems: [
+      {
+        id: "1",
+        year: "2000",
+        correctEventName: "Event 1",
+        correctEventId: "event-1",
+      },
+    ],
+  }
+
+  return { ...emptyQuiz, items: [closedEndedQuestionQuizItem] }
+}
+
+export function generatePrivateSpecWithOneChooseNQuizItem(): PrivateSpecQuiz {
+  const emptyQuiz = generateEmptyPrivateSpecQuiz()
+  const closedEndedQuestionQuizItem: PrivateSpecQuizItemChooseN = {
+    type: "choose-n",
+    id: "988b9c17-9f03-4062-b5ff-c6071d3c6f06",
+    order: 0,
+    successMessage: SUCCESS_MESSAGE_CANARY_FOR_TESTS,
+    failureMessage: FAILURE_MESSAGE_CANARY_FOR_TESTS,
+    n: 10,
+    options: [
+      {
+        id: "id-1",
+        order: 0,
+        correct: true,
+        title: "Positive",
+        body: null,
+        messageAfterSubmissionWhenSelected: MESSAGE_AFTER_SUBMISSION_CANARY_FOR_TESTS,
+        additionalCorrectnessExplanationOnModelSolution:
+          ADDITIONAL_CORRECTNESS_EXPLANATION_ON_MODEL_SOLUTION_CANARY_FOR_TESTS,
+      },
+      {
+        id: "id-2",
+        order: 0,
+        correct: false,
+        title: "Just no",
+        body: null,
+        messageAfterSubmissionWhenSelected: MESSAGE_AFTER_SUBMISSION_CANARY_FOR_TESTS,
+        additionalCorrectnessExplanationOnModelSolution:
+          ADDITIONAL_CORRECTNESS_EXPLANATION_ON_MODEL_SOLUTION_CANARY_FOR_TESTS,
+      },
+    ],
+    title: null,
+    body: null,
+  }
+
+  return { ...emptyQuiz, items: [closedEndedQuestionQuizItem] }
+}
+
+export function generatePrivateSpecWithOneMultipleChoiceDropdownQuizItem(): PrivateSpecQuiz {
+  const emptyQuiz = generateEmptyPrivateSpecQuiz()
+  const closedEndedQuestionQuizItem: PrivateSpecQuizItemMultiplechoiceDropdown = {
+    type: "multiple-choice-dropdown",
+    id: "988b9c17-9f03-4062-b5ff-c6071d3c6f06",
+    order: 0,
+    successMessage: SUCCESS_MESSAGE_CANARY_FOR_TESTS,
+    failureMessage: FAILURE_MESSAGE_CANARY_FOR_TESTS,
+    options: [
+      {
+        id: "id-1",
+        order: 0,
+        correct: true,
+        title: "Positive",
+        body: null,
+        messageAfterSubmissionWhenSelected: MESSAGE_AFTER_SUBMISSION_CANARY_FOR_TESTS,
+        additionalCorrectnessExplanationOnModelSolution:
+          ADDITIONAL_CORRECTNESS_EXPLANATION_ON_MODEL_SOLUTION_CANARY_FOR_TESTS,
+      },
+      {
+        id: "id-2",
+        order: 0,
+        correct: false,
+        title: "Just no",
+        body: null,
+        messageAfterSubmissionWhenSelected: MESSAGE_AFTER_SUBMISSION_CANARY_FOR_TESTS,
+        additionalCorrectnessExplanationOnModelSolution:
+          ADDITIONAL_CORRECTNESS_EXPLANATION_ON_MODEL_SOLUTION_CANARY_FOR_TESTS,
+      },
+    ],
+    title: null,
+    body: null,
   }
 
   return { ...emptyQuiz, items: [closedEndedQuestionQuizItem] }

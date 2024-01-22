5894551139ecb58dd47746e6397fd2f40afaf96f
Anastasia Diseth <annadiseth@gmail.com>
2024-01-22 08:50:25 +0200

    refactor  BlockPlaceholderWrapper

--- services/cms/src/blocks/BlockPlaceholderWrapper.tsx
+++ services/cms/src/blocks/BlockPlaceholderWrapper.tsx
@@ -5,15 +5,23 @@
 
 interface BlockPlaceholderWrapperProps {
   id: string
+  title?: string
+  explanation?: string
 }
 
 const BlockPlaceholderWrapper: React.FC<React.PropsWithChildren<BlockPlaceholderWrapperProps>> = ({
   id,
   children,
+  title,
+  explanation,
 }) => {
   return (
     <div className={"wp-block wp-block-embed"} id={"placeholder-block-" + id}>
-      <div className={"components-placeholder"}>{children}</div>
+      <div className={"components-placeholder"}>
+        <h3>{title}</h3>
+        <p>{explanation}</p>
+        {children}
+      </div>
     </div>
   )
 }

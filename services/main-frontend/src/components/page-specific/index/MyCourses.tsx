45ebada43b87f0f71d3702255851abfd9ee50cff
Henrik Nygren <nygrenh@gmail.com>
2024-02-13 14:02:13 +0200

    Add a front page

--- services/main-frontend/src/components/page-specific/index/MyCourses.tsx
+++ services/main-frontend/src/components/page-specific/index/MyCourses.tsx
@@ -1,21 +1,62 @@
 import { useQuery } from "@tanstack/react-query"
+import { useTranslation } from "react-i18next"
 
+import useAllOrganizationsQuery from "../../../hooks/useAllOrganizationsQuery"
 import { getMyCourses } from "../../../services/backend/users"
 import ErrorBanner from "../../../shared-module/components/ErrorBanner"
 import Spinner from "../../../shared-module/components/Spinner"
+import useAuthorizeMultiple from "../../../shared-module/hooks/useAuthorizeMultiple"
+import { CourseComponent, CourseGrid } from "../org/organizationSlug/CourseCard"
 
 const MyCourses: React.FC = () => {
+  const { t } = useTranslation()
   const myCoursesQuery = useQuery({
     queryKey: ["my-courses"],
     queryFn: () => getMyCourses(),
   })
-  if (myCoursesQuery.isError) {
-    return <ErrorBanner error={myCoursesQuery.error} variant="readOnly" />
+  const allOrganizationsQuery = useAllOrganizationsQuery()
+  const canMangeCourse = useAuthorizeMultiple(
+    myCoursesQuery.data?.map((course) => {
+      // eslint-disable-next-line i18next/no-literal-string
+      return { action: { type: "teach" }, resource: { type: "course", id: course.id } }
+    }) ?? [],
+  )
+  if (myCoursesQuery.isError || allOrganizationsQuery.isError) {
+    return (
+      <ErrorBanner error={myCoursesQuery.error ?? allOrganizationsQuery.error} variant="readOnly" />
+    )
   }
-  if (myCoursesQuery.isLoading) {
+
+  if (myCoursesQuery.isLoading || allOrganizationsQuery.isLoading) {
     return <Spinner variant="medium" />
   }
-  return <pre>{JSON.stringify(myCoursesQuery.data, undefined, 2)}</pre>
+
+  return (
+    <CourseGrid>
+      {myCoursesQuery.data?.map((course, n) => {
+        const organization = allOrganizationsQuery.data?.find(
+          (org) => org.id === course.organization_id,
+        )
+        return (
+          <CourseComponent
+            key={course.id}
+            title={course.name}
+            isDraft={course.is_draft}
+            description={course.description ?? t("no-description-available")}
+            languageCode={course.language_code}
+            // eslint-disable-next-line i18next/no-literal-string
+            manageHref={`/manage/courses/${course.id}`}
+            // eslint-disable-next-line i18next/no-literal-string
+            navigateToCourseHref={`/org/${organization?.slug ?? "undefined"}/courses/${
+              course.slug
+            }`}
+            id={course.id}
+            showManageButton={canMangeCourse.data?.[n] === true}
+          />
+        )
+      })}
+    </CourseGrid>
+  )
 }
 
 export default MyCourses
--- services/main-frontend/src/pages/index.tsx
+++ services/main-frontend/src/pages/index.tsx
@@ -1,15 +1,111 @@
+import { css } from "@emotion/css"
+import Link from "next/link"
+import { useTranslation } from "react-i18next"
+
 import MyCourses from "../components/page-specific/index/MyCourses"
+import OnlyRenderIfPermissions from "../shared-module/components/OnlyRenderIfPermissions"
 import { withSignedIn } from "../shared-module/contexts/LoginStateContext"
+import {
+  allOrganizationsRoute,
+  globalPermissionsRoute,
+  globalStatsRoute,
+  manageExerciseServicesRoute,
+  searchUsersRoute,
+} from "../shared-module/utils/routes"
 import withErrorBoundary from "../shared-module/utils/withErrorBoundary"
 
 const FrontPage = () => {
+  const { t } = useTranslation()
+
   return (
     <div>
-      <h1>Welcome to lol</h1>
-      <h2>My courses</h2>
+      <h1>{t("welcome-to-place", { place: document.location.host })}</h1>
+      <h2>{t("heading-my-courses")}</h2>
       <MyCourses />
-      <h2>Register completion and certificates</h2>
-      <h2>Permissions</h2>
+
+      <h2>{t("heading-navigation")}</h2>
+      <div>
+        <a href="https://www.mooc.fi">{t("link-text-find-more-courses")}</a>
+      </div>
+      <div>
+        <Link
+          href={allOrganizationsRoute()}
+          className={css`
+            cursor: pointer;
+            color: blue;
+            text-decoration: underline;
+          `}
+        >
+          {t("link-text-all-organizations")}
+        </Link>
+      </div>
+
+      <OnlyRenderIfPermissions action={{ type: "edit" }} resource={{ type: "global_permissions" }}>
+        <div>
+          <Link
+            href={manageExerciseServicesRoute()}
+            className={css`
+              cursor: pointer;
+              color: blue;
+              text-decoration: underline;
+            `}
+          >
+            {t("link-manage-exercise-services")}
+          </Link>
+        </div>
+      </OnlyRenderIfPermissions>
+
+      <OnlyRenderIfPermissions
+        action={{ type: "view_user_progress_or_details" }}
+        resource={{ type: "global_permissions" }}
+      >
+        <div>
+          <Link
+            href={searchUsersRoute()}
+            className={css`
+              cursor: pointer;
+              color: blue;
+              text-decoration: underline;
+            `}
+          >
+            {t("title-user-search")}
+          </Link>
+        </div>
+      </OnlyRenderIfPermissions>
+      <OnlyRenderIfPermissions
+        action={{ type: "edit_role", variant: "Admin" }}
+        resource={{ type: "global_permissions" }}
+      >
+        <div>
+          <Link
+            href={globalPermissionsRoute()}
+            className={css`
+              cursor: pointer;
+              color: blue;
+              text-decoration: underline;
+            `}
+          >
+            {t("global-permissions")}
+          </Link>
+        </div>
+      </OnlyRenderIfPermissions>
+      <OnlyRenderIfPermissions
+        action={{ type: "view_stats" }}
+        resource={{ type: "global_permissions" }}
+      >
+        <div>
+          <Link
+            href={globalStatsRoute()}
+            className={css`
+              cursor: pointer;
+              color: blue;
+              text-decoration: underline;
+            `}
+          >
+            {t("link-text-global-stats")}
+          </Link>
+        </div>
+      </OnlyRenderIfPermissions>
     </div>
   )
 }

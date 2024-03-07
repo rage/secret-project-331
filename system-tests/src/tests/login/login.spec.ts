d41817eab3f2612426adfd59c1cac482ece121ad
Henrik Nygren <nygrenh@gmail.com>
2024-03-07 14:46:57 +0200

    Eslint fixes

--- system-tests/src/tests/login/login.spec.ts
+++ system-tests/src/tests/login/login.spec.ts
@@ -8,7 +8,7 @@ import {
 
 import { logout } from "../../utils/logout"
 
-test.describe("Login session with Playwright", async () => {
+test.describe("Login session with Playwright", () => {
   /// Login state to use
   test.use({ storageState: "src/states/admin@example.com.json" })
 
@@ -54,7 +54,7 @@ test.describe("Login session with Playwright", async () => {
   })
 })
 
-test.describe("Login return_to", async () => {
+test.describe("Login return_to", () => {
   test.beforeEach(
     async ({
       page,

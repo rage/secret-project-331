d4baf3cc83c12532cf4af3082fc6969e7064585a
Henrik Nygren <nygrenh@gmail.com>
2024-03-08 11:01:33 +0200

    Retry multiple-choice dropdown test

--- system-tests/src/tests/quizzes/feedback/multiple-choice-dropdown.spec.ts
+++ system-tests/src/tests/quizzes/feedback/multiple-choice-dropdown.spec.ts
@@ -8,77 +8,83 @@ test.use({
   storageState: "src/states/user@example.com.json",
 })
 
-test("quizzes multiple-choice-dropdown", async ({ page, headless }, testInfo) => {
-  await page.goto(
-    "http://project-331.local/org/uh-cs/courses/introduction-to-everything/chapter-1/page-5",
-  )
+test.describe(() => {
+  // Chrome sometimes does not render the ui correctly after resizing the window without reloading the page.
+  // This does not seem to be something we can fix, so we'll retry
+  test.describe.configure({ retries: 4 })
 
-  await selectCourseInstanceIfPrompted(page)
+  test("quizzes multiple-choice-dropdown", async ({ page, headless }, testInfo) => {
+    await page.goto(
+      "http://project-331.local/org/uh-cs/courses/introduction-to-everything/chapter-1/page-5",
+    )
 
-  const frame = await getLocatorForNthExerciseServiceIframe(page, "quizzes", 1)
+    await selectCourseInstanceIfPrompted(page)
 
-  await frame.getByText("Choose the right answer from given options.").waitFor()
+    const frame = await getLocatorForNthExerciseServiceIframe(page, "quizzes", 1)
 
-  await frame
-    .locator(`select:below(:text("Choose the right answer from given options."))`)
-    .selectOption({ label: "The Wright answer" })
+    await frame.getByText("Choose the right answer from given options.").waitFor()
 
-  await page.getByText("Submit").click()
-  await page.getByText("Try again").waitFor()
+    await frame
+      .locator(`select:below(:text("Choose the right answer from given options."))`)
+      .selectOption({ label: "The Wright answer" })
 
-  await expectScreenshotsToMatchSnapshots({
-    screenshotTarget: page,
-    headless,
-    testInfo,
-    snapshotName: "multiple-choice-dropdown-feedback-incorrect-answer",
-    waitForTheseToBeVisibleAndStable: [
-      page.locator(`text=This is an extra submit message from the teacher.`),
-    ],
-  })
+    await page.getByText("Submit").click()
+    await page.getByText("Try again").waitFor()
 
-  await page.getByText("Try again").click()
-
-  await frame.getByText("Choose the right answer from given options.").waitFor()
-
-  await frame
-    .locator(`select:below(:text("Choose the right answer from given options."))`)
-    .selectOption({
-      label: "The right answer",
+    await expectScreenshotsToMatchSnapshots({
+      screenshotTarget: page,
+      headless,
+      testInfo,
+      snapshotName: "multiple-choice-dropdown-feedback-incorrect-answer",
+      waitForTheseToBeVisibleAndStable: [
+        page.locator(`text=This is an extra submit message from the teacher.`),
+      ],
     })
 
-  await page.getByText("Submit").click()
-  await page.getByText("Try again").waitFor()
+    await page.getByText("Try again").click()
 
-  await expectScreenshotsToMatchSnapshots({
-    screenshotTarget: page,
-    headless,
-    testInfo,
-    snapshotName: "multiple-choice-dropdown-feedback-correct-answer",
-    waitForTheseToBeVisibleAndStable: [
-      page.locator(`text=This is an extra submit message from the teacher.`),
-    ],
-  })
+    await frame.getByText("Choose the right answer from given options.").waitFor()
 
-  await page.getByText("Try again").click()
+    await frame
+      .locator(`select:below(:text("Choose the right answer from given options."))`)
+      .selectOption({
+        label: "The right answer",
+      })
 
-  await frame.getByText("Choose the right answer from given options.").waitFor()
+    await page.getByText("Submit").click()
+    await page.getByText("Try again").waitFor()
 
-  await frame
-    .locator(`select:below(:text("Choose the right answer from given options."))`)
-    .selectOption({
-      label: "The Wright answer",
+    await expectScreenshotsToMatchSnapshots({
+      screenshotTarget: page,
+      headless,
+      testInfo,
+      snapshotName: "multiple-choice-dropdown-feedback-correct-answer",
+      waitForTheseToBeVisibleAndStable: [
+        page.locator(`text=This is an extra submit message from the teacher.`),
+      ],
     })
 
-  await page.getByText("Submit").click()
-  await page.getByText("Try again").waitFor()
+    await page.getByText("Try again").click()
 
-  await expectScreenshotsToMatchSnapshots({
-    screenshotTarget: page,
-    headless,
-    testInfo,
-    snapshotName: "multiple-choice-dropdown-feedback-incorrect-answer-after-correct",
-    waitForTheseToBeVisibleAndStable: [
-      page.locator(`text=This is an extra submit message from the teacher.`),
-    ],
+    await frame.getByText("Choose the right answer from given options.").waitFor()
+
+    await frame
+      .locator(`select:below(:text("Choose the right answer from given options."))`)
+      .selectOption({
+        label: "The Wright answer",
+      })
+
+    await page.getByText("Submit").click()
+    await page.getByText("Try again").waitFor()
+
+    await expectScreenshotsToMatchSnapshots({
+      screenshotTarget: page,
+      headless,
+      testInfo,
+      snapshotName: "multiple-choice-dropdown-feedback-incorrect-answer-after-correct",
+      waitForTheseToBeVisibleAndStable: [
+        page.locator(`text=This is an extra submit message from the teacher.`),
+      ],
+    })
   })
 })

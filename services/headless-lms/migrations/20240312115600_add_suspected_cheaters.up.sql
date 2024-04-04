43998a5ca9677b175dd2f4be207df97f7ac73746
george-misan <george.eyoyibo@gmail.com>
2024-04-04 09:11:06 +0300

    [cheater-feature]: update migration files

--- services/headless-lms/migrations/20240312115600_add_suspected_cheaters.up.sql
+++ services/headless-lms/migrations/20240312115600_add_suspected_cheaters.up.sql
@@ -5,7 +5,7 @@ CREATE TABLE suspected_cheaters (
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
   deleted_at TIMESTAMP WITH TIME ZONE,
   total_duration INTEGER NOT NULL,
-  total_points INTEGER NOT NULL, 
+  total_points INTEGER NOT NULL
 );
 
 CREATE TRIGGER set_timestamp
@@ -20,9 +20,7 @@ COMMENT ON COLUMN suspected_cheaters.created_at IS 'Timestamp when the record wa
 COMMENT ON COLUMN suspected_cheaters.updates_at IS 'Timestamp when the record was updated.';
 COMMENT ON COLUMN suspected_cheaters.deleted_at IS 'Timestamp when the record was deleted. If null, the record is not deleted.';
 COMMENT ON COLUMN suspected_cheaters.total_duration IS 'The total duration the student spend completing the course.';
-COMMENT ON COLUMN suspected_cheaters.student_average_duration IS 'The average total duration other student spent completing the course.';
 COMMENT ON COLUMN suspected_cheaters.total_points IS 'The total points the student acquired in the course.';
-COMMENT ON COLUMN suspected_cheaters.student_average_points IS 'The average total points other students acquired in the course.';
 
 CREATE TABLE course_student_average (
   id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
@@ -44,5 +42,5 @@ COMMENT ON COLUMN course_student_average.course_id IS 'A unique, stable identifi
 COMMENT ON COLUMN course_student_average.created_at IS 'Timestamp when the record was created.';
 COMMENT ON COLUMN course_student_average.updates_at IS 'Timestamp when the record was updated.';
 COMMENT ON COLUMN course_student_average.deleted_at IS 'Timestamp when the record was deleted. If null, the record is not deleted.';
-COMMENT ON COLUMN course_student_average.student_average_duration IS 'The average total duration other student spent completing the course.';
-COMMENT ON COLUMN course_student_average.student_average_points IS 'The average total points other students acquired in the course.';
+COMMENT ON COLUMN course_student_average.student_average_duration IS 'The average duration all student spent completing the course.';
+COMMENT ON COLUMN course_student_average.student_average_points IS 'The average points all students acquired in the course.';
--- services/headless-lms/migrations/20240312121500_add_suspected_cheaters_exercise_list.up.sql
+++ services/headless-lms/migrations/20240312121500_add_suspected_cheaters_exercise_list.up.sql
@@ -7,7 +7,7 @@ CREATE TABLE exercise_list_of_suspected_cheaters_exercise_list (
   duration INTEGER NOT NULL,
   points INTEGER NOT NULL,
   attempts INTEGER NOT NULL,
-  status activity_progress NOT NULL DEFAULT 'initialized',
+  status activity_progress NOT NULL DEFAULT 'initialized'
 );
 
 CREATE TRIGGER set_timestamp
@@ -22,13 +22,11 @@ COMMENT ON COLUMN suspected_cheaters_exercise_list.created_at IS 'Timestamp when
 COMMENT ON COLUMN suspected_cheaters_exercise_list.deleted_at IS 'Timestamp when the record was deleted. If null, the record is not deleted.';
 COMMENT ON COLUMN suspected_cheaters_exercise_list.exercise_id IS 'Exercise Id of an exercise completed by the suspected student.';
 COMMENT ON COLUMN suspected_cheaters_exercise_list.duration IS 'The duration a suspected student used in completing an exercise.';
-COMMENT ON COLUMN suspected_cheaters_exercise_list.student_average_duration IS 'The average duration a other student used in completing an exercise.';
 COMMENT ON COLUMN suspected_cheaters_exercise_list.points IS 'The points a suspected student received from completing an exercise.';
-COMMENT ON COLUMN suspected_cheaters_exercise_list.student_average_points IS 'The average points other student received from completing an exercise.';
 COMMENT ON COLUMN suspected_cheaters_exercise_list.attempts IS 'The number of times a student attempt an exercise.';
 COMMENT ON COLUMN suspected_cheaters_exercise_list.status IS 'The status of an exercise.';
 
--- user_exercise_states
+-- suspected_cheater_exercise_states
 CREATE TYPE activity_progress AS ENUM (
   'initialized',
   'started',
@@ -44,7 +42,7 @@ CREATE TABLE exercise_student_average (
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
   deleted_at TIMESTAMP WITH TIME ZONE,
   exercise_average_duration INTEGER NOT NULL,
-  exercise_average_points INTEGER NOT NULL,
+  exercise_average_points INTEGER NOT NULL
 );
 
 CREATE TRIGGER set_timestamp
@@ -58,5 +56,5 @@ COMMENT ON COLUMN exercise_student_average.exercise_id IS 'The exercise_id of th
 COMMENT ON COLUMN exercise_student_average.created_at IS 'Timestamp when the record was created.';
 COMMENT ON COLUMN exercise_student_average.updates_at IS 'Timestamp when the record was updated.';
 COMMENT ON COLUMN exercise_student_average.deleted_at IS 'Timestamp when the record was deleted. If null, the record is not deleted.';
-COMMENT ON COLUMN exercise_student_average.student_average_duration IS 'The average duration a other student used in completing an exercise.';
-COMMENT ON COLUMN exercise_student_average.student_average_points IS 'The average points other student received from completing an exercise.';
+COMMENT ON COLUMN exercise_student_average.student_average_duration IS 'The average duration a all student used in completing an exercise.';
+COMMENT ON COLUMN exercise_student_average.student_average_points IS 'The average points all student received from completing an exercise.';

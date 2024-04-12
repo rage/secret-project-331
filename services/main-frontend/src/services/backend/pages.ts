8ee583490bb9ba0bf852e8a570b6ca97e04e1ab1
Henrik Nygren <nygrenh@gmail.com>
2024-04-12 16:05:47 +0300

    Fix uploading audio files (#1264)

--- services/main-frontend/src/services/backend/pages.ts
+++ services/main-frontend/src/services/backend/pages.ts
@@ -16,7 +16,6 @@ import {
   isPageInfo,
 } from "../../shared-module/bindings.guard"
 import { isArray, isNumber, isString, validateResponse } from "../../shared-module/utils/fetching"
-import { validateFile } from "../../shared-module/utils/files"
 import { mainFrontendClient } from "../mainFrontendClient"
 
 export const postNewPage = async (data: NewPage): Promise<Page> => {
@@ -54,20 +53,6 @@ export const restorePage = async (pageId: string, historyId: string): Promise<st
 export const fetchPageInfo = async (pageId: string): Promise<PageInfo> => {
   const response = await mainFrontendClient.get(`/pages/${pageId}/info`)
   return validateResponse(response, isPageInfo)
-}
-
-export const postPageAudioFile = async (pageId: string, file: File): Promise<boolean> => {
-  // eslint-disable-next-line i18next/no-literal-string
-  validateFile(file, ["audio"])
-  const data = new FormData()
-  // eslint-disable-next-line i18next/no-literal-string
-  data.append("file", file, file.name || "unknown")
-  const response = await mainFrontendClient.post(`/page_audio/${pageId}`, data)
-  return validateResponse(response, isBoolean)
-}
-
-export const removePageAudioFile = async (fileId: string): Promise<void> => {
-  await mainFrontendClient.delete(`/page_audio/${fileId}`)
 }
 
 export const fetchPageAudioFiles = async (pageId: string): Promise<PageAudioFile[]> => {

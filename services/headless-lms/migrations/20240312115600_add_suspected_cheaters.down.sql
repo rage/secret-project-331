d1ccf991c84ab7e728d14e9b5130daa25f9a5302
george-misan <george.eyoyibo@gmail.com>
2024-04-27 15:12:15 +0300

    [migration-job]: update migaration file

--- services/headless-lms/migrations/20240312115600_add_suspected_cheaters.down.sql
+++ services/headless-lms/migrations/20240312115600_add_suspected_cheaters.down.sql
@@ -1,8 +1,2 @@
 DROP TABLE suspected_cheaters;
-DROP TABLE course_student_average;
-
---
-
-DROP TABLE suspected_cheaters_exercise_list;
-DROP TABLE exercise_student_average;
 DROP TABLE cheater_thresholds;
--- services/headless-lms/migrations/20240312115600_add_suspected_cheaters.up.sql
+++ services/headless-lms/migrations/20240312115600_add_suspected_cheaters.up.sql
@@ -9,78 +9,16 @@ CREATE TABLE suspected_cheaters (
 );
 CREATE TRIGGER set_timestamp BEFORE
 UPDATE ON suspected_cheaters FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
-COMMENT ON TABLE suspected_cheaters IS 'This table stores data regarding student that are suspected of cheating in a course.';
+-- The suspected_cheaters table is a that contains a student that are suspected of cheating because they meet the cheating requirement (i.e. score > threshold && duration > average_duration)
+COMMENT ON TABLE suspected_cheaters IS 'This table stores data regarding student that are suspected of cheating in a course instance.';
 COMMENT ON COLUMN suspected_cheaters.id IS 'A unique, stable identifier for the record.';
 COMMENT ON COLUMN suspected_cheaters.user_id IS 'The user_id of the student being suspected.';
 COMMENT ON COLUMN suspected_cheaters.created_at IS 'Timestamp when the record was created.';
 COMMENT ON COLUMN suspected_cheaters.updated_at IS 'Timestamp when the record was updated.';
 COMMENT ON COLUMN suspected_cheaters.deleted_at IS 'Timestamp when the record was deleted. If null, the record is not deleted.';
-COMMENT ON COLUMN suspected_cheaters.total_duration_seconds IS 'The total duration the student spend completing the course.';
-COMMENT ON COLUMN suspected_cheaters.total_points IS 'The total points the student acquired in the course.';
-CREATE TABLE course_student_average (
-  id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  course_instance_id UUID NOT NULL REFERENCES course_instances,
-  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  deleted_at TIMESTAMP WITH TIME ZONE,
-  average_duration_seconds INTEGER,
-  average_points INTEGER NOT NULL
-);
-CREATE TRIGGER set_timestamp BEFORE
-UPDATE ON course_student_average FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
-COMMENT ON TABLE course_student_average IS 'This table stores data regarding the average in a specific course.';
-COMMENT ON COLUMN course_student_average.course_instance_id IS 'A unique, stable identifier for the record.';
-COMMENT ON COLUMN course_student_average.created_at IS 'Timestamp when the record was created.';
-COMMENT ON COLUMN course_student_average.updated_at IS 'Timestamp when the record was updated.';
-COMMENT ON COLUMN course_student_average.deleted_at IS 'Timestamp when the record was deleted. If null, the record is not deleted.';
-COMMENT ON COLUMN course_student_average.average_duration_seconds IS 'The average duration all student spent completing the course.';
-COMMENT ON COLUMN course_student_average.average_points IS 'The average points all students acquired in the course.';
---
-
-CREATE TABLE suspected_cheaters_exercise_list (
-  id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  student_id UUID NOT NULL REFERENCES course_module_completions,
-  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  deleted_at TIMESTAMP WITH TIME ZONE,
-  exercise_id UUID REFERENCES exercises NOT NULL,
-  duration_seconds INTEGER,
-  points INTEGER NOT NULL,
-  attempts INTEGER NOT NULL,
-  status activity_progress NOT NULL DEFAULT 'initialized'
-);
-CREATE TRIGGER set_timestamp BEFORE
-UPDATE ON suspected_cheaters_exercise_list FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
-COMMENT ON TABLE suspected_cheaters_exercise_list IS 'This table stores data regarding the list of exercises pertaining to students that have been suspected of cheating in a course.';
-COMMENT ON COLUMN suspected_cheaters_exercise_list.id IS 'A unique, stable identifier for the record.';
-COMMENT ON COLUMN suspected_cheaters_exercise_list.student_id IS 'The id of the student being suspected.';
-COMMENT ON COLUMN suspected_cheaters_exercise_list.updated_at IS 'Timestamp when the record was updated.';
-COMMENT ON COLUMN suspected_cheaters_exercise_list.created_at IS 'Timestamp when the record was created.';
-COMMENT ON COLUMN suspected_cheaters_exercise_list.deleted_at IS 'Timestamp when the record was deleted. If null, the record is not deleted.';
-COMMENT ON COLUMN suspected_cheaters_exercise_list.exercise_id IS 'Exercise Id of an exercise completed by the suspected student.';
-COMMENT ON COLUMN suspected_cheaters_exercise_list.duration_seconds IS 'The duration a suspected student used in completing an exercise.';
-COMMENT ON COLUMN suspected_cheaters_exercise_list.points IS 'The points a suspected student received from completing an exercise.';
-COMMENT ON COLUMN suspected_cheaters_exercise_list.attempts IS 'The number of times a student attempt an exercise.';
-COMMENT ON COLUMN suspected_cheaters_exercise_list.status IS 'The status of an exercise.';
-CREATE TABLE exercise_student_average (
-  id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  exercise_id UUID NOT NULL REFERENCES exercises,
-  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-  deleted_at TIMESTAMP WITH TIME ZONE,
-  average_duration_seconds INTEGER,
-  average_points INTEGER NOT NULL
-);
-CREATE TRIGGER set_timestamp BEFORE
-UPDATE ON exercise_student_average FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
-COMMENT ON TABLE exercise_student_average IS 'This table stores data regarding the average in a specific course.';
-COMMENT ON COLUMN exercise_student_average.id IS 'A unique, stable identifier for the record.';
-COMMENT ON COLUMN exercise_student_average.exercise_id IS 'The exercise_id of the exercise.';
-COMMENT ON COLUMN exercise_student_average.created_at IS 'Timestamp when the record was created.';
-COMMENT ON COLUMN exercise_student_average.updated_at IS 'Timestamp when the record was updated.';
-COMMENT ON COLUMN exercise_student_average.deleted_at IS 'Timestamp when the record was deleted. If null, the record is not deleted.';
-COMMENT ON COLUMN exercise_student_average.average_duration_seconds IS 'The average duration a all student used in completing an exercise.';
-COMMENT ON COLUMN exercise_student_average.average_points IS 'The average points all student received from completing an exercise.';
+COMMENT ON COLUMN suspected_cheaters.total_duration_seconds IS 'The total duration the student spent completing the course.';
+COMMENT ON COLUMN suspected_cheaters.total_points IS 'The total points earned by the student in the course.';
+-- The cheater_thresholds table contains thresholds set by the instructor, representing the maximum score or duration a student can surpass before being suspected of cheating.
 CREATE TABLE cheater_thresholds (
   id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
   course_instance_id UUID NOT NULL REFERENCES course_instances,
@@ -92,7 +30,7 @@ CREATE TABLE cheater_thresholds (
 );
 CREATE TRIGGER set_timestamp BEFORE
 UPDATE ON cheater_thresholds FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
-COMMENT ON TABLE cheater_thresholds IS 'This table stores threshold for measuring cheaters.';
+COMMENT ON TABLE cheater_thresholds IS 'This table stores thresholds set by the instructor, representing the maximum score or duration a student can surpass before being suspected of cheating cheaters.';
 COMMENT ON COLUMN cheater_thresholds.id IS 'A unique, stable identifier for the record.';
 COMMENT ON COLUMN cheater_thresholds.course_instance_id IS 'The course_instance_id of the course.';
 COMMENT ON COLUMN cheater_thresholds.created_at IS 'Timestamp when the record was created.';

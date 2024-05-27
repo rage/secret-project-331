b8cde299e77a49b9a14b6d3ae1f7633fe47ff2d6
george-misan <george.eyoyibo@gmail.com>
2024-05-27 07:10:39 +0300

    stash commit

--- services/headless-lms/migrations/20240527014445_add_course_instance_id_to_suspected_cheaters_and_cheater_thresholds_down.sql
+++ services/headless-lms/migrations/20240527014445_add_course_instance_id_to_suspected_cheaters_and_cheater_thresholds_down.sql
@@ -1,3 +1,3 @@
-ALTER TABLE suspected_cheaters DROP course_instance_id;
-ALTER TABLE cheater_thresholds DROP course_instance_id;
-DROP INDEX nique_course_instance_id;
+ALTER TABLE suspected_cheaters DROP COLUMN IF EXISTS course_instance_id;
+ALTER TABLE cheater_thresholds DROP COLUMN IF EXISTS course_instance_id;
+DROP INDEX IF EXISTS unique_course_instance_id;

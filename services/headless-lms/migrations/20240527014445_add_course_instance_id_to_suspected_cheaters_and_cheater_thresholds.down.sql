fc2293a04e1126e4da8385bb35d71b7acbfe0515
george-misan <george.eyoyibo@gmail.com>
2024-06-17 13:58:41 +0300

    update migration file

--- services/headless-lms/migrations/20240527014445_add_course_instance_id_to_suspected_cheaters_and_cheater_thresholds.down.sql
+++ services/headless-lms/migrations/20240527014445_add_course_instance_id_to_suspected_cheaters_and_cheater_thresholds.down.sql
@@ -1,4 +1,7 @@
-ALTER TABLE suspected_cheaters DROP COLUMN IF EXISTS course_instance_id;
-DROP INDEX IF EXISTS unique_course_instance_id;
+ALTER TABLE suspected_cheaters DROP COLUMN IF EXISTS course_id;
+DROP INDEX IF EXISTS unique_course_id;
 ----
-ALTER TABLE course_module_completions DROP IF EXISTS need_to_be_reviewed;
+ALTER TABLE course_module_completions DROP IF EXISTS needs_to_be_reviewed;
+ALTER TABLE cheater_thresholds
+ADD course_instance_id UUID NOT NULL REFERENCES course_instances,
+  DROP course_id;
--- services/headless-lms/migrations/20240527014445_add_course_instance_id_to_suspected_cheaters_and_cheater_thresholds.up.sql
+++ services/headless-lms/migrations/20240527014445_add_course_instance_id_to_suspected_cheaters_and_cheater_thresholds.up.sql
@@ -1,18 +1,17 @@
--- This migration adds the course_instance_id column to the suspected_cheaters and cheater_thresholds tables
--- and ensures that it references the id column in the course_instances table.
--- Add course_instance_id to suspected_cheaters table and set it to reference course_instances(id)
+-- This migration adds the course_id column to the suspected_cheaters and cheater_thresholds tables
+-- and ensures that it references the id column in the courses table.
+-- Add course_id to suspected_cheaters table and set it to reference courses(id)
 ALTER TABLE suspected_cheaters
-ADD course_instance_id UUID NOT NULL REFERENCES course_instances;
--- Create a unique index on the course_instance_id column in cheater_thresholds table
-CREATE UNIQUE INDEX unique_course_instance_id ON cheater_thresholds (course_instance_id);
+ADD course_id UUID NOT NULL REFERENCES courses;
+--Replace course-instance-id to course-id in cheater_thresholds
+ALTER TABLE cheater_thresholds DROP course_instance_id,
+  ADD course_id UUID NOT NULL REFERENCES courses;
+-- Create a unique index on the course_id column in cheater_thresholds table
+CREATE UNIQUE INDEX unique_course_id ON cheater_thresholds (course_id);
 -- Add constraint
 ALTER TABLE cheater_thresholds
-ADD CONSTRAINT unique_instance_id UNIQUE USING INDEX unique_course_instance_id;
-----
-ALTER TABLE course_module_completions
-ADD COLUMN needs_to_be_reviewed BOOLEAN NOT NULL DEFAULT FALSE;
+ADD CONSTRAINT unique_course_id_constraint UNIQUE USING INDEX unique_course_id;
+-- Add needs_to_be_reviewed column to course_module_completions table with default NULL
+ALTER TABLE course_module_completions -- this is not breaking the db because of the default
+ADD COLUMN needs_to_be_reviewed BOOLEAN DEFAULT NULL;
 COMMENT ON COLUMN course_module_completions.needs_to_be_reviewed IS 'Determine if a course module needs review as a result of a student being suspected of cheating';
--- Set value to all existing tables
-UPDATE course_module_completions
-SET needs_to_be_reviewed = false
-WHERE needs_to_be_reviewed IS NULL;

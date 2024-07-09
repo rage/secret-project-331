a256a3c5c884ce971392f7d885cbc86e4d78c0af
Henrik Nygren <nygrenh@gmail.com>
2024-07-09 16:45:23 +0300

    Fixes 349 (#1292)
    * Update svg libraries
    
    * Don't show double icon for links that open in new tab
    
    * Fix code blocks inside exercise blocks
    
    * Fix custom html block preview
    
    * Fix html in code block
    
    * Fix html in code block by not rendering ligatures
    
    * Update snapshots

--- services/course-material/src/components/ContentRenderer/core/formatting/CodeBlock/index.tsx
+++ services/course-material/src/components/ContentRenderer/core/formatting/CodeBlock/index.tsx
@@ -1,9 +1,9 @@
 import { css } from "@emotion/css"
 import dynamic from "next/dynamic"
+import { useMemo } from "react"
 
 import { BlockRendererProps } from "../../.."
 import { CodeAttributes } from "../../../../../../types/GutenbergBlockAttributes"
-import { fontSizeMapper } from "../../../../../styles/fontSizeMapper"
 
 import BreakFromCentered from "@/shared-module/common/components/Centering/BreakFromCentered"
 import Spinner from "@/shared-module/common/components/Spinner"
@@ -19,15 +19,36 @@ const SyntaxHighlightedContainer = dynamic(() => import("./SyntaxHighlightedCont
 
 const CodeBlock: React.FC<React.PropsWithChildren<BlockRendererProps<CodeAttributes>>> = ({
   data,
+  dontAllowBlockToBeWiderThanContainerWidth,
 }) => {
-  const { anchor, content, fontSize } = data.attributes
+  const { anchor, content } = data.attributes
+  const fontSizePx = useMemo(() => {
+    const longestLine = (content ?? "")
+      .split("\n")
+      .reduce((acc, line) => (line.length > acc ? line.length : acc), 0)
+
+    let fontSizePx = 20
+    if (longestLine > 70) {
+      fontSizePx = 16
+    }
+    if (longestLine > 100) {
+      fontSizePx = 14
+    }
+    return fontSizePx
+  }, [content])
+
   return (
-    <BreakFromCentered sidebar={false}>
+    <BreakFromCentered sidebar={false} disabled={dontAllowBlockToBeWiderThanContainerWidth}>
       <pre
         className={css`
           max-width: 1000px;
           margin: 0 auto;
-          ${fontSize && `font-size: ${fontSizeMapper(fontSize)};`}
+          ${dontAllowBlockToBeWiderThanContainerWidth &&
+          // If this is inside a container, large margins don't look good.
+          `margin-top: -1.5rem;
+            margin-bottom: -1.5rem;
+            `}
+          font-size: ${fontSizePx}px;
           font-family: ${monospaceFont} !important;
           line-height: 1.75rem;
           white-space: pre-wrap;

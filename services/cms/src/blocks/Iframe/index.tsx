9cc87c4fb07390ea83b144ce9f484ec2751385da
Akira Taguchi <akirahattivatti@gmail.com>
2024-07-30 13:09:25 +0300

    Fix iFrame typo (#1297)

--- services/cms/src/blocks/Iframe/index.tsx
+++ services/cms/src/blocks/Iframe/index.tsx
@@ -14,7 +14,7 @@ export const IFRAME_BLOCK_DEFAULT_HEIGHT_PX = 630
 
 const IframeBoxConfiguration: BlockConfiguration<IframeAttributes> = {
   title: "Iframe",
-  description: "Can be used to embed some sites that the embded block doesn't support.",
+  description: "Can be used to embed some sites that the Embed block doesn't support.",
   category: "text",
   attributes: {
     url: {

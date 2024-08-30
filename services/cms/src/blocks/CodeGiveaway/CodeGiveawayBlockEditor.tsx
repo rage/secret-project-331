838125f6a956d3b32ad964c943cd439fea4e6ed2
Henrik Nygren <nygrenh@gmail.com>
2024-08-30 14:45:43 +0300

    Block in CMS

--- services/cms/src/blocks/CodeGiveaway/CodeGiveawayBlockEditor.tsx
+++ services/cms/src/blocks/CodeGiveaway/CodeGiveawayBlockEditor.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled"
 import { useQuery } from "@tanstack/react-query"
 import { InnerBlocks, InspectorControls } from "@wordpress/block-editor"
 import { BlockEditProps } from "@wordpress/blocks"
-import React, { useContext } from "react"
+import React, { useContext, useMemo } from "react"
 import { useTranslation } from "react-i18next"
 
 import PageContext from "../../contexts/PageContext"
@@ -15,6 +15,8 @@ import { ConditionAttributes } from "."
 import InnerBlocksWrapper from "@/components/blocks/InnerBlocksWrapper"
 import CourseContext from "@/contexts/CourseContext"
 import { fetchCodeGiveawaysByCourseId } from "@/services/backend/code-giveaways"
+import DropdownMenu from "@/shared-module/common/components/DropdownMenu"
+import SelectField from "@/shared-module/common/components/InputFields/SelectField"
 import { assertNotNullOrUndefined } from "@/shared-module/common/utils/nullability"
 
 const ALLOWED_NESTED_BLOCKS = [
@@ -31,9 +33,6 @@ const Wrapper = styled.div`
   margin-right: 1rem;
   height: auto;
 `
-const Text = styled.p`
-  padding-bottom: 1rem;
-`
 
 const CodeGiveawayBlockEditor: React.FC<
   React.PropsWithChildren<BlockEditProps<ConditionAttributes>>
@@ -47,17 +46,44 @@ const CodeGiveawayBlockEditor: React.FC<
     enabled: !!courseId,
   })
 
+  const title = useMemo(() => {
+    let title = t("code-giveaway")
+    if (codeGivawayQuery.data) {
+      const selected = codeGivawayQuery.data.find((o) => o.id === attributes.code_giveaway_id)
+      if (selected) {
+        title += ` (${selected.name})`
+      }
+    }
+    return title
+  }, [attributes.code_giveaway_id, codeGivawayQuery.data, t])
+
+  const dropdownOptions = useMemo(() => {
+    const res = [{ label: t("select-an-option"), value: "" }]
+    if (!codeGivawayQuery.data) {
+      return res
+    }
+    const additional = codeGivawayQuery.data.map((o) => ({
+      label: o.name,
+      value: o.id,
+    }))
+    return res.concat(additional)
+  }, [codeGivawayQuery.data, t])
+
   return (
     <BlockPlaceholderWrapper
       id={clientId}
-      title={t("code-giveaway")}
+      title={title}
       explanation={t("code-giveaway-explanation")}
     >
       <InspectorControls>
         {codeGivawayQuery.data && (
           <Wrapper>
-            <Text>{t("module-completion-condition")}</Text>
-            TODO
+            <SelectField
+              label={t("code-giveaway")}
+              options={dropdownOptions}
+              defaultValue={attributes.code_giveaway_id}
+              onChangeByValue={(value) => setAttributes({ code_giveaway_id: value })}
+            />
           </Wrapper>
         )}
       </InspectorControls>

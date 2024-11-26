88aef6eeec8cc8183db6b04128b2a95994888836
george-misan <george.eyoyibo@gmail.com>
2024-11-26 21:06:29 +0200

    + comment to migration file

--- services/headless-lms/migrations/20241112104854_create_partners_blocks.up.sql
+++ services/headless-lms/migrations/20241112104854_create_partners_blocks.up.sql
@@ -12,3 +12,9 @@ CREATE TABLE partners_blocks (
 CREATE TRIGGER set_timestamp BEFORE
 UPDATE ON partners_blocks FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
 COMMENT ON TABLE partners_blocks IS 'A partners block is a custom content block displayed across all pages of a course, positioned directly above the site footer. This block showcases partner logos and links, providing easy access to relevant partner sites. The partners_blocks table stores the content data for this block. Content is created and managed through the Gutenberg Editor.';
+COMMENT ON COLUMN partners_blocks.id IS 'A unique, stable identifier for the record.';
+COMMENT ON COLUMN partners_blocks.created_at IS 'Timestamp of when the record was created';
+COMMENT ON COLUMN partners_blocks.updated_at IS 'Timestamp when the record was last updated. The field is updated automatically by the set_timestamp trigger.';
+COMMENT ON COLUMN partners_blocks.deleted_at IS 'Timestamp when the record was deleted. If null, the record is not deleted.';
+COMMENT ON COLUMN partners_blocks.content IS 'The content of the partners block that is derived from the Gutenberg Editor';
+COMMENT ON COLUMN partners_blocks.course_id IS 'The course_id of the course the partners_block relates to.';

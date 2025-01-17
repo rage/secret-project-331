dd34ef3481294d33a450259bd3c47fa7f01e3a97
Henrik Nygren <nygrenh@gmail.com>
2025-01-17 15:59:15 +0200

    WIP cleaning up dialogs

---
+++ services/main-frontend/src/components/page-specific/org/organizationSlug/NewCourseDialog.tsx
@@ -0,0 +1,41 @@
+import React from "react"
+import { useTranslation } from "react-i18next"
+
+import NewCourseForm from "../../../forms/NewCourseForm"
+
+import { Course, NewCourse } from "@/shared-module/common/bindings"
+import StandardDialog from "@/shared-module/common/components/StandardDialog"
+
+interface NewCourseDialogProps {
+  open: boolean
+  onClose: () => void
+  organizationId: string
+  courses: Course[]
+  onSubmitNewCourse: (newCourse: NewCourse) => Promise<void>
+  onSubmitDuplicateCourse: (oldCourseId: string, newCourse: NewCourse) => Promise<void>
+}
+
+const NewCourseDialog: React.FC<NewCourseDialogProps> = ({
+  open,
+  onClose,
+  organizationId,
+  courses,
+  onSubmitNewCourse,
+  onSubmitDuplicateCourse,
+}) => {
+  const { t } = useTranslation()
+
+  return (
+    <StandardDialog open={open} onClose={onClose} title={t("new-course")}>
+      <NewCourseForm
+        organizationId={organizationId}
+        courses={courses}
+        onSubmitNewCourseForm={onSubmitNewCourse}
+        onSubmitDuplicateCourseForm={onSubmitDuplicateCourse}
+        onClose={onClose}
+      />
+    </StandardDialog>
+  )
+}
+
+export default NewCourseDialog

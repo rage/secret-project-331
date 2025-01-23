1cf5b65bd1289b587acc23cac3c1382831987f63
Henrik Nygren <nygrenh@gmail.com>
2025-01-23 11:59:54 +0200

    Make peer review updater to handle answers that teacher has not reviewed in 3 months
    - Introduced a new SQL query in `peer_review_queue_entries.rs` to fetch entries that have been waiting for teacher review longer than a specified timestamp.
    - Added a new JSON file defining the query structure and its parameters.
    - Updated the peer review updater logic to utilize the new query for processing entries that require manual grading.

--- services/headless-lms/server/src/programs/peer_review_updater.rs
+++ services/headless-lms/server/src/programs/peer_review_updater.rs
@@ -67,9 +67,9 @@ async fn process_course_instance(
     let pass_automatically_cutoff = earliest_manual_review_cutoff - chrono::Duration::days(90);
 
     // Process automatic pass cases
-    let should_pass = headless_lms_models::peer_review_queue_entries::get_entries_that_need_reviews_and_are_older_than(conn, course_instance.id, pass_automatically_cutoff).await?;
+    let should_pass = headless_lms_models::peer_review_queue_entries::get_entries_that_need_teacher_review_and_are_older_than_with_course_instance_id(conn, course_instance.id, pass_automatically_cutoff).await?;
     if !should_pass.is_empty() {
-        info!(course_instance_id = ?course_instance.id, "Found {:?} answers that have been added to the peer review queue before {:?} and have not received enough peer reviews or have not been reviewed manually. Giving them full points.", should_pass.len(), pass_automatically_cutoff);
+        info!(course_instance_id = ?course_instance.id, "Found {:?} answers that have been added to the peer review queue before {:?}. The teacher has not reviewed the answers manually after 3 months. Giving them full points.", should_pass.len(), pass_automatically_cutoff);
         for peer_review_queue_entry in should_pass {
             let _res = peer_review_queue_entries::remove_from_queue_and_give_full_points(
                 conn,

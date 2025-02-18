a3e5bc661f291811b5832b8625573dc2194f5cc4
Antti Leinonen <Redande@users.noreply.github.com>
2025-02-18 10:39:50 +0200

    Update mooc.fi logo in nav bar and footer (#1353)
    * Update mooc.fi logo in nav bar and footer
    
    * Update moocfiLogo to droplet
    
    * Update system test snapshots
    
    * Increase droplet size
    
    * Update system-tests snapshots
    
    ---------
    
    Co-authored-by: Leinonen Antti K M <antleino@vdi-antleino.ad.helsinki.fi>

--- shared-module/packages/common/src/components/Navigation/NavBar/SimpleNav.tsx
+++ shared-module/packages/common/src/components/Navigation/NavBar/SimpleNav.tsx
@@ -1,7 +1,7 @@
 import { css, cx } from "@emotion/css"
 import { useTranslation } from "react-i18next"
 
-import MOOCfi from "../../../img/moocfiLogoNoText.svg"
+import MOOCfi from "../../../img/moocfiLogo.svg"
 import { baseTheme } from "../../../styles"
 import { respondToOrLarger } from "../../../styles/respond"
 import { MARGIN_BETWEEN_NAVBAR_AND_CONTENT } from "../../../utils/constants"
@@ -11,10 +11,9 @@ import { NavigationProps } from "."
 
 const StyledIcon = css`
   font-size: 1.8rem;
-  transform: scale(0.7);
 
   path {
-    fill: ${baseTheme.colors.gray[600]} !important;
+    color: ${baseTheme.colors.gray[600]} !important;
   }
 `
 const Navbar = css`

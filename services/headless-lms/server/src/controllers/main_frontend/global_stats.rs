fef4489174f79870efe14835f59ff3b6c074fac9
Henrik Nygren <nygrenh@gmail.com>
2025-03-07 15:35:06 +0200

    Add endpoint for counting global completion stats for single email domain

--- services/headless-lms/server/src/controllers/main_frontend/global_stats.rs
+++ services/headless-lms/server/src/controllers/main_frontend/global_stats.rs
@@ -1,7 +1,7 @@
 //! Controllers for requests starting with `/api/v0/main-frontend/global-stats`.
 
 use models::library::global_stats::{
-    DomainCompletionStats, GlobalCourseModuleStatEntry, GlobalStatEntry,
+    CourseCompletionStats, DomainCompletionStats, GlobalCourseModuleStatEntry, GlobalStatEntry,
 };
 
 use crate::{domain::authorization::authorize, prelude::*};
@@ -144,6 +144,45 @@ async fn get_completion_stats_by_email_domain(
 }
 
 /**
+ * GET `/api/v0/main-frontend/global-stats/course-completion-stats-for-email-domain`
+ *
+ * Query parameters:
+ * - email_domain: String - The email domain to get stats for (required)
+ * - year: Optional<i32> - Filter results to specific year (e.g. ?year=2023)
+ */
+#[instrument(skip(pool))]
+async fn get_course_completion_stats_for_email_domain(
+    pool: web::Data<PgPool>,
+    user: AuthUser,
+    query: web::Query<HashMap<String, String>>,
+) -> ControllerResult<web::Json<Vec<CourseCompletionStats>>> {
+    let mut conn = pool.acquire().await?;
+    let token = authorize(
+        &mut conn,
+        Act::ViewStats,
+        Some(user.id),
+        Res::GlobalPermissions,
+    )
+    .await?;
+
+    let email_domain = query
+        .get("email_domain")
+        .ok_or_else(|| Error::BadRequest("email_domain is required".into()))?
+        .to_string();
+
+    let year = query.get("year").and_then(|y| y.parse::<i32>().ok());
+
+    let res = models::library::global_stats::get_course_completion_stats_for_email_domain(
+        &mut conn,
+        email_domain,
+        year,
+    )
+    .await?;
+
+    token.authorized_ok(web::Json(res))
+}
+
+/**
 Add a route for each controller in this module.
 
 The name starts with an underline in order to appear before other functions in the module documentation.
@@ -174,5 +213,9 @@ pub fn _add_routes(cfg: &mut ServiceConfig) {
     .route(
         "/completion-stats-by-email-domain",
         web::get().to(get_completion_stats_by_email_domain),
+    )
+    .route(
+        "/course-completion-stats-for-email-domain",
+        web::get().to(get_course_completion_stats_for_email_domain),
     );
 }

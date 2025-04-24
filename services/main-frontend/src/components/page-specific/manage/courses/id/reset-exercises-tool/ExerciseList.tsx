a004f37a7a76f9b2f8ff20279368bde40a656957
Maija Y <maija.ylitalo@helsinki.fi>
2025-04-24 20:44:08 +0300

    Sorting chapters and exercises

--- services/main-frontend/src/components/page-specific/manage/courses/id/reset-exercises-tool/ExerciseList.tsx
+++ services/main-frontend/src/components/page-specific/manage/courses/id/reset-exercises-tool/ExerciseList.tsx
@@ -161,106 +161,122 @@ const ExerciseList: React.FC<Props> = ({
         </Button>
       </div>
       <div>
-        {Object.entries(groupedExercises).map(([chapterId, pagesExercises]) => {
-          const chapter = chapterId !== t("label-no-chapter") ? chapterMap.get(chapterId) : null
-          const chapterTitle = chapter
-            ? `${t("chapter")} ${chapter.chapter_number}: ${chapter.name}`
-            : t("label-no-chapter")
-          return (
-            <div key={chapterId}>
-              <p
-                className={css`
-                  font-family: ${secondaryFont};
-                  color: ${baseTheme.colors.gray[700]};
-                  font-weight: ${fontWeights.medium};
-                  font-size: ${baseTheme.fontSizes[0]}px;
-                  padding-bottom: 10px;
-                  padding-top: 20px;
-                `}
-              >
-                {chapterTitle}
-              </p>
-              <div
-                className={css`
-                  border: 1px solid #ced1d7;
-                  border-radius: 8px;
-                  border-width: 1px;
-                  color: ${baseTheme.colors.gray[700]};
-                `}
-              >
-                <table
-                  className={css`
-                    border-collapse: collapse;
-                    width: 100%;
-                    text-align: left;
+        {Object.entries(groupedExercises)
+          .sort(([aId], [bId]) => {
+            const chapterA = aId !== t("label-no-chapter") ? chapterMap.get(aId) : null
+            const chapterB = bId !== t("label-no-chapter") ? chapterMap.get(bId) : null
 
-                    th {
-                      font-weight: ${fontWeights.medium};
-                      background: #f7f8f9;
-                      border-radius: 8px;
-                    }
-                    td {
-                      border-top: 1px solid #ced1d7;
-                    }
-                    td,
-                    th {
-                      font-size: ${baseTheme.fontSizes[0]}px;
-                      padding: 1rem;
-                      opacity: 0.8;
-                    }
+            if (!chapterA) {
+              return 1
+            }
+            if (!chapterB) {
+              return -1
+            }
+            return chapterA.chapter_number - chapterB.chapter_number
+          })
+          .map(([chapterId, pagesExercises]) => {
+            const chapter = chapterId !== t("label-no-chapter") ? chapterMap.get(chapterId) : null
+            const chapterTitle = chapter
+              ? `${t("chapter")} ${chapter.chapter_number}: ${chapter.name}`
+              : t("label-no-chapter")
+            return (
+              <div key={chapterId}>
+                <p
+                  className={css`
+                    font-family: ${secondaryFont};
+                    color: ${baseTheme.colors.gray[700]};
+                    font-weight: ${fontWeights.medium};
+                    font-size: ${baseTheme.fontSizes[0]}px;
+                    padding-bottom: 10px;
+                    padding-top: 20px;
                   `}
                 >
-                  <thead>
-                    <tr>
-                      <th>{t("label-select")}</th>
-                      <th>{t("exercise")}</th>
-                      <th>{t("title-page")}</th>
-                      <th>{t("title-peer-review")}</th>
-                      <th>{t("title-self-review")}</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {Object.entries(pagesExercises)
-                      .sort(([pageAId], [pageBId]) => {
-                        const pageA = pageMap.get(pageAId)
-                        const pageB = pageMap.get(pageBId)
-                        const orderA = pageA ? pageA.order_number : Number.MAX_VALUE
-                        const orderB = pageB ? pageB.order_number : Number.MAX_VALUE
-                        return orderA - orderB
-                      })
-                      .map(([pageId, exercises]) => {
-                        return exercises.map((exercise) => (
-                          <tr key={exercise.id}>
-                            <td>
-                              <CheckBox
-                                checked={selectedExerciseIds.includes(exercise.id)}
-                                onChange={() => toggleExercise(exercise.id)}
-                                label={""}
-                                className={css`
-                                  margin: 0px;
-                                  padding-left: 10px;
-                                `}
-                              />
-                            </td>
-                            <td>{exercise.name}</td>
-                            <td>
-                              {t("title-page")} {pageMap.get(pageId)?.order_number}
-                            </td>
-                            <td>
-                              {exercise.needs_peer_review ? t("label-true") : t("label-false")}
-                            </td>
-                            <td>
-                              {exercise.needs_self_review ? t("label-true") : t("label-false")}
-                            </td>
-                          </tr>
-                        ))
-                      })}
-                  </tbody>
-                </table>
+                  {chapterTitle}
+                </p>
+                <div
+                  className={css`
+                    border: 1px solid #ced1d7;
+                    border-radius: 8px;
+                    border-width: 1px;
+                    color: ${baseTheme.colors.gray[700]};
+                  `}
+                >
+                  <table
+                    className={css`
+                      border-collapse: collapse;
+                      width: 100%;
+                      text-align: left;
+
+                      th {
+                        font-weight: ${fontWeights.medium};
+                        background: #f7f8f9;
+                        border-radius: 8px;
+                      }
+                      td {
+                        border-top: 1px solid #ced1d7;
+                      }
+                      td,
+                      th {
+                        font-size: ${baseTheme.fontSizes[0]}px;
+                        padding: 1rem;
+                        opacity: 0.8;
+                      }
+                    `}
+                  >
+                    <thead>
+                      <tr>
+                        <th>{t("label-select")}</th>
+                        <th>{t("exercise")}</th>
+                        <th>{t("title-page")}</th>
+                        <th>{t("title-peer-review")}</th>
+                        <th>{t("title-self-review")}</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {Object.entries(pagesExercises)
+                        .sort(([pageAId], [pageBId]) => {
+                          const pageA = pageMap.get(pageAId)
+                          const pageB = pageMap.get(pageBId)
+                          const orderA = pageA ? pageA.order_number : Number.MAX_VALUE
+                          const orderB = pageB ? pageB.order_number : Number.MAX_VALUE
+                          return orderA - orderB
+                        })
+                        .map(([pageId, exercises]) => {
+                          return exercises
+                            .slice()
+                            .sort((a, b) => a.order_number - b.order_number)
+                            .map((exercise) => (
+                              <tr key={exercise.id}>
+                                <td>
+                                  <CheckBox
+                                    checked={selectedExerciseIds.includes(exercise.id)}
+                                    onChange={() => toggleExercise(exercise.id)}
+                                    label={""}
+                                    className={css`
+                                      margin: 0px;
+                                      padding-left: 10px;
+                                    `}
+                                  />
+                                </td>
+                                <td>{exercise.name}</td>
+                                <td>
+                                  {t("title-page")} {pageMap.get(pageId)?.order_number}
+                                </td>
+                                <td>
+                                  {exercise.needs_peer_review ? t("label-true") : t("label-false")}
+                                </td>
+                                <td>
+                                  {exercise.needs_self_review ? t("label-true") : t("label-false")}
+                                </td>
+                              </tr>
+                            ))
+                        })}
+                    </tbody>
+                  </table>
+                </div>
               </div>
-            </div>
-          )
-        })}
+            )
+          })}
       </div>
     </div>
   )

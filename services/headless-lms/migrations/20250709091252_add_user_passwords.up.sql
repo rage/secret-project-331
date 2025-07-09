5db8f451ef89d4f05154e3d9b404eeecde1e5c41
Maija Y <maija.ylitalo@helsinki.fi>
2025-07-09 17:48:32 +0300

    Add user passwords migration

---
+++ services/headless-lms/migrations/20250709091252_add_user_passwords.up.sql
@@ -0,0 +1,16 @@
+CREATE TABLE user_passwords (
+  user_id UUID PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
+  password_hash TEXT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  deleted_at TIMESTAMP WITH TIME ZONE
+);
+CREATE TRIGGER set_timestamp BEFORE
+UPDATE ON user_passwords FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
+
+COMMENT ON TABLE user_passwords IS 'This table is used to keep a record of the users password';
+COMMENT ON COLUMN user_passwords.user_id IS '';
+COMMENT ON COLUMN user_passwords.password_hash IS '';
+COMMENT ON COLUMN user_passwords.created_at IS 'Timestamp of when the record was created.';
+COMMENT ON COLUMN user_passwords.updated_at IS 'Timestamp when the record was last updated. The field is updated automatically by the set_timestamp trigger.';
+COMMENT ON COLUMN user_passwords.deleted_at IS 'Timestamp when the record was deleted. If null, the record is not deleted.';

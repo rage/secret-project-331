1f08e0f1b2c1038c5d061b86b7859204e185536a
janitus <janitus@Ubuntu-22.04.4.myguest.virtualbox.org>
2025-08-06 22:49:49 +0300

    Fixed organization utils after broken tests and updated new snapshots

--- system-tests/src/utils/organizationUtils.ts
+++ system-tests/src/utils/organizationUtils.ts
@@ -4,16 +4,9 @@ import { Page } from "@playwright/test"
  * Clicks the "Select" button for the organization with the given heading.
  */
 export async function selectOrganization(page: Page, orgName: string) {
-  await page
-    .getByLabel(orgName) // This selects the outermost <div aria-label="orgName">
-    .getByRole("link", { name: "Select" }) // <a> with text "Select"
-    .click()
+  await page.getByRole("button", { name: `Visit organization ${orgName}` }).click()
 }
 
 export async function manageOrganization(page: Page, orgName: string) {
-  await page
-    .getByLabel(orgName)
-    .getByRole("link")
-    .filter({ hasText: ".gear_svg__cls-1{fill:none;" })
-    .click()
+  await page.getByRole("button", { name: `Manage organization ${orgName}` }).click()
 }

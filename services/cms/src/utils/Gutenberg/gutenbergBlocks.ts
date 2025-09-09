f6f16b2f8df913dfa3f01632d7235c4edde25dc2
Henrik Nygren <nygrenh@gmail.com>
2025-09-09 10:30:20 +0300

    Refactor isGutenbergBlockArray function to utilize isGutenbergBlock guard for improved type checking of GutenbergBlock objects.

--- services/cms/src/utils/Gutenberg/gutenbergBlocks.ts
+++ services/cms/src/utils/Gutenberg/gutenbergBlocks.ts
@@ -1,11 +1,12 @@
 import { GutenbergBlock } from "@/shared-module/common/bindings"
+import { isGutenbergBlock } from "@/shared-module/common/bindings.guard"
 
 export function isGutenbergBlockArray(obj: unknown): obj is GutenbergBlock[] {
   if (!Array.isArray(obj)) {
     return false
   }
   for (const o of obj) {
-    if (typeof o.name !== "string" || typeof o.clientId !== "string") {
+    if (!isGutenbergBlock(o)) {
       return false
     }
   }

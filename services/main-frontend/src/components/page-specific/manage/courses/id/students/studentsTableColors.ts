8cddbee149866aa84e6a8f85b8f825bfffca9980
janitus <janitus@Ubuntu-22.04.4.myguest.virtualbox.org>
2025-09-23 00:30:54 +0300

    Utilizing breakfromcentered component, modified colors for testing and trying to make the table work properly, some issues still persist

--- services/main-frontend/src/components/page-specific/manage/courses/id/students/studentsTableColors.ts
+++ services/main-frontend/src/components/page-specific/manage/courses/id/students/studentsTableColors.ts
@@ -2,10 +2,18 @@
 
 export const colorPairs = [
   // blue
+  ["#DAE3EB", "#DAE3EB"], // [lighter, darker]
+  // red
+  ["#F0E1DD", "#F0E1DD"], // [lighter, darker]
+  // green
+  ["#DAE6E5", "#DAE6E5"], // [lighter, darker]
+  // ...add more color pairs here!
+  /*
+  // blue
   ["#DAE3EB", "#B5C7D7"], // [lighter, darker]
   // red
   ["#F0E1DD", "#E2C2BC"], // [lighter, darker]
   // green
   ["#DAE6E5", "#B4CDCB"], // [lighter, darker]
-  // ...add more color pairs here!
+  */
 ]

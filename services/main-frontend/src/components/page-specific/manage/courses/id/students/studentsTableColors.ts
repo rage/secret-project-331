495c622846df2a94516012a75297b73e86438196
janitus <janitus@Ubuntu-22.04.4.myguest.virtualbox.org>
2025-09-22 07:32:00 +0300

    first iteration for table colors

---
+++ services/main-frontend/src/components/page-specific/manage/courses/id/students/studentsTableColors.ts
@@ -0,0 +1,11 @@
+// studentsTableColors.ts
+
+export const colorPairs = [
+  // blue
+  ["#DAE3EB", "#B5C7D7"], // [lighter, darker]
+  // red
+  ["#F0E1DD", "#E2C2BC"], // [lighter, darker]
+  // green
+  ["#DAE6E5", "#B4CDCB"], // [lighter, darker]
+  // ...add more color pairs here!
+]

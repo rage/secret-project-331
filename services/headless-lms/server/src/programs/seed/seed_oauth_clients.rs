80b17cca8e5d4f1669012b536127dd018a9f7ac9
Daniel Koch <daniel.koch@helsinki.fi>
2025-11-17 13:45:35 +0200

    Fix borrowing on seed_oauth_clients

--- services/headless-lms/server/src/programs/seed/seed_oauth_clients.rs
+++ services/headless-lms/server/src/programs/seed/seed_oauth_clients.rs
@@ -18,22 +18,26 @@ pub async fn seed_oauth_clients(db_pool: Pool<Postgres>) -> anyhow::Result<SeedO
         Digest::from_str("b644133604bf99632137be3e9230c4056bd32eb2f404020d70adcde88353c760")
             .unwrap(); // "very-secret"
     let mut conn = db_pool.acquire().await?;
+    let redirect_uris = vec!["http://127.0.0.1:8765/callback".to_string()];
+    let scopes = vec![
+        "openid".to_string(),
+        "profile".to_string(),
+        "email".to_string(),
+    ];
+    let allowed_grant_types = vec![GrantTypeName::AuthorizationCode];
+    let pkce_methods_allowed = vec![pkce::PkceMethod::S256];
     let new_client_parms = oauth_client::NewClientParams {
         client_name: "Test Client",
         application_type: oauth_client::ApplicationType::Web,
         client_id: "test-client-id",
         client_secret: Some(&secret), // "very-secret"
         client_secret_expires_at: None,
-        redirect_uris: &["http://127.0.0.1:8765/callback".to_string()],
-        allowed_grant_types: &[GrantTypeName::AuthorizationCode],
-        scopes: &[
-            "openid".to_string(),
-            "profile".to_string(),
-            "email".to_string(),
-        ],
+        redirect_uris: redirect_uris.as_slice(),
+        allowed_grant_types: &allowed_grant_types,
+        scopes: scopes.as_slice(),
         origin: "http://localhost",
         bearer_allowed: true,
-        pkce_methods_allowed: &[pkce::PkceMethod::S256],
+        pkce_methods_allowed: &pkce_methods_allowed,
         post_logout_redirect_uris: None,
         require_pkce: true,
         token_endpoint_auth_method: oauth_client::TokenEndpointAuthMethod::ClientSecretPost,

83ef36858685da91ed4a521f0e8cdf8477e1f9e9
Daniel Koch <daniel.koch@helsinki.fi>
2025-11-19 12:29:27 +0200

    move digest and grant type one down and remove oauth_shared_types submodule

--- services/headless-lms/models/src/library/oauth/mod.rs
+++ services/headless-lms/models/src/library/oauth/mod.rs
@@ -1,6 +1,8 @@
-pub mod oauth_shared_types;
+pub mod digest;
+pub mod grant_type;
 pub mod pkce;
 pub mod tokens;
 
-pub use oauth_shared_types::{Digest, DigestError, GrantTypeName};
+pub use digest::{Digest, DigestError};
+pub use grant_type::GrantTypeName;
 pub use tokens::{generate_access_token, token_digest_sha256};
--- services/headless-lms/models/src/library/oauth/oauth_shared_types/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod digest;
-pub mod grant_type;
-
-pub use digest::{Digest, DigestError};
-pub use grant_type::GrantTypeName;

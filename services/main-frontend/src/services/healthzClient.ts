5fab5d390301c7a0bef2549ba169e86aaa5a886d
Henrik Nygren <nygrenh@gmail.com>
2026-01-05 13:04:43 +0200

    Add health probes and status endpoints (#1618)
    * Add health probes and status endpoints for various services
    
    These are conservative so that it's unlikely that high load could trigger these
    
    * Update health probe paths
    
    * Update replica counts for main-frontend and cms services in kustomization.yaml
    
    * Disable liveness and readiness probes in development environment to reduce log spamming, while retaining the startup probe. Added a new patch file for this configuration.
    
    * Update health probe handler path from `/api/v0/healthz` to `/api/v0/health` for consistency with service endpoints.
    
    * Update health probe configurations to increase period and decrease failure thresholds
    
    * Fix removing probes in bin/dev

--- services/main-frontend/src/services/healthzClient.ts
+++ services/main-frontend/src/services/healthzClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const healthzClient = axios.create({
-  baseURL: "/api/v0/healthz",
+  baseURL: "/api/v0/health",
   responseType: "json",
   headers: { "Content-Type": "application/json" },
 })

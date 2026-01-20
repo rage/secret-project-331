3dc67830f89eb2825ff076d23e97303c4b968ac2
Heljä Räisänen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2026-01-20 14:15:36 +0200

    fix createChatbotTranscript code

--- services/main-frontend/src/utils/course-material/createChatbotTranscript.ts
+++ services/main-frontend/src/utils/course-material/createChatbotTranscript.ts
@@ -10,16 +10,16 @@ export const createChatbotTranscript = (info: ChatbotConversationInfo) => {
   }
   const citations = info.current_conversation_message_citations ?? []
   const bot = info.chatbot_name
-  const hide_citations = info.hide_citations
-  let msgs_w_citations = new Set()
-  if (!hide_citations) {
-    msgs_w_citations = new Set(
+  const hideCitations = info.hide_citations
+  let msgsWithCitations = new Set()
+  if (!hideCitations) {
+    msgsWithCitations = new Set(
       info.current_conversation_message_citations?.map((c) => c.conversation_message_id),
     )
   }
-  let latest_cit_n = 0
-  let any_citations_used = false
-  let citation_list = "________________________________________________________\n\nReferences\n\n"
+  let latestCitNumber = 0
+  let anyCitationsUsed = false
+  let citationList = "________________________________________________________\n\nReferences\n\n"
 
   let transcript = messages
     ?.map((m) => {
@@ -41,21 +41,21 @@ export const createChatbotTranscript = (info: ChatbotConversationInfo) => {
       let name = m.message_role === "user" ? "You" : bot
       t += `[${name} said:]\n`
 
-      if (hide_citations && m.message_role === "assistant") {
+      if (hideCitations && m.message_role === "assistant") {
         t += msg.replace(REMOVE_CITATIONS_REGEX, "") + "\n\n"
         return t
       }
 
       // if there are citations, process them and modify msg
-      if (m.message_role === "assistant" && msgs_w_citations.has(m.id)) {
-        let current_citations = citations.filter((c) => c.conversation_message_id === m.id)
+      if (m.message_role === "assistant" && msgsWithCitations.has(m.id)) {
+        let currentCitations = citations.filter((c) => c.conversation_message_id === m.id)
         // citationNumberingMap should contain the same numbers as the filteredCitations array
         let { filteredCitations, citationNumberingMap } = renumberFilterCitations(
           msg,
-          current_citations,
+          currentCitations,
           true,
         )
-        any_citations_used = filteredCitations.length > 0 || any_citations_used
+        anyCitationsUsed = filteredCitations.length > 0 || anyCitationsUsed
         filteredCitations
           .sort((a, b) =>
             citationNumberingMap.get(a.citation_number) <
@@ -67,12 +67,12 @@ export const createChatbotTranscript = (info: ChatbotConversationInfo) => {
             // the 1st cit in this message has number 1 in citationNumberingMap
             // add to it the last cit number from the prev message, so that
             // the numbers are unique across the whole transcript.
-            let new_cit_n = citationNumberingMap.get(cit.citation_number) + latest_cit_n
-            msg = msg.replaceAll(`[doc${cit.citation_number}]`, `[doc${new_cit_n}]`) ?? null
-            citation_list += `[doc${new_cit_n}] ${cit.title}, ${cit.document_url}\n`
+            let newCitNumber = citationNumberingMap.get(cit.citation_number) + latestCitNumber
+            msg = msg.replaceAll(`[doc${cit.citation_number}]`, `[doc${newCitNumber}]`)
+            citationList += `[doc${newCitNumber}] ${cit.title}, ${cit.document_url}\n`
           })
         // latest_cit_n should equal the largest cit number in this message
-        latest_cit_n += filteredCitations.length
+        latestCitNumber += filteredCitations.length
       }
       t += msg + "\n\n"
 
@@ -81,7 +81,9 @@ export const createChatbotTranscript = (info: ChatbotConversationInfo) => {
     .join("")
 
   // add the references list to the transcript only on this condition
-  transcript += !hide_citations && any_citations_used ? citation_list : ""
+  if (!hideCitations && anyCitationsUsed) {
+    transcript += citationList
+  }
 
   return transcript.trim()
 }

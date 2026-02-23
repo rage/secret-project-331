8d7d132caf261b07d019b6a081ff18db5f86c904
Heljä Räisänen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2026-02-23 12:48:09 +0200

    fix down migratioN

--- services/headless-lms/migrations/20260219120000_add_locked_reviewing_stage.down.sql
+++ services/headless-lms/migrations/20260219120000_add_locked_reviewing_stage.down.sql
@@ -2,7 +2,8 @@ UPDATE user_exercise_states
 SET reviewing_stage = 'reviewed_and_locked'
 WHERE reviewing_stage = 'locked';
 
-ALTER TYPE reviewing_stage RENAME TO reviewing_stage_old;
+ALTER TYPE reviewing_stage
+RENAME TO reviewing_stage_old;
 
 CREATE TYPE reviewing_stage AS ENUM (
   'not_started',
@@ -14,6 +15,15 @@ CREATE TYPE reviewing_stage AS ENUM (
 );
 
 ALTER TABLE user_exercise_states
-ALTER COLUMN reviewing_stage TYPE reviewing_stage USING reviewing_stage::text::reviewing_stage;
+ALTER COLUMN reviewing_stage DROP DEFAULT,
+  ALTER COLUMN reviewing_stage TYPE reviewing_stage USING reviewing_stage::text::reviewing_stage,
+  ALTER COLUMN reviewing_stage
+SET DEFAULT 'not_started'::reviewing_stage;
+
+ALTER TABLE user_exercise_states_copy
+ALTER COLUMN reviewing_stage DROP DEFAULT,
+  ALTER COLUMN reviewing_stage TYPE reviewing_stage USING reviewing_stage::text::reviewing_stage,
+  ALTER COLUMN reviewing_stage
+SET DEFAULT 'not_started'::reviewing_stage;
 
 DROP TYPE reviewing_stage_old;

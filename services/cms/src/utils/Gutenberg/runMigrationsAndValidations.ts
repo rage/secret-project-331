5fb7bf3be92a439c64043679abeaa23a3cef7c15
Henrik Nygren <nygrenh@gmail.com>
2026-03-24 14:30:04 +0200

    Implement undo/redo (#1687)
    * Implement undo/redo
    
    * Gutenberg refactoring
    
    * Fixes
    
    * Update snapshots

--- services/cms/src/utils/Gutenberg/runMigrationsAndValidations.ts
+++ services/cms/src/utils/Gutenberg/runMigrationsAndValidations.ts
@@ -2,6 +2,9 @@ import { BlockInstance, getBlockType } from "@wordpress/blocks"
 import { produce } from "immer"
 
 const ENABLED_BLOCKS = ["moocfi/aside", "moocfi/infobox"]
+type MutableBlockInstance = {
+  -readonly [Key in keyof BlockInstance]: BlockInstance[Key]
+}
 
 /** This migrates deprecated block attributes to the current format.  */
 export default function runMigrationsAndValidations(
@@ -12,41 +15,53 @@ export default function runMigrationsAndValidations(
 
   // TODO: For now only implemented for our own custom blocks that need it, further testing required before enabling for other blocks.
   // Also, gutenberg will also run migrations on blocks that are not valid, should figure out how to handle that.
-  // Should also migrate innerblocks in the future.
   const newContent = produce(content, (draftContent) => {
-    for (const block of draftContent) {
-      if (ENABLED_BLOCKS.indexOf(block.name) === -1) {
-        continue
-      }
+    const migrateBlockRecursively = (block: BlockInstance): void => {
       let blockMigrated = false
-      const blockDefinition = getBlockType(block.name)
-      if (!blockDefinition || !blockDefinition.deprecated) {
-        continue
-      }
-      for (const blockDeprecation of blockDefinition.deprecated) {
-        if (
-          !blockDeprecation.migrate ||
-          !blockDeprecation.isEligible ||
-          !blockDeprecation.isEligible(block.attributes, block.innerBlocks)
-        ) {
-          continue
-        }
-        console.info(`Migrating deprecated block ${block.name}`)
-        const migrationResult = blockDeprecation.migrate(block.attributes, block.innerBlocks)
-        blockMigrated = true
+      const innerBlocks = block.innerBlocks ?? []
 
-        if (Array.isArray(migrationResult)) {
-          // migrationResult is of form [newAttributes, newInnerBlocks]
-          block.attributes = migrationResult[0]
-          block.innerBlocks = migrationResult[1]
-        } else {
-          // migrationResult is of form newAttributes
-          block.attributes = migrationResult
-        }
-        if (blockMigrated) {
-          numberOfBlocksMigrated = numberOfBlocksMigrated + 1
+      if (ENABLED_BLOCKS.indexOf(block.name) === -1) {
+        innerBlocks.forEach(migrateBlockRecursively)
+        return
+      }
+
+      const blockDefinition = getBlockType(block.name)
+      if (blockDefinition?.deprecated) {
+        for (const blockDeprecation of blockDefinition.deprecated) {
+          if (
+            !blockDeprecation.migrate ||
+            !blockDeprecation.isEligible ||
+            !blockDeprecation.isEligible(block.attributes, innerBlocks)
+          ) {
+            continue
+          }
+
+          console.info(`Migrating deprecated block ${block.name}`)
+          const migrationResult = blockDeprecation.migrate(block.attributes, innerBlocks)
+          const mutableBlock = block as MutableBlockInstance
+          blockMigrated = true
+
+          if (Array.isArray(migrationResult)) {
+            // migrationResult is of form [newAttributes, newInnerBlocks]
+            mutableBlock.attributes = migrationResult[0]
+            mutableBlock.innerBlocks = migrationResult[1]
+          } else {
+            // migrationResult is of form newAttributes
+            mutableBlock.attributes = migrationResult
+          }
         }
       }
+
+      if (blockMigrated) {
+        numberOfBlocksMigrated = numberOfBlocksMigrated + 1
+      }
+
+      const nestedInnerBlocks = block.innerBlocks ?? []
+      nestedInnerBlocks.forEach(migrateBlockRecursively)
+    }
+
+    for (const block of draftContent) {
+      migrateBlockRecursively(block)
     }
   })
   return [newContent, numberOfBlocksMigrated]

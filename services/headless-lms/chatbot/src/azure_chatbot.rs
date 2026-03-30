a0a74b09ca9d9f3385c9e4b5d2145721ee052ded
Heljä Räisänen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2026-03-30 13:00:55 +0300

    actually fix it

--- services/headless-lms/chatbot/src/azure_chatbot.rs
+++ services/headless-lms/chatbot/src/azure_chatbot.rs
@@ -329,9 +329,9 @@ impl LLMRequest {
                         auth_type: "api_key".to_string(),
                         key: search_config.search_api_key.clone(),
                     },
+                    semantic_configuration: format!("{}-semantic-configuration", &index_name),
                     index_name,
                     query_type: query_type.to_string(),
-                    semantic_configuration: "azureml-default".to_string(),
                     embedding_dependency: EmbeddingDependency {
                         dep_type: "deployment_name".to_string(),
                         deployment_name: search_config.vectorizer_deployment_id.clone(),

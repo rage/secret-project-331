9b3c4bdd67faabfe7d9f2d5dbc742b9b23ac9b67
Henrik Nygren <nygrenh@gmail.com>
2026-04-01 13:24:11 +0300

    HighlightBox editor improvements

--- services/cms/src/blocks/HighlightBox/HighlightEditor.tsx
+++ services/cms/src/blocks/HighlightBox/HighlightEditor.tsx
@@ -1,22 +1,23 @@
 "use client"
 
-/* eslint-disable i18next/no-literal-string */
 import { css } from "@emotion/css"
 import { RichText } from "@wordpress/block-editor"
 import { BlockEditProps } from "@wordpress/blocks"
 import React from "react"
+import { useTranslation } from "react-i18next"
 
 import BlockWrapper from "../BlockWrapper"
 
 import { HighlightAttributes } from "."
 
-import { baseTheme, monospaceFont } from "@/shared-module/common/styles"
+import { baseTheme, fontWeights, monospaceFont, primaryFont } from "@/shared-module/common/styles"
 
 const HighlightEditor: React.FC<React.PropsWithChildren<BlockEditProps<HighlightAttributes>>> = ({
   clientId,
   attributes,
   setAttributes,
 }) => {
+  const { t } = useTranslation()
   const { title, content } = attributes
 
   return (
@@ -25,27 +26,73 @@ const HighlightEditor: React.FC<React.PropsWithChildren<BlockEditProps<Highlight
         className={css`
           background: #fafbfb;
           border: 1px solid #e2e4e6;
-          padding: 1rem;
+          padding: 1rem 0;
         `}
       >
-        <RichText
+        <div
           className={css`
-            color: ${baseTheme.colors.green[700]};
-            font-weight: 700;
-            font-family: ${monospaceFont};
+            max-width: 48rem;
+            margin-left: auto;
+            margin-right: auto;
+            padding: 0 1.375rem;
           `}
-          tagName="span"
-          value={title}
-          onChange={(value: string) => setAttributes({ title: value })}
-          placeholder={"Heading: "}
-        />
-        <RichText
-          className="has-text-align-center wp-block-heading"
-          tagName="span"
-          value={content}
-          onChange={(value: string) => setAttributes({ content: value })}
-          placeholder={"Write a text here"}
-        />
+        >
+          <p
+            className={css`
+              margin: 0 0 0.5rem 0;
+              font-family: ${primaryFont};
+              font-size: 0.8125rem;
+              color: ${baseTheme.colors.gray[700]};
+              font-weight: ${fontWeights.medium};
+            `}
+          >
+            {t("highlight-box-title-label")}
+          </p>
+          <RichText
+            className={css`
+              display: block;
+              width: 100%;
+              border: 1px solid ${baseTheme.colors.gray[500]};
+              border-radius: 4px;
+              padding: 0.5rem 0.75rem;
+              background: #ffffff;
+              color: ${baseTheme.colors.green[700]};
+              font-weight: 700;
+              font-family: ${monospaceFont};
+              margin-bottom: 0.875rem;
+            `}
+            tagName="div"
+            value={title}
+            onChange={(value: string) => setAttributes({ title: value })}
+            placeholder={t("highlight-box-title-placeholder")}
+          />
+          <p
+            className={css`
+              margin: 0 0 0.5rem 0;
+              font-family: ${primaryFont};
+              font-size: 0.8125rem;
+              color: ${baseTheme.colors.gray[700]};
+              font-weight: ${fontWeights.medium};
+            `}
+          >
+            {t("highlight-box-content-label")}
+          </p>
+          <RichText
+            className={css`
+              display: block;
+              width: 100%;
+              min-height: 2rem;
+              border: 1px solid ${baseTheme.colors.gray[500]};
+              border-radius: 4px;
+              padding: 0.5rem 0.75rem;
+              background: #ffffff;
+            `}
+            tagName="div"
+            value={content}
+            onChange={(value: string) => setAttributes({ content: value })}
+            placeholder={t("highlight-box-content-placeholder")}
+          />
+        </div>
       </div>
     </BlockWrapper>
   )

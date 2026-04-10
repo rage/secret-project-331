a0d6f76d782daa27ebceeb7971af3f6743609b2c
Heljä Räisänen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2026-04-10 10:43:00 +0300

    migrations for new message system in v1 api

---
+++ services/headless-lms/migrations/20260408100033_new_chatbot_conversation_messages.down.sql
@@ -0,0 +1,65 @@
+-- Add down migration script here
+ALTER TABLE chatbot_conversation_messages
+ADD COLUMN message VARCHAR(131072),
+  ADD COLUMN message_role message_role,
+  -- not null
+ADD COLUMN tool_output_id UUID REFERENCES chatbot_conversation_message_tool_outputs(id),
+  ADD COLUMN used_tokens INT NOT NULL DEFAULT 0,
+  ADD COLUMN message_is_complete BOOLEAN NOT NULL DEFAULT FALSE;
+
+COMMENT ON COLUMN chatbot_conversation_messages.message IS 'The message content.';
+COMMENT ON COLUMN chatbot_conversation_messages.message_role IS 'The role of the message, is it from the user or the chatbot, or does it contain chatbot tool call output.';
+COMMENT ON COLUMN chatbot_conversation_messages.message_is_complete IS 'Always true for messages from the user. The chatbot messages are streamed to the client, and this field is used to indicate whether that the stream is complete.';
+COMMENT ON COLUMN chatbot_conversation_messages.used_tokens IS 'The number of tokens used to send or receive this message.';
+
+UPDATE chatbot_conversation_messages ccm
+SET message = ccmm.text,
+  message_role = ccmm.role,
+  used_tokens = ccmm.used_tokens
+FROM chatbot_conversation_message_messages ccmm
+WHERE ccm.id = ccmm.chatbot_conversation_message_id;
+
+UPDATE chatbot_conversation_messages ccm
+SET tool_output_id = ccmto.id,
+  message_role = 'tool'::message_role
+FROM chatbot_conversation_message_tool_outputs ccmto
+WHERE ccm.id = ccmto.chatbot_conversation_message_id;
+
+UPDATE chatbot_conversation_messages ccm
+SET message_role = 'assistant'::message_role
+FROM chatbot_conversation_message_tool_calls ccmtc
+WHERE ccm.id = ccmtc.chatbot_conversation_message_id;
+
+UPDATE chatbot_conversation_messages
+SET message_is_complete = TRUE
+WHERE TRUE;
+
+DELETE FROM chatbot_conversation_messages ccm USING chatbot_conversation_message_reasoning ccmr
+WHERE ccm.id = ccmr.chatbot_conversation_message_id;
+
+ALTER TABLE chatbot_conversation_messages
+ALTER COLUMN message_role
+SET NOT NULL;
+
+DROP TABLE chatbot_conversation_message_messages;
+DROP TABLE chatbot_conversation_message_reasoning;
+
+ALTER TABLE chatbot_conversation_message_tool_calls DROP COLUMN kind,
+  RENAME COLUMN chatbot_conversation_message_id TO message_id;
+
+ALTER TABLE chatbot_conversation_message_tool_outputs DROP COLUMN kind,
+  RENAME COLUMN chatbot_conversation_message_id TO message_id,
+  RENAME COLUMN output TO tool_output,
+  ADD COLUMN tool_name varchar(255);
+
+COMMENT ON COLUMN chatbot_conversation_message_tool_outputs.tool_name IS 'The chatbot tool that was called.';
+
+
+UPDATE chatbot_conversation_message_tool_outputs ccmto
+SET tool_name = ccmtc.tool_name
+FROM chatbot_conversation_message_tool_calls ccmtc
+WHERE ccmto.tool_call_id = ccmtc.id;
+
+ALTER TABLE chatbot_conversation_message_tool_outputs
+ALTER COLUMN tool_name
+SET NOT NULL;

f9c0f5ee478eefe470133814d49adbc3c9f48cac
Henrik Nygren <nygrenh@gmail.com>
2026-05-26 12:15:35 +0300

    Improve devcontainer configuration and setup (#1716)
    * Devcontainer improvements
    
    * WIP
    
    * Use the right user

--- .devcontainer/scripts/post-create.fish
+++ .devcontainer/scripts/post-create.fish
@@ -6,24 +6,10 @@ set -l repo_root (realpath "$script_dir/../..")
 set -gx NIX_CONFIG "experimental-features = nix-command flakes"
 set -gx TILT_DISABLE_ANALYTICS 1
 
-if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
-    source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
-end
-if test -d /nix/var/nix/profiles/default/bin
-    fish_add_path -m /nix/var/nix/profiles/default/bin
-end
-if test -d "$HOME/.nix-profile/bin"
-    fish_add_path -m "$HOME/.nix-profile/bin"
-end
-
 cd "$repo_root"
 
 "$script_dir/ensure-nix.fish"
 "$script_dir/ensure-local-secrets.fish"
-
-if command -q direnv
-    direnv allow "$repo_root"
-end
 
 nix develop "$repo_root" --command pnpm --version
 nix develop "$repo_root" --command rustc --version

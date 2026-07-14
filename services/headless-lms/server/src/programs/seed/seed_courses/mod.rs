b9cf1b175c3b815c9c5495bf2d3c03558fab46d6
Emil Kivelä <emil.kivela@gmail.com>
2026-07-14 15:09:43 +0300

    Add tests for jsonLd

--- services/headless-lms/server/src/programs/seed/seed_courses/mod.rs
+++ services/headless-lms/server/src/programs/seed/seed_courses/mod.rs
@@ -5,6 +5,8 @@ pub mod seed_lock_chapter_course;
 pub use seed_lock_chapter_course::seed_lock_chapter_course;
 pub mod seed_material_reference_course;
 pub use seed_material_reference_course::seed_material_reference_course;
+pub mod seed_metadata_course;
+pub use seed_metadata_course::seed_metadata_course;
 pub mod seed_course_with_peer_review;
 pub mod seed_graded;
 pub use seed_graded::seed_graded_course;
---
+++ system-tests/src/tests/course-metadata-json.spec.ts
@@ -0,0 +1,53 @@
+import { test, expect } from "@playwright/test"
+import { selectCourseInstanceIfPrompted } from "@/utils/courseMaterialActions"
+
+test.use({
+  storageState: "src/states/teacher@example.com.json",
+})
+
+test("Metadata jsonLd exist", async ({ page }) => {
+  await page.goto("http://project-331.local/")
+  await page.getByRole("link", { name: "Navigate to course 'Metadata course'" }).click()
+  await selectCourseInstanceIfPrompted(page)
+  const jsonLdLocator = page.locator("script[type='application/ld+json']")
+  await expect(jsonLdLocator).toBeAttached()
+})
+
+test("Metadata has correct fields set fields", async ({ page }) => {
+  await page.goto("http://project-331.local/")
+  await page.getByRole("link", { name: "Navigate to course 'Metadata course'" }).click()
+  await selectCourseInstanceIfPrompted(page)
+  const jsonLdLocator = page.locator("script[type='application/ld+json']")
+  const rawJSON = await jsonLdLocator.textContent()
+  const metadata = JSON.parse(rawJSON!)
+  expect(metadata["@context"]).toBe("https://schema.org")
+  expect(metadata["@type"]).toBe("Course")
+  expect(metadata["name"]).toBe("Metadata course")
+  expect(metadata["description"]).toBe("Course for testing jsonld metadata.")
+  expect(metadata["courseCode"]).toBe("TEST001")
+  expect(metadata["coursePrerequisites"]).toStrictEqual([])
+  expect(metadata["audience"]).toStrictEqual([])
+})
+
+test("Teacher setting metadata suggestions changes jsonLd", async ({ page }) => {
+  await page.goto("http://project-331.local/")
+  await page.getByRole("link", { name: "Manage course 'Metadata" }).click()
+  await page.getByRole("button", { name: "Suggest metadata" }).click()
+  await page.getByRole("button", { name: "Replace metadata" }).click()
+  await page.getByRole("button", { name: "Open course front page" }).click()
+  await selectCourseInstanceIfPrompted(page)
+  await selectCourseInstanceIfPrompted(page)
+  const jsonLdLocator = page.locator("script[type='application/ld+json']")
+  const rawJSON = await jsonLdLocator.textContent()
+  const metadata = JSON.parse(rawJSON!)
+  expect(metadata["description"]).toBe(
+    "Introductory course to containers and containerization with Docker. Introduces containerization with Docker and relevant concepts such as image and volume. After completion, students are able to run containerized applications, containerize applications, utilize volumes to store data persistently outside containers, use port mapping to enable access via TCP to containerized applications, and share their own containers publicly.",
+  )
+  expect(metadata["coursePrerequisites"]).toStrictEqual([
+    "No hard prerequisites",
+    "Linux operating systems and web development experience are useful",
+  ])
+  expect(metadata["audience"]).toStrictEqual([
+    { "@type": "EducationalAudience", educationalRole: "student", audienceType: "students" },
+  ])
+})

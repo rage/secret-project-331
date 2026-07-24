17429c794d9c20c503ed0c2efda99b1151c7e11f
Erik Peteri <peteri.erik@gmail.com>
2026-07-24 13:17:44 +0300

    fix error handling in chatbot tools

--- services/headless-lms/chatbot/src/chatbot_tools/custom_tools/course_progress.rs
+++ services/headless-lms/chatbot/src/chatbot_tools/custom_tools/course_progress.rs
@@ -33,13 +33,22 @@ impl ChatbotTool for CourseProgressTool {
         user_context: &ChatbotUserContext,
     ) -> ChatbotResult<Self> {
         let Some(user_id) = user_context.user_id else {
-            return Err(chatbot_err!(Other, "User id is missing.".to_string()));
+            return Err(chatbot_err!(
+                ToolUseError,
+                "User id is missing.".to_string()
+            ));
         };
         let Some(course_id) = user_context.course_id else {
-            return Err(chatbot_err!(Other, "Course id is missing.".to_string()));
+            return Err(chatbot_err!(
+                ToolUseError,
+                "Course id is missing.".to_string()
+            ));
         };
         let Some(course_name) = &user_context.course_name else {
-            return Err(chatbot_err!(Other, "Course name is missing.".to_string()));
+            return Err(chatbot_err!(
+                ToolUseError,
+                "Course name is missing.".to_string()
+            ));
         };
         let user_progress = headless_lms_models::user_exercise_states::get_user_course_progress(
             conn, course_id, user_id, true,

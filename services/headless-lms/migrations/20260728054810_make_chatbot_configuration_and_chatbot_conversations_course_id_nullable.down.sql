5adb0720ee7fbddaa2a13024d7a22460fee8a4a9
Erik Peteri <peteri.erik@gmail.com>
2026-07-28 08:51:55 +0300

    remove begin and commit from migrations

--- services/headless-lms/migrations/20260728054810_make_chatbot_configuration_and_chatbot_conversations_course_id_nullable.down.sql
+++ services/headless-lms/migrations/20260728054810_make_chatbot_configuration_and_chatbot_conversations_course_id_nullable.down.sql
@@ -1,6 +1,4 @@
 -- Add down migration script here
-BEGIN;
-
 ALTER TABLE chatbot_configurations
 ALTER COLUMN course_id
 SET NOT NULL;
@@ -8,5 +6,3 @@ SET NOT NULL;
 ALTER TABLE chatbot_conversations
 ALTER COLUMN course_id
 SET NOT NULL;
-
-COMMIT;

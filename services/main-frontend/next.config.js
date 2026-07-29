69036d0bbb4f36727164061ecce2ee3bbfa0ce58
Erik Peteri <peteri.erik@gmail.com>
2026-07-29 09:16:24 +0300

    create page with chatbot that is allowed to be embedded inside iframe

--- services/main-frontend/next.config.js
+++ services/main-frontend/next.config.js
@@ -1,10 +1,13 @@
 /* oxlint-disable import/order */
 const generateNormalResponseHeaders =
   require("./src/shared-module/common/utils/responseHeaders").generateNormalResponseHeaders
+const externallyEmbeddableIFrameResponseHeaders =
+  require("./src/shared-module/common/utils/responseHeaders").externallyEmbeddableIFrameResponseHeaders
 const svgoConfig = require("./src/shared-module/common/utils/svgoConfig")
 
 // Trusted types blocked on: https://github.com/vercel/next.js/issues/32209
 const normalResponseHeaders = generateNormalResponseHeaders({ requireTrustedTypesFor: false })
+const iframeResponseHeaders = externallyEmbeddableIFrameResponseHeaders
 
 /** @type {import('next').NextConfig} */
 const config = {
@@ -24,6 +27,10 @@ const config = {
       {
         source: "/(.*)",
         headers: normalResponseHeaders,
+      },
+      {
+        source: "/chatbot-embed/:id",
+        headers: iframeResponseHeaders,
       },
     ]
   },

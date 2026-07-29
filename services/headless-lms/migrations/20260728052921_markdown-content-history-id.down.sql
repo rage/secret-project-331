ab09463bd7cb586274857e3343427e55fc349397
Heljä Räisänen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2026-07-29 12:46:10 +0300

    improve migrations

--- services/headless-lms/migrations/20260728052921_markdown-content-history-id.down.sql
+++ services/headless-lms/migrations/20260728052921_markdown-content-history-id.down.sql
@@ -1,2 +1,3 @@
--- Add down migration script here
+DROP INDEX IF EXISTS course_page_markdown_content_page_history_id;
+
 ALTER TABLE course_page_markdown_content DROP COLUMN page_history_id;
--- services/headless-lms/migrations/20260728052921_markdown-content-history-id.up.sql
+++ services/headless-lms/migrations/20260728052921_markdown-content-history-id.up.sql
@@ -1,12 +1,11 @@
--- Add up migration script here
 UPDATE chatbot_page_sync_statuses
-SET converted_markdown_content_id = NULL
-WHERE TRUE;
+SET converted_markdown_content_id = NULL;
 
-DELETE FROM course_page_markdown_content
-WHERE TRUE;
+DELETE FROM course_page_markdown_content;
 
 ALTER TABLE course_page_markdown_content
 ADD COLUMN page_history_id UUID NOT NULL REFERENCES page_history(id);
 
 COMMENT ON COLUMN course_page_markdown_content.page_history_id IS 'The page history version that this Markdown was generated from.';
+
+CREATE INDEX course_page_markdown_content_page_history_id ON course_page_markdown_content(page_history_id);

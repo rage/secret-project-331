9a4122b6086a9d590feca7e62d73e69d36c13055
Erik Peteri <peteri.erik@gmail.com>
2026-08-07 10:15:40 +0300

    rename NoLayoutLayout to YesLayoutLayout

--- services/main-frontend/src/app/(layout)/layout.tsx
+++ services/main-frontend/src/app/(layout)/layout.tsx
@@ -5,7 +5,7 @@ import React from "react"
 import Layout from "@/components/Layout"
 import withErrorBoundary from "@/shared-module/common/utils/withErrorBoundary"
 
-const NoLayoutLayout = ({
+const YesLayoutLayout = ({
   children,
   params: _params,
 }: {
@@ -15,4 +15,4 @@ const NoLayoutLayout = ({
   return <Layout>{children}</Layout>
 }
 
-export default withErrorBoundary(NoLayoutLayout)
+export default withErrorBoundary(YesLayoutLayout)

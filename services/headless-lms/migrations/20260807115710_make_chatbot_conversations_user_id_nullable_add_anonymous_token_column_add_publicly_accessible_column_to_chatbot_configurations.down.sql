5dfc10739c24109216afa35a047acc1390783a08
Erik Peteri <peteri.erik@gmail.com>
2026-08-07 15:04:38 +0300

    add column publicly_accessible to chatbot_configurations

--- services/headless-lms/migrations/20260807115710_make_chatbot_conversations_user_id_nullable_add_anonymous_token_column_add_publicly_accessible_column_to_chatbot_configurations.down.sql
+++ services/headless-lms/migrations/20260807115710_make_chatbot_conversations_user_id_nullable_add_anonymous_token_column_add_publicly_accessible_column_to_chatbot_configurations.down.sql
@@ -1,7 +1,10 @@
--- Add down migration script here
 ALTER TABLE chatbot_conversations DROP CONSTRAINT user_id_or_anonymous_token_set;
 
+ALTER TABLE chatbot_configurations DROP CONSTRAINT course_id_and_publicly_accessible_not_both_set;
+
 ALTER TABLE chatbot_conversations DROP COLUMN anonymous_token;
+
+ALTER TABLE chatbot_configurations DROP COLUMN publicly_accessible;
 
 ALTER TABLE chatbot_conversations
 ALTER COLUMN user_id

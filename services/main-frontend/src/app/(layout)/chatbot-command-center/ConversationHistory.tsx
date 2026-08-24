49260cd0325ed39d65710c38db64394eeedc8a70
Erik Peteri <peteri.erik@gmail.com>
2026-08-24 15:44:55 +0300

    make whole conversation item clickable

--- services/main-frontend/src/app/(layout)/chatbot-command-center/ConversationHistory.tsx
+++ services/main-frontend/src/app/(layout)/chatbot-command-center/ConversationHistory.tsx
@@ -23,85 +23,77 @@ const ConversationHistory: React.FC<ConversationHistory> = ({
   chatbots,
 }) => {
   return (
-    <div
-      className={css`
-        display: flex;
-        flex-direction: column;
-      `}
-    >
+    <>
       {conversations.map((conversation) => (
-        <div
+        <Button
+          size="medium"
+          variant="icon"
+          onClick={() => {
+            setConversationId(conversation.id)
+            setValue("id", conversation.chatbot_configuration_id, {
+              shouldValidate: true,
+              shouldDirty: true,
+            })
+          }}
           className={css`
+            width: calc(100% - 2rem);
+            justify-content: flex-start;
             border-top: 1px solid ${baseTheme.colors.gray[75]};
-            padding: 0.5rem 0;
-            margin: 0 12px;
+            padding: 2rem 0;
+            border-radius: 0;
+            margin: 0 1rem;
           `}
           key={conversation.id}
         >
-          <Button
-            size="medium"
-            variant="icon"
-            onClick={() => {
-              setConversationId(conversation.id)
-              setValue("id", conversation.chatbot_configuration_id, {
-                shouldValidate: true,
-                shouldDirty: true,
-              })
-            }}
+          <div
             className={css`
+              display: flex;
+              flex-direction: column;
+              align-items: flex-start;
               font-size: 14px;
               font-weight: 500;
-              padding-left: 0;
             `}
           >
             <div
               className={css`
-                display: flex;
-                flex-direction: column;
-                align-items: flex-start;
+                white-space: nowrap;
+                max-width: 300px;
+                overflow: hidden;
+                text-overflow: ellipsis;
               `}
             >
-              <div
-                className={css`
-                  white-space: nowrap;
-                  max-width: 320px;
-                  overflow: hidden;
-                  text-overflow: ellipsis;
-                `}
-              >
-                {conversation.conversation_title !== null
-                  ? conversation.conversation_title
-                  : // oxlint-disable-next-line i18next/no-literal-string
-                    "untitled conversation"}
-              </div>
-              <span
-                className={css`
-                  padding-top: 0.25rem;
-                  font-size: 10px;
-                  white-space: nowrap;
-                  width: 200px;
-                  overflow: hidden;
-                  text-overflow: ellipsis;
-                  text-align: left;
-                  border: 1px solid ${baseTheme.colors.green[300]};
-                  border-radius: 999px;
-                  color: gray;
-                  max-width: 100px;
-                  padding-left: 10px;
-                  margin-top: 5px;
-                  padding-bottom: 5px;
-                `}
-              >
-                {
-                  chatbots.find((chatbot) => chatbot.id === conversation.chatbot_configuration_id)
-                    ?.chatbot_name
-                }
-              </span>
+              {conversation.conversation_title !== null
+                ? conversation.conversation_title
+                : // oxlint-disable-next-line i18next/no-literal-string
+                  "untitled conversation"}
             </div>
-          </Button>
-        </div>
+            <span
+              className={css`
+                padding-top: 0.25rem;
+                font-size: 10px;
+                white-space: nowrap;
+                width: 200px;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                text-align: left;
+                border: 1px solid ${baseTheme.colors.green[300]};
+                border-radius: 999px;
+                color: gray;
+                max-width: 100px;
+                padding-left: 10px;
+                margin-top: 5px;
+                padding-bottom: 5px;
+              `}
+            >
+              {
+                chatbots.find((chatbot) => chatbot.id === conversation.chatbot_configuration_id)
+                  ?.chatbot_name
+              }
+            </span>
+          </div>
+        </Button>
       ))}
-    </div>
+    </>
   )
 }
 

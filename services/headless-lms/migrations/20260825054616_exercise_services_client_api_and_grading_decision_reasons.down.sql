5f345ade38f3a3aee90086f26b316a8938a75c95
Henrik Nygren <nygrenh@gmail.com>
2026-08-25 08:53:03 +0300

    Consolidate the branch migrations into one

--- services/headless-lms/migrations/20260825054616_exercise_services_client_api_and_grading_decision_reasons.down.sql
+++ services/headless-lms/migrations/20260825054616_exercise_services_client_api_and_grading_decision_reasons.down.sql
@@ -1,6 +1,3 @@
-ALTER TABLE exercise_reset_logs
-DROP COLUMN teacher_feedback;
-
 UPDATE teacher_grading_decisions
 SET teacher_decision = 'suspected-plagiarism'
 WHERE teacher_decision = 'unauthorized-ai-use';
@@ -24,3 +21,16 @@ ALTER TABLE teacher_grading_decisions
 ALTER COLUMN teacher_decision TYPE teacher_decision_type USING teacher_decision::text::teacher_decision_type;
 
 DROP TYPE teacher_decision_type_old;
+
+ALTER TABLE exercise_service_info DROP COLUMN answer_files_endpoint_path;
+
+DROP TABLE exercise_service_client_uploads;
+
+DROP TABLE exercise_task_submission_files;
+
+ALTER TABLE exercise_service_info DROP COLUMN build_user_answer_endpoint_path;
+
+DROP TABLE IF EXISTS oauth_device_codes;
+DROP TYPE IF EXISTS device_code_status;
+
+DROP TABLE IF EXISTS exercise_slide_submission_shares;
